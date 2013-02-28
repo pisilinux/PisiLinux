27f03703b3a59be60f93d46f7e5e4a84d26ba3ea
marcin- <marcin.bojara@gmail.com>
2013-02-28 12:40:30 +0100

    libdrm: version bump

--- x11/library/libdrm/actions.py
+++ x11/library/libdrm/actions.py
@@ -3,12 +3,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.autoreconf("-vif")
     autotools.configure("--enable-udev")
 
 def build():
