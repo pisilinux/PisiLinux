4a727f058e7d1a7c20dc077da11408d9799abde9
obsoleteman <tulliana@gmail.com>
2013-02-28 20:13:17 +0200

    some fixes for Pisi Linux

--- office/spellcheck/zpspell/actions.py
+++ office/spellcheck/zpspell/actions.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 # Copyleft PiSi GNU/Linux Community
-# Copyleft PiSi GNU/Linux Community
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
