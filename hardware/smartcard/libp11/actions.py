4dbe93eaf31fb6cee2b02605e97af644643e4e70
sezaiyeniay <sezaiyeniay@ozgurlukicin.com>
2013-03-01 11:49:35 +0200

    kozmetik

--- hardware/smartcard/libp11/actions.py
+++ hardware/smartcard/libp11/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#Copyleft 2012 Pardus ANKA Community
 # Copyleft PiSi GNU/Linux Community
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
--- programming/misc/libkolab/actions.py
+++ programming/misc/libkolab/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyleft 2013 Pardus ANKA Community
+# Copyleft PisiLinux Community
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
