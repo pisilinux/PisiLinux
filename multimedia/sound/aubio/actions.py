19698a4ae12dde85fa3207ae443c257573d3ea52
namso-01 <osman.erkan@yandex.com>
2013-03-07 20:44:49 +0200

    aubio fixed

--- multimedia/sound/aubio/actions.py
+++ multimedia/sound/aubio/actions.py
@@ -6,9 +6,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.export("LDFLAGS", "%s -lm" % get.LDFLAGS())
     #prevent sandbox error
     pisitools.dosed("python/aubio/Makefile.in", "^py_compile =.*", "py_compile = /bin/true")
 
