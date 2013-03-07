6a13358a1595777db3df1f5299c622ca3a2ad11a
circass <erdincgultekin@gmail.com>
2013-03-07 06:16:41 +0200

    amazing update of firmwares

--- hardware/firmware/linux-firmware/actions.py
+++ hardware/firmware/linux-firmware/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "linux-firmware-20121212"
+WorkDir = "linux-firmware-20130224"
 
 def setup():
     # Remove source files
@@ -34,9 +34,6 @@ def install():
     pisitools.remove("/lib/firmware/LICENSE*")
     pisitools.remove("/lib/firmware/configure")
     pisitools.remove("/lib/firmware/Makefile")
-    pisitools.remove("/lib/firmware/ipw2200-bss.fw")
-    pisitools.remove("/lib/firmware/ipw2200-ibss.fw")
-    pisitools.remove("/lib/firmware/ipw2200-sniffer.fw")
     pisitools.removeDir("/lib/firmware/mix")
 
     # Install LICENSE files
