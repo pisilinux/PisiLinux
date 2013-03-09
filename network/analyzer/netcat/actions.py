579a110544cba975d16ab5cfcd1da7f591cc57e6
namso-01 <osman.erkan@yandex.com>
2013-03-09 18:27:04 +0200

    netcat: adress fixed

--- network/analyzer/netcat/actions.py
+++ network/analyzer/netcat/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "netcat-openbsd-%s.orig" % get.srcVERSION().split("_", 1)[1]
-
 def build():
     autotools.make("CC=%s" %get.CC())
 
