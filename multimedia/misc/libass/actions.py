63fe281c6539ce73fe83ecb30a0e0cb564a7e155
namso-01 <osman.erkan@yandex.com>
2013-03-09 17:29:02 +0200

    libass fix

--- multimedia/misc/libass/actions.py
+++ multimedia/misc/libass/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-#WorkDir = "libass"
-
 def setup():
     autotools.autoreconf("-vfi")
     autotools.configure("--enable-fontconfig \
