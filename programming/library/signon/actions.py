9445fc08b2fa9e44adb840cf794cfb26657583cf
circass <erdincgultekin@gmail.com>
2013-03-09 08:11:43 +0200

    fix path

--- programming/library/signon/actions.py
+++ programming/library/signon/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    qt4.configure(parameters="LIBDIR=/lib")
+    qt4.configure(parameters="LIBDIR=/usr/lib")
 
 def build():
     qt4.make()
