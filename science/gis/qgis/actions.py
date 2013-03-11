4d8512e5e0e06229f5edd9ef86877568a1d022a2
richdb <rr.debruin@gmail.com>
2013-03-11 22:45:09 +0100

    added qgis

---
+++ science/gis/qgis/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+                          -DSPATIALINDEX_INCLUDE_DIR=/usr/include/libspatialindex \
+                          -DQGIS_MANUAL_SUBDIR=share/man", installPrefix="/usr",sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("")
+    pisitools.dosed('debian/qgis.desktop',  'qgis-icon',  '/usr/share/qgis/images/icons/qgis-icon.png')
+    pisitools.insinto("/usr/share/applications/", "debian/qgis.desktop")
+    pisitools.domove("/usr/share/qgis/doc/*", "/usr/share/doc/qgis/")
+    pisitools.dosym("/usr/share/doc/qgis/news.html", "/usr/share/qgis/doc/news.html")
