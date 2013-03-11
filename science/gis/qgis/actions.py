195d0c0784834581f880ef577699b2165f3c6f6c
Ertan Güven <ertangvn@gmail.com>
2013-03-11 23:46:16 +0200

    licensing has been fixed

--- science/gis/qgis/actions.py
+++ science/gis/qgis/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
