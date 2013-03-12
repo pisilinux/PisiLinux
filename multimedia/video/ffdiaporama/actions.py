a4945571b27e06c4a4ac10983ff113357f52f77b
richdb <rr.debruin@gmail.com>
2013-03-12 22:05:27 +0100

    first release ffdiaporama

---
+++ multimedia/video/ffdiaporama/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt4
+
+WorkDir = "."
+
+def setup():
+    qt4.configure()
+
+def build():
+    qt4.make()
+
+def install():
+    qt4.install()
+
+# By PiSiDo 2.0.0
