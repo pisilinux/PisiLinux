c23cc106641c3f51add51bb64360df39147fc7aa
Ertan Güven <ertangvn@gmail.com>
2013-03-15 22:10:23 +0200

    NetworkManager revert

--- network/connection/NetworkManager/actions.py
+++ network/connection/NetworkManager/actions.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
