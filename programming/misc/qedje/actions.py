0b4e837ce0af003ccc6ae5fd09b39364257161f5
circass <erdincgultekin@gmail.com>
2013-03-18 02:44:52 +0200

    sandbox

--- programming/misc/qedje/actions.py
+++ programming/misc/qedje/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "qedje-mainline"
-
 def setup():
     shelltools.makedirs("build")
     shelltools.cd("build")
