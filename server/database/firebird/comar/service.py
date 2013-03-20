16a3f8a2b18ee197e348b8561186e0f527284ab2
Ertan Güven <ertangvn@gmail.com>
2013-03-20 11:46:16 +0200

    Firebird Service Fix.

--- server/database/firebird/comar/service.py
+++ server/database/firebird/comar/service.py
@@ -11,11 +11,11 @@ pid_file = "/run/firebird/firebird.pid"
 
 @synchronized
 def start():
-    startService(command="/opt/firebird/bin/fbmgr.bin",
-                 args="-pidfile %s -start -forever" % pid_file,
-                 chuid="firebird",
+    startService(command="/opt/firebird/bin/fbguard",
+                 args="-pidfile %s -start -forever -daemon" % pid_file,
                  pidfile="/run/firebird/firebird.pid",
                  donotify=True)
+#                 chuid="firebird",
 
 @synchronized
 def stop():
