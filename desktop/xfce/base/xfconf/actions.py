0474df9d7e9be158a19febdb27d594b3eb0cc578
obsoleteman <tulliana@gmail.com>
2013-03-22 05:49:57 +0200

    xfconf: fix build

--- desktop/xfce/base/xfconf/actions.py
+++ desktop/xfce/base/xfconf/actions.py
@@ -6,12 +6,10 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import perlmodules
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system('xdt-autogen')
     autotools.configure('--disable-static')
 
 def build():
