c609fddc163338c00735a43c22a7efc2c7039a70
marcin- <marcin.bojara@gmail.com>
2013-03-23 19:51:36 +0100

    hinting-viewer: fix build

--- desktop/font/hinting-viewer/actions.py
+++ desktop/font/hinting-viewer/actions.py
@@ -4,12 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 
 
 def build():
+    pisitools.dosed("Makefile", "^(CFLAGS=)", r"\1%s " % get.CFLAGS())
+    pisitools.dosed("Makefile", "^(LDFLAGS=)", r"\1-lX11 -lm ")
     autotools.make()
 
 def install():
