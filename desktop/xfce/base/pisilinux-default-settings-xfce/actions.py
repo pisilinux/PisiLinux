24c0179b8006056a4cc53c52f5d7dce44d8e0d45
eguven <ertangvn@gmail.com>
2013-04-04 19:34:09 +0300

    xfce-setting settings dell

--- desktop/xfce/base/pisilinux-default-settings-xfce/actions.py
+++ desktop/xfce/base/pisilinux-default-settings-xfce/actions.py
@@ -4,7 +4,6 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
