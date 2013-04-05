050efce0462b6e937845f65046ec737f2a4d9d47
obsoleteman <tulliana@gmail.com>
2013-04-05 20:05:04 +0300

    new YALI, i ll add new po files for translators

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -9,17 +9,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    for f in ("yali/constants.py", "yali/storage/bootloader.py", "yali/storage/__init__.py"):
-        pisitools.dosed(f, "pardus-release", "pisilinux-release")
-
-    pisitools.dosed("yali/gui/Ui/exception.ui", "bugs.pardus.org.tr", r"bugs.pisilinux.org")
-
-    pisitools.dosed("yali/postinstall.py", "(yali-(theme|branding)-)pardus", r"\1pisilinux")
-    pisitools.dosed("yali-bin", '(default=")pardus', r'\1pisilinux')
-
-    repo_uri = "http://anka.pardus-linux.org/2013/anka/repos/alpha/pisi-index.xml.xz" # FIXME
+    repo_uri = "http://packages.pisilinux.org/repositories/1.0/testing/64/pisi-index.xml.xz" # FIXME
     pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
-    pisitools.dosed("yali/constants.py", "@REPO_NAME@", "pisi01") # FIXME
+    pisitools.dosed("yali/constants.py", "@REPO_NAME@", "pisi") # FIXME
 
     pisitools.dosed("conf/yali.conf", "@INSTALL_TYPE@", "system")
 
