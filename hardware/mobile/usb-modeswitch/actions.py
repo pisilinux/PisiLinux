7a2cdc527fcc605563aef30214db8d4449a64d5d
namso-01 <namso-01@hotmail.it>
2013-04-12 21:25:53 +0300

    usb-modemswitch update

--- hardware/mobile/usb-modeswitch/actions.py
+++ hardware/mobile/usb-modeswitch/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-SNAPSHOT="20110714"
+SNAPSHOT="20121109"
 
 def build():
     autotools.make("CC='%s' CFLAGS='%s'" % (get.CC(), get.CFLAGS()))
