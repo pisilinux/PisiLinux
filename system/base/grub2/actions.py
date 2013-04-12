cb878c4004c2e3ab7323684fc6182ac3c7256b04
obsoleteman <tulliana@gmail.com>
2013-04-12 14:04:44 +0300

    merge: new grub2

--- system/base/grub2/actions.py
+++ system/base/grub2/actions.py
@@ -8,9 +8,6 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-unifontfile_path="/usr/share/fonts/unifont/unifont-5.1.20080820.pcf"
-pf2_fonts=["dejavu_10.pf2","dejavu_12.pf2","dejavu_14.pf2","dejavu_16.pf2","dejavu_bold_14.pf2"]
-
 def setup():
     shelltools.copy("../unifont*.pcf.gz", "./unifont.pcf.gz")
     shelltools.export("GRUB_CONTRIB", "%s/grub-%s/grub-extras" % (get.workDIR(), get.srcVERSION()))
@@ -24,26 +21,39 @@ def setup():
                          --htmldir='/usr/share/doc/${PF}/html' ")
 
 def build():
+    #make-dist for creating all updated translation files
+    autotools.make("dist")
     autotools.make()
     
-
 def install():
-    # Install unicode.pf2 using installed font source. 
-    # Do not touch installation path, grub2 needs it exactly in /boot/grub2.
-    cmd="./grub-mkfont -o unicode.pf2 %s" % unifontfile_path
-    shelltools.system(cmd)
+    # Install unicode.pf2 using downloaded font source. 
+    shelltools.system("./grub-mkfont -o unicode.pf2 unifont.pcf.gz")
+     
+    # Create directory for grub.cfg file
     pisitools.dodir("/boot/grub2")
     pisitools.insinto("/boot/grub2", "unicode.pf2")
     
-    #Install dejavu.pf2 fonts to /usr/share/grub/fonts
-    pisitools.dodir("/usr/share/grub/fonts")
-    for i in pf2_fonts:
-        pisitools.insinto("/usr/share/grub/fonts", i)
+    # Insall our theme
+    pisitools.insinto("/usr/share/grub/themes/","themes/pisilinux")
     
+    #remove -r 0x0-0x7F entries to fix ugly fonts or find a suitable range parameter -r ***
+    shelltools.system("./grub-mkfont -o DejaVuSans-10.pf2 -r 0x0-0x7F -s 10 /usr/share/fonts/dejavu/DejaVuSans.ttf")
+    shelltools.system("./grub-mkfont -o DejaVuSans-12.pf2 -r 0x0-0x7F -s 12 /usr/share/fonts/dejavu/DejaVuSans.ttf")
+    shelltools.system("./grub-mkfont -o DejaVuSans-14.pf2 -r 0x0-0x7F -s 14 /usr/share/fonts/dejavu/DejaVuSans.ttf")
+    shelltools.system("./grub-mkfont -o DejaVuSans-16.pf2 -r 0x0-0x7F -s 16 /usr/share/fonts/dejavu/DejaVuSans.ttf")
+    shelltools.system("./grub-mkfont -o DejaVuSans-Bold-14.pf2 -r 0x0-0x7F -s 14 /usr/share/fonts/dejavu/DejaVuSans-Bold.ttf")
+    shelltools.system("./grub-mkfont -o DejaVuSans-Mono-14.pf2 -r 0x0-0x7F -s 14 /usr/share/fonts/dejavu/DejaVuSansMono.ttf")
+    shelltools.copy("ascii.pf2","%s/usr/share/grub/themes/pisilinux" % get.installDIR())
+
+    # Do not install auto generated dejavu* fonts
+    fonts=["DejaVuSans-10.pf2" , "DejaVuSans-12.pf2" , "DejaVuSans-14.pf2" , "DejaVuSans-16.pf2" , "DejaVuSans-Mono-14.pf2", "DejaVuSans-Bold-14.pf2"]
+    for font in fonts:
+        shelltools.copy(font,"%s/usr/share/grub/themes/pisilinux" % get.installDIR())
+
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     
-    #Remove default starfiled theme. Use Anka's brillant one :) 
+    #Remove default starfiled theme.
     pisitools.removeDir("/usr/share/grub/themes/starfield")
     
     pisitools.dodoc("ABOUT-NLS", "AUTHORS", "BUGS", "ChangeLog", "COPYING", "TODO", "README")
-    
+
