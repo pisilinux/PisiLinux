882113b5b539482a783a028a2995687862ce6337
namso-01 <osman.erkan@yandex.com>
2013-04-28 16:38:39 +0300

    blender fixed + v.bump

--- multimedia/editor/blender/actions.py
+++ multimedia/editor/blender/actions.py
@@ -11,65 +11,46 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import scons
 from pisi.actionsapi import get
 
-def setup():
-    # Set python version in user-config
-    #pisitools.dosed("user-config.py", "@LIB@", "/usr/lib")
-    #pisitools.dosed("user-config.py", "@PYVER@", get.curPYTHON().replace("python", ""))
-    #pisitools.dosed("user-config.py", "@CC@", get.CC())
-    #pisitools.dosed("user-config.py", "@CXX@", get.CXX())
-    #pisitools.dosed("user-config.py", "^WITH_BF_FFMPEG = 'false'.*", "WITH_BF_FFMPEG = 'true'")
+shelltools.export("HOME", get.workDIR())
 
-    # Drop bundled libraries
-    for d in ["ffmpeg", "x264", "xvidcore", "fftw", "glew", "libopenjpeg", "ode", "qhull"]:
-        shelltools.system("rm -rf extern/%s" % d)
-    shelltools.system("rm -rf scons")
+def setup():
+    shelltools.system("rm -rf CMakeCache.txt")
+    shelltools.cd("..")
+    shelltools.makedirs("cmake-make")
+    shelltools.cd("cmake-make") 
+    shelltools.system("cmake ../blender-2.66 \
+		      -DCMAKE_INSTALL_PREFIX=/usr \
+		      -DCMAKE_BUILD_TYPE=Release \
+		      -DWITH_JACK=ON \
+		      -DWITH_PLAYER=ON \
+		      -DWITH_CODEC_FFMPEG=ON \
+		      -DWITH_INSTALL_PORTABLE=OFF \
+		      -DWITH_GAMEENGINE=ON \
+		      -DWITH_PYTHON_INSTALL=OFF \
+		      -DWITH_CODEC_SNDFILE=ON ")
 
 def build():
-    #scons.make("WITH_BF_PLAYER=1 WITH_BF_OPENAL=1 BF_FANCY=0")
-    shelltools.export("RPM_OPT_FLAGS", get.CFLAGS())
-    scons.make("WITH_BF_PLAYER=1 WITH_BF_OPENAL=1 WITH_BF_INTERNATIONAL=1 BF_QUIET=0 BF_NUMJOBS=%s" % get.makeJOBS().replace("-j", "1"))
-
-    shelltools.makedirs("release/plugins/include")
-    shelltools.copy("source/blender/blenpluginapi/*.h", "release/plugins/include")
-
-    shelltools.chmod("release/plugins/bmake", 0755)
-    autotools.make("-C release/plugins")
+    cmaketools.make()
 
 def install():
-    pisitools.insinto("/usr/bin","install/linux2/blender","blender-bin")
-    pisitools.dobin("install/linux2/blenderplayer")
+    shelltools.cd("../") 
+    shelltools.cd("build_linux/")
+    pisitools.insinto("/usr/bin/", "bin/blender","blender-bin")
+    pisitools.insinto("/usr/bin/", "bin/blender-thumbnailer.py")
+    pisitools.insinto("/usr/share/man/man1", "bin/blender.1")
+    pisitools.insinto("/usr/share/doc/", "bin/*.txt")
+    pisitools.insinto("/usr/share/doc/", "bin/*.html")
+    pisitools.insinto("/usr/share/pixmaps/", "bin/blender.svg")
+    pisitools.insinto("/usr/lib/", "bin/2.66/python/lib/python3.3/")
+    pisitools.insinto("/usr/share/", "bin/2.66/datafiles/locale/")
+    pisitools.insinto("/usr/share/blender/", "bin/2.66/scripts")
+    
+    shelltools.cd("../") 
+    shelltools.cd("blender-2.66/release/")
+    ## Install miscellaneous files
+    pisitools.insinto("/usr/share/blender/", "scripts/*")
+    
+    pisitools.insinto("/usr/share/blender/", "datafiles/colormanagement/")
 
-    # Install plugins
-    for d in ("/usr/lib/blender/scripts", "/usr/lib/blender/plugins/sequence", "/usr/lib/blender/plugins/texture"):
-        pisitools.dodir(d)
-    pisitools.insinto("/usr/lib/blender/plugins/texture","release/plugins/texture/*.so")
-    pisitools.insinto("/usr/lib/blender/plugins/sequence","release/plugins/sequence/*.so")
-
-    # Install miscellaneous files
-    pisitools.insinto("/usr/share/blender", "release/scripts")
-
-    pisitools.insinto("/usr/share", "install/linux2/.blender/locale")
-    pisitools.insinto("/usr/share/blender", "install/linux2/.blender/.Blanguages")
-    pisitools.insinto("/usr/share/blender", "install/linux2/.blender/.bfont.ttf")
-    pisitools.insinto("/usr/share/blender", "release/VERSION")
-
-    # chmod 644 for scripts
-    shelltools.chmod("%s/usr/share/blender/scripts/*.py" % get.installDIR(), 0644)
-    shelltools.chmod("%s/usr/share/blender/scripts/bpymodules/blend2renderinfo.py" % get.installDIR(), 0755)
-
-    # Headers
-    pisitools.insinto("/usr/include/blender", "install/linux2/plugins/include/*.h")
-
-    #Install desktop file
-    pisitools.insinto("/usr/share/applications", "release/freedesktop/blender.desktop")
-
-    #Install icon files
-    import os.path
-    for size in map(lambda x: os.path.basename(x), shelltools.ls("release/freedesktop/icons/")):
-        pisitools.dodir("/usr/share/icons/hicolor/%s/apps" % size)
-        if size == "scalable":
-            pisitools.insinto("/usr/share/icons/hicolor/%s/apps" % size, "release/freedesktop/icons/%s/*.svg" % size)
-        else:
-            pisitools.insinto("/usr/share/icons/hicolor/%s/apps" % size, "release/freedesktop/icons/%s/*.png" % size)
-
-    pisitools.dodoc("COPYING", "README")
+    ##Install icon files    
+    pisitools.insinto("/usr/share/icons/hicolor/", "freedesktop/icons/*")
--- multimedia/editor/blender/comar/package.py
+++
@@ -1,7 +0,0 @@
-#/usr/bin/python
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    if os.path.islink("/usr/lib/blender/plugins/include"):
-        os.system("/bin/rm -f /usr/lib/blender/plugins/include")
