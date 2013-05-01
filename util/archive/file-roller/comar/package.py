c4ea34ca35697d4995f5c37a9f9129f59f33f23a
aydemir <yusuf.aydemir@istanbul.com>
2013-05-01 18:44:15 +0300

    file-roller depodan silindi

--- util/archive/file-roller/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system('update-desktop-database')
-
-def preRemove():
-    os.system('update-desktop-database')
