d7b9e08287f3be27a703900eb322dbfa3464d412
marcin- <marcin.bojara@gmail.com>
2013-05-03 19:52:53 +0200

    xorg-video-tga: fix build with latest xorg-server

--- x11/driver/xorg-video-tga/actions.py
+++ x11/driver/xorg-video-tga/actions.py
@@ -4,11 +4,8 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
-WorkDir = "xf86-video-tga-%s" % get.srcVERSION()
 
 def setup():
     autotools.configure()
