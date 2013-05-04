18cc94809b0147a7e7d100c2270a5f3de8c0e07a
namso-01 <osman.erkan@yandex.com>
2013-05-04 15:46:28 +0300

    jthread v.bump

--- programming/misc/jthread/actions.py
+++ programming/misc/jthread/actions.py
@@ -4,24 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("LDFLAGS", "%s -lpthread" % get.LDFLAGS())
-
-    for files in ("AUTHORS", "NEWS", "README"):
-        shelltools.touch(files)
-
-    autotools.autoreconf("-fi")
-    autotools.configure("--disable-static")
+    cmaketools.configure()
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.install()
+    cmaketools.install()
 
     pisitools.dodoc("ChangeLog", "README.TXT", "LICENSE.MIT", "TODO")
