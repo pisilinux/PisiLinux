49a4c64cd45e8f455612de5d7cb027ee8cf70e3c
namso-01 <osman.erkan@yandex.com>
2013-05-04 16:52:03 +0300

    grsync linkleme fixed

--- network/share/grsync/actions.py
+++ network/share/grsync/actions.py
@@ -6,10 +6,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    shelltools.system("./configure prefix=/usr LDFLAGS=-lm")
 
 def build():
     autotools.make()
