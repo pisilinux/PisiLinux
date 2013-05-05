0eca8a88fe16f195358e39b3d0b0a0cf22609a23
namso-01 <osman.erkan@yandex.com>
2013-05-05 22:16:59 +0300

    w3m patched

--- network/web/w3m/actions.py
+++ network/web/w3m/actions.py
@@ -7,18 +7,25 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
-
-#shelltools.export("HOME", get.workDIR())
 
 def setup():
-    #autotools.autoreconf("-fiv")
-    autotools.configure("--enable-unicode --disable-nls --with-termlib=ncurses --host=x86_64-pc-linux-gnu --enable-image=x11 --enable-keymap=w3m --disable-dict \
-                         --without-mailer --with-imagelib=imlib2 --with-editor=/usr/bin/nano --with-browser=/usr/bin/firefox")
+    autotools.configure("--enable-unicode \
+	                 --disable-nls \
+		         --with-termlib=ncurses \
+		         --host=x86_64-pc-linux-gnu \
+			 --enable-image=x11 \
+			 --enable-keymap=w3m \
+			 --disable-dict \
+                         --without-mailer \
+		         --with-imagelib=imlib2 \
+			 --with-editor=/usr/bin/nano \
+			 --with-browser=/usr/bin/firefox")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.dodoc("ABOUT-NLS", "ChangeLog", "README")
  
