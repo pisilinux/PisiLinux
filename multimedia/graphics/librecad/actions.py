d850b2b15812c2256fd5d29674c360de0ddeada6
eguven <ertangvn@gmail.com>
2013-05-08 18:41:47 +0300

    librecad fixed

--- multimedia/graphics/librecad/actions.py
+++ multimedia/graphics/librecad/actions.py
@@ -6,6 +6,9 @@
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import qt4
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     qt4.configure()
