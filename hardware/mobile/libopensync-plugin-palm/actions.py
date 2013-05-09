a2c040ef080af4f0c5475da6b312bd4a9d62224a
circass <erdincgultekin@gmail.com>
2013-05-09 23:57:41 +0300

    libopensync-plugin-palm 0.22

--- hardware/mobile/libopensync-plugin-palm/actions.py
+++ hardware/mobile/libopensync-plugin-palm/actions.py
@@ -4,21 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    cmaketools.configure(sourceDir="..")
-
+    autotools.autoreconf("-fi")
+    autotools.configure()
 
 def build():
-    shelltools.cd("build")
-    cmaketools.make()
+    autotools.make()
 
 def install():
-    shelltools.cd("build")
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING")
