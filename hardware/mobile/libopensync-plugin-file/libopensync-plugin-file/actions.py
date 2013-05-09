7fddebabcf6bda8576aeeea0c819c34d80662581
circass <erdincgultekin@gmail.com>
2013-05-09 23:45:36 +0300

    libopensync-plugin-file 0.22

--- hardware/mobile/libopensync-plugin-file/libopensync-plugin-file/actions.py
+++ hardware/mobile/libopensync-plugin-file/libopensync-plugin-file/actions.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# Copyright 2007-2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
