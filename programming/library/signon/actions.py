d2db110bd01681956e380855b0ba2eebce53680e
namso-01 <osman.erkan@yandex.com>
2013-05-11 14:08:49 +0300

    signon fixed

--- programming/library/signon/actions.py
+++ programming/library/signon/actions.py
@@ -6,7 +6,10 @@
 
 from pisi.actionsapi import qt4
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     qt4.configure(parameters="LIBDIR=/usr/lib")
