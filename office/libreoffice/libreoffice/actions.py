87dff24c5b23752e1f0f24d85d458100422fbdb2
aydemir <yusuf.aydemir@istanbul.com>
2013-05-11 18:01:47 +0300

    libreoffice add buildep libjpeg-turbo

--- office/libreoffice/libreoffice/actions.py
+++ office/libreoffice/libreoffice/actions.py
@@ -56,6 +56,7 @@ def setup():
                        --libdir=/usr/lib --mandir=/usr/share/man \
                        --enable-release-build \
                        --enable-verbose \
+                       --enable-split-app-modules \
                        --disable-dependency-tracking \
                        --disable-rpath \
                        --disable-crashdump \
