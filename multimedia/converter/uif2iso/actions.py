fae4ca3c7432c986afeb87fd1746994b28837b32
namso-01 <osman.erkan@yandex.com>
2013-05-11 21:32:53 +0300

    uif2iso fixed

--- multimedia/converter/uif2iso/actions.py
+++ multimedia/converter/uif2iso/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
-WorkDir = "."
-
 def build():
     shelltools.cd("src")
     autotools.make()
