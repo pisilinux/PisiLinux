22724cd6ef681752250c227cccc7a8536842c51a
namso-01 <osman.erkan@yandex.com>
2013-05-13 20:29:30 +0300

    pidgin-sip fixed

--- network/plugin/pidgin-sipe/actions.py
+++ network/plugin/pidgin-sipe/actions.py
@@ -6,6 +6,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     autotools.autoreconf("-vfi")
@@ -15,6 +16,6 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
