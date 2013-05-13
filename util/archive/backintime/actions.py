f8364e7fdfd4dde2ce7c0a9dcfc89a49a9601378
namso-01 <osman.erkan@yandex.com>
2013-05-13 22:25:38 +0300

    backintime fixed

--- util/archive/backintime/actions.py
+++ util/archive/backintime/actions.py
@@ -12,11 +12,23 @@ from pisi.actionsapi import get
 def setup():
     shelltools.cd("kde4") 
     autotools.configure()
+    shelltools.cd("..")
+    shelltools.cd("common") 
+    autotools.configure()
+    shelltools.cd("..")
 
 def build():
     shelltools.cd("kde4")
     autotools.make()
+    shelltools.cd("..")
+    shelltools.cd("common")
+    autotools.make()
+    shelltools.cd("..")
 
 def install(): 
     shelltools.cd("kde4")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
+    shelltools.cd("common")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
