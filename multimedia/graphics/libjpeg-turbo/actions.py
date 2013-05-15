378903b378cc8b6763972bbf1c2c6325af6e949c
marcin- <marcin.bojara@gmail.com>
2013-05-15 19:53:31 +0200

    Switching from jpeg to libjpeg-turbo

--- multimedia/graphics/libjpeg-turbo/actions.py
+++ multimedia/graphics/libjpeg-turbo/actions.py
@@ -8,20 +8,20 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    if not get.buildTYPE() == "emul32":
+        autotools.configure("--with-jpeg8")
+    else:
+        pisitools.dosed("configure", "(NAFLAGS='-fel)f64( -DELF -D__x86_64__)", "\\1f32\\2")
+        autotools.configure("--with-jpeg8 --without-simd")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    if get.buildTYPE() == "emul32": return
 
-    pisitools.insinto("/usr/share/doc/libjpeg-turbo/", "ChangeLog.txt")
-    pisitools.insinto("/usr/share/doc/libjpeg-turbo/", "README*")
-    #jpeg-devel confilicts
-    pisitools.remove("/usr/share/doc/README")
-    pisitools.remove("/usr/include/jerror.h")
-    pisitools.remove("/usr/include/jconfig.h")
-    pisitools.remove("/usr/include/jpeglib.h")
-    pisitools.remove("/usr/include/jmorecfg.h")
+    pisitools.dodoc("ChangeLog.txt", "README*")
 
+    # provide jpegint.h as it is required by various software
+    pisitools.insinto("/usr/lib/include", "jpegint.h")
