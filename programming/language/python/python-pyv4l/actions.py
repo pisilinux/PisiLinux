92a8263ebea720615bcacb51647cc66b8e81f15f
eguven <ertangvn@gmail.com>
2013-05-18 19:54:53 +0300

    actions.py cosmetic

--- programming/language/python/python-pyv4l/actions.py
+++ programming/language/python/python-pyv4l/actions.py
@@ -5,7 +5,6 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
