b0fa4fb75006b520ba187f780734164e901ccfe9
marcin- <marcin.bojara@gmail.com>
2013-05-18 22:51:14 +0200

    k3d: temporary K3D_BUILD_ODE_MODULE=OFF

--- multimedia/editor/k3d/actions.py
+++ multimedia/editor/k3d/actions.py
@@ -10,7 +10,8 @@ def setup():
     shelltools.makedirs("build")
     shelltools.cd("build")
     cmaketools.configure("-DPYTHON_INCLUDE_DIR=/usr/include/python2.7 \
-                          -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so", sourceDir = "..")
+                          -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so \
+                          -DK3D_BUILD_ODE_MODULE=OFF", sourceDir = "..")
 
 def build():
     shelltools.cd("build")
