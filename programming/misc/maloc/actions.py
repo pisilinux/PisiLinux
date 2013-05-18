5ef72fd3640f74bc04d48b565420586379fb49e9
namso-01 <osman.erkan@yandex.com>
2013-05-18 13:57:42 +0300

    malloc for apbs

---
+++ programming/misc/maloc/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+	autotools.autoreconf("-vif")
+	autotools.autoconf()
+	libtools.libtoolize("--copy --force")
+	libtools.libtoolize("--automake")
+	autotools.aclocal("-I m4")
+	autotools.autoheader()
+	autotools.configure("--disable-static \
+						 --docdir=/usr/share/doc/maloc")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("doc/*txt")
