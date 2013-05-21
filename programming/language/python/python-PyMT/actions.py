d21a8d54f848fbca6ebd670a34cba9151c6e881d
namso-01 <osman.erkan@yandex.com>
2013-05-21 20:56:22 +0300

    python-pymt error fixed

--- programming/language/python/python-PyMT/actions.py
+++ programming/language/python/python-PyMT/actions.py
@@ -5,11 +5,11 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-
-WorkDir = "pymt"
+from pisi.actionsapi import pisitools
 
 def build():
-    pythonmodules.compile()
+	pisitools.dosed("pymt/c_ext/c_graphics.pyx", "self._indices = ", "self._indices = <bytes>")
+	pythonmodules.compile()
 
 def install():
     pythonmodules.install()
