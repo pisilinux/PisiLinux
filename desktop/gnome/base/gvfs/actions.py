ef45173e08fd5cfef3cc0825949e2fc81b7cfb07
marcin- <marcin.bojara@gmail.com>
2013-05-21 19:08:03 +0200

    gvfs: version bump

--- desktop/gnome/base/gvfs/actions.py
+++ desktop/gnome/base/gvfs/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fiv")
+#    autotools.autoreconf("-fiv")
     autotools.configure("--disable-static \
                          --enable-keyring \
                          --enable-bash-completion \
