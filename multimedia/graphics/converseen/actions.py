62b3847260687a04ff5035b6f1552600d0a30f06
namso-01 <osman.erkan@yandex.com>
2013-05-22 22:45:59 +0300

    converseen v.bump fixed

--- multimedia/graphics/converseen/actions.py
+++ multimedia/graphics/converseen/actions.py
@@ -8,11 +8,17 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import cmaketools
 
 def setup():
-    cmaketools.configure()
+    cmaketools.configure("-DImageMagick_Magick++_INCLUDE_DIR=/usr/include/ImageMagick-6 \
+						  -DImageMagick_MagickCore_INCLUDE_DIR=/usr/include/ImageMagick-6 \
+						  -DImageMagick_MagickWand_INCLUDE_DIR=/usr/include/ImageMagick-6 \
+						  -DImageMagick_Magick++_LIBRARY=/usr/lib/libMagick++-6.Q16HDRI.so \
+						  -DImageMagick_MagickCore_LIBRARY=/usr/lib/libMagickCore-6.Q16HDRI.so \
+						  -DImageMagick_MagickWand_LIBRARY=/usr/lib/libMagickWand-6.Q16HDRI.so")
     
 def build():
     cmaketools.make()
 
 def install():
     cmaketools.install()
-    pisitools.dodoc("COPYING", "INSTALL", "README*")
+    
+    pisitools.dodoc("COPYING", "INSTALL")
