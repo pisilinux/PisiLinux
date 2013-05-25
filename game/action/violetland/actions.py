5df58cb016f0673b6ac976fcc3be1370f9459aee
namso-01 <osman.erkan@yandex.com>
2013-05-25 14:56:21 +0300

    violetland fixed

--- game/action/violetland/actions.py
+++ game/action/violetland/actions.py
@@ -6,14 +6,13 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import cmaketools
 
-WorkDir = "violetland-v0.4.3"
-
 def setup():
-	cmaketools.configure("--prefix=/usr -DOPENGL_INCLUDE_DIR=/usr/include/GL")
+    cmaketools.configure("--prefix=/usr -DOPENGL_INCLUDE_DIR=/usr/include/GL")
 
 def build():
 	autotools.make()
 
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	
 	pisitools.dodoc("README_EN.TXT","README_RU.TXT","CHANGELOG")
