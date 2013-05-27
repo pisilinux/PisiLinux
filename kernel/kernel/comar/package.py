024c96ef0bc31582bec7b949d7a20cd1a47af7ba
marcin- <marcin.bojara@gmail.com>
2013-05-27 21:59:11 +0200

    kernel: fix comar script

--- kernel/kernel/comar/package.py
+++ kernel/kernel/comar/package.py
@@ -14,33 +14,35 @@ def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     gcf = "/boot/grub2/grub.cfg"
     cfg = read_file(gcf)
     ogp = []
-    for line in cfg.split("\n"):
-        if line.startswith("\tlinux"):
-            ogp = line.split(" ro ")[1].split()
-            break
+    if os.path.isfile(gcf):
+        for line in cfg.split("\n"):
+            if line.startswith("\tlinux"):
+                ogp = line.split(" ro ")[1].split()
+                break
 
     gdf = "/etc/default/grub"
     gdc = read_file(gdf)
     new = []
     gcl = []
     gcld = []
-    for line in gdc.split("\n"):
-        if line.startswith("GRUB_CMDLINE_LINUX_DEFAULT"): gcld = line.split("DEFAULT=")[1][1:-1].split()
-        elif "GRUB_CMDLINE_LINUX=" in line:
-            gcl = line.split("LINUX=")[1][1:-1].split()
-            # remove default options
-            for p in gcld: 
-                if p in ogp: ogp.remove(p)
-            # add options if assigned to GRUB_CMDLINE_LINUX
-            if not line.startswith("#"):
-                for p in gcl: 
-                    if not p in ogp: ogp.append(p)
-            line = 'GRUB_CMDLINE_LINUX="%s"' % " ".join(ogp)
-        new.append(line)
+    if os.path.isfile(gdf):
+        for line in gdc.split("\n"):
+            if line.startswith("GRUB_CMDLINE_LINUX_DEFAULT"): gcld = line.split("DEFAULT=")[1][1:-1].split()
+            elif "GRUB_CMDLINE_LINUX=" in line:
+                gcl = line.split("LINUX=")[1][1:-1].split()
+                # remove default options
+                for p in gcld: 
+                    if p in ogp: ogp.remove(p)
+                # add options if assigned to GRUB_CMDLINE_LINUX
+                if not line.startswith("#"):
+                    for p in gcl: 
+                        if not p in ogp: ogp.append(p)
+                line = 'GRUB_CMDLINE_LINUX="%s"' % " ".join(ogp)
+            new.append(line)
+    
+        write_file(gdf, "\n".join(new))
 
-    write_file(gdf, "\n".join(new))
-
-    os.environ["LANG"] = read_file("/etc/mudur/locale").split("\n")[0]
+    if os.path.isfile("/etc/mudur/locale"): os.environ["LANG"] = read_file("/etc/mudur/locale").split("\n")[0]
     os.environ["PATH"] = "/usr/sbin:/usr/bin:/sbin:/bin"
     os.system("grub2-mkconfig -o %s" % gcf)
 
