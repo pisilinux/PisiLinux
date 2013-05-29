bd4e0882c09c15a974ee94b6ec232e969378b263
namso-01 <osman.erkan@yandex.com>
2013-05-29 17:27:08 +0300

    editra confilicts

--- editor/Editra/actions.py
+++ editor/Editra/actions.py
@@ -4,10 +4,12 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-
+from pisi.actionsapi import pisitools
 
 def build():
     pythonmodules.compile()
 
 def install():
     pythonmodules.install()
+    
+    pisitools.rename("/usr/bin/editra", "Editra")
