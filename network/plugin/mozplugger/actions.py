da7b0735e5bd70ff7ab59b0c8df9df6283e58192
erdinc <erdincgultekin@gmail.com>
2013-06-05 00:00:23 +0300

    mozplugger fix up

--- network/plugin/mozplugger/actions.py
+++ network/plugin/mozplugger/actions.py
@@ -5,10 +5,22 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+def setup():
+    autotools.configure("--enable-debug")
+
 def build():
-    autotools.make("linux")
+    autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s root=%s" % (get.installDIR(), get.installDIR()))
+    pisitools.insinto("/etc", "mozpluggerrc")    
+    pisitools.insinto("/usr/bin", "mozplugger-controller")
+    pisitools.insinto("/usr/bin", "mozplugger-helper")
+    pisitools.insinto("/usr/bin", "mozplugger-linker")
+    pisitools.insinto("/usr/lib/mozilla/plugins", "mozplugger.so")
+    pisitools.insinto("/usr/share/man/man7", "mozplugger.7")
+
+    pisitools.dodoc("COPYING")
