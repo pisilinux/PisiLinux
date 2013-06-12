0c778467bdf533abc3f9df22fe25e8bf7df85ab6
marcin- <marcin.bojara@gmail.com>
2013-06-12 17:17:12 +0200

    yali: fix progressbar

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -12,6 +12,9 @@ def setup():
     # change slider direction
     pisitools.dosed("yali/gui/Ui/", "RightToLeft", "LeftToRight", namePattern="^(logicalvolume|partition)\..*")
 
+    # let progressbar starts from 0
+    pisitools.dosed("yali/gui/Ui/installprogress.ui", ">65<", ">0<")
+
     repo_uri = "http://packages.pisilinux.org/repositories/1.0/testing/64/pisi-index.xml.xz" # FIXME
     pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
     pisitools.dosed("yali/constants.py", "@REPO_NAME@", "pisi") # FIXME
