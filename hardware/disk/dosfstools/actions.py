d22db6539212565b98eebe3f57fb80fa4a05b9bb
marcin- <marcin.bojara@gmail.com>
2013-06-13 15:22:42 +0200

    dosfstools: fix version number, version bump

--- hardware/disk/dosfstools/actions.py
+++ hardware/disk/dosfstools/actions.py
@@ -4,18 +4,11 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-WorkDir = "dosfstools-3.0.13"
-
+from pisi.actionsapi import autotools
 
 def build():
     autotools.make("CFLAGS='%s -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fno-strict-aliasing'" % get.CFLAGS())
 
 def install():
     autotools.rawInstall("DESTDIR=%s install-bin install-man PREFIX=%s SBINDIR=/sbin" % (get.installDIR(), get.defaultprefixDIR()))
-
-    #pisitools.dodoc("COPYING", "ChangeLog", "doc/*")
