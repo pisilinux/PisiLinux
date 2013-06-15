0f8438239c800d6ca2603426ee6c7e5b7cd12279
namso-01 <namso-01@hotmail.it>
2013-06-15 13:57:22 +0300

    celestia fixed

--- science/astronomy/celestia/actions.py
+++ science/astronomy/celestia/actions.py
@@ -10,19 +10,32 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vif")
+    shelltools.export("CFLAGS", "%s -fsigned-char" % get.CFLAGS())
+    shelltools.export("CXXFLAGS", "%s -fsigned-char" % get.CXXFLAGS())
+    autotools.autoreconf("-fi")
     autotools.configure("--with-gtk \
-			 --with-pic \
-			 --with-qt \
-			 --enable-pch \
-			 --disable-rpath \
-			 --enable-theora \
-			 --enable-cairo \
-			 --without-arts \
-			 --with-lua")
+                         --with-pic \
+                         --with-qt \
+                         --enable-pch \
+                         --disable-rpath \
+                         --enable-theora \
+                         --enable-cairo \
+                         --without-arts \
+                         --with-lua")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall('DESTDIR=%s' % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s MKDIR_P='mkdir -p'" % get.installDIR())
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    #pisitools.insinto("/usr/bin/", "src/celestia/kde/data/celestia.desktop")
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    #pisitools.insinto("/usr/bin/", "src/celestia/celestia")
+    
+    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "COPYING", "README", "TRANSLATORS", "locale/COPYING_*")  
+    
