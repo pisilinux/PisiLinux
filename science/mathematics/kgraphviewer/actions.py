8f143ad8271d9d7cb35a2b3e3453df0e9bef7176
marcin- <marcin.bojara@gmail.com>
2013-06-15 17:28:33 +0200

    kgraphviewer: fix build

--- science/mathematics/kgraphviewer/actions.py
+++ science/mathematics/kgraphviewer/actions.py
@@ -4,12 +4,11 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import kde4
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
 
-#WorkDir = "%s-%s-kde4.3.3" % (get.srcNAME(), get.srcVERSION())
 NoStrip=["/usr/share"]
 
 def setup():
