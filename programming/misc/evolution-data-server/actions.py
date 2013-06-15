7b9cbd08abce331b34b51f362e2450b7795f7e36
ademirel <aydin@demirel.web.tr>
2013-06-16 00:33:05 +0300

    evolution-data-server: version bump

--- programming/misc/evolution-data-server/actions.py
+++ programming/misc/evolution-data-server/actions.py
@@ -7,18 +7,31 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 shelltools.export("CFLAGS", "%s -fPIC -fno-strict-aliasing" % get.CFLAGS())
 
 def setup():
+    autotools.aclocal("-I m4")
+    autotools.autoheader()
+    libtools.libtoolize()
+    shelltools.system("intltoolize --force --copy --automake")
     autotools.autoreconf("-fi")
     autotools.configure("--with-openldap=yes \
                          --enable-smime=yes \
-                         --enable-gnome-keyring=yes \
+                         --program-prefix= --disable-dependency-tracking \
+                         --disable-goa \
+                         --enable-nss=yes \
+                         --enable-dot-locking=no \
+                         --enable-file-locking=fcntl \
+                         --disable-maintainer-mode \
+                         --enable-nntp=yes \
+                         --enable-vala-bindings=yes \
+                         --enable-introspection=yes \
+                         --disable-weather \
                          --with-krb5=/usr \
-                         --with-libdb=/usr \
-                         --enable-calendar=yes")
+                         --with-libdb=/usr ")
 
 def build():
     autotools.make()
