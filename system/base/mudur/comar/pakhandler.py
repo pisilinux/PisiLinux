f849fba443a2a6c1583f79d249b0787d10641bd9
marcin- <marcin.bojara@gmail.com>
2013-06-26 21:02:23 +0200

    mudur: Fix migration from /var/run to /run

--- system/base/mudur/comar/pakhandler.py
+++ system/base/mudur/comar/pakhandler.py
@@ -1,19 +1,47 @@
 # -*- coding: utf-8 -*-
 
 import piksemel
+import stat
 import imp
+import os
 
-def doenv(filepath):
-    doc = piksemel.parse(filepath)
-    for item in doc.tags("File"):
+def doenv(filetag):
+    for item in filetag:
         path = item.getTagData("Path")
         if path.startswith("etc/env.d"):
             updenv = imp.load_source("updenv", "/sbin/update-environment")
             updenv.update_environment("/")
             return
 
+def docopy(filetag):
+    if not os.path.exists("/var/rundirs"): os.makedirs("/var/rundirs")
+    for item in filetag:
+        path = item.getTagData("Path")
+        if path.startswith("var/run/"): path = path[4:]
+        if path.startswith("run/"):
+            if not os.path.exists("/var/rundirs/%s" % path[4:]): os.makedirs("/var/rundirs/%s" % path[4:])
+            subpath = ""
+            for d in path.split("/")[1:]:
+                subpath = os.path.join(subpath, d)
+                s = os.stat("/run/%s" % subpath)
+                os.chmod("/var/rundirs/%s" % subpath, stat.S_IMODE(s.st_mode))
+                os.chown("/var/rundirs/%s" % subpath, s.st_uid, s.st_gid)
+
+def doclean(filetag):
+    for item in filetag:
+        path = item.getTagData("Path")
+        if path.startswith("var/run/"): path = path[4:]
+        if path.startswith("run/"):
+            if os.path.exists("/var/rundirs/%s" % path[4:]):
+                os.removedirs("/var/rundirs/%s" % path[4:])
+
+
 def setupPackage(metapath, filepath):
-    doenv(filepath)
+    doc = piksemel.parse(filepath)
+    doenv(doc.tags("File"))
+    docopy(doc.tags("File"))
 
 def postCleanupPackage(metapath, filepath):
-    doenv(filepath)
+    doc = piksemel.parse(filepath)
+    doenv(doc.tags("File"))
+    doclean(doc.tags("File"))
