d0361c9e6c91f2c28627dd8e56fd40129b79e57a
marcin- <marcin.bojara@gmail.com>
2013-07-02 22:25:37 +0200

    eclipse: fix unpack

--- programming/environment/eclipse/eclipse/actions.py
+++ programming/environment/eclipse/eclipse/actions.py
@@ -4,11 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = get.ARCH()
 
 def install():
+    shelltools.system("tar -xf *.tar")
     pisitools.dodir("/opt")
     pisitools.insinto("/opt", "eclipse")
