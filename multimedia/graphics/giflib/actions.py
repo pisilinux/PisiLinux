c0387bfd387e7a87cfb7b7aebc49ae3f20a08c58
marcin- <marcin.bojara@gmail.com>
2013-07-06 23:56:54 +0200

    giflib: version bump

--- multimedia/graphics/giflib/actions.py
+++ multimedia/graphics/giflib/actions.py
@@ -4,22 +4,27 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
+from pisi.actionsapi import libtools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sed -i -e '/AC_PATH_XTRA/d' -e 's/AM_CONFIG_HEADER/AC_CONFIG_HEADER/' configure.ac")
     libtools.libtoolize("--force --install")
     autotools.autoreconf("-fi")
-    
+
     autotools.configure("--with-x \
                          --disable-gl \
                          --disable-static")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
+
+    # libungif compatibility - instructions taken from Redhat specfile
+    MAJOR=get.srcVERSION().split(".")[0]
+    shelltools.system('%s -shared -Wl,-soname,libungif.so.%s -Llib/.libs -lgif -o libungif.so.%s' % (get.CC(), MAJOR, get.srcVERSION()))
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
