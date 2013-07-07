825697c052ef776468b65f18f9f0d18cb2fb18ab
marcin- <marcin.bojara@gmail.com>
2013-07-07 08:54:07 +0200

    libcdio-paranoia: new package

---
+++ hardware/optical/libcdio-paranoia/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    pisitools.dosed("configure.ac", "AM_CONFIG_HEADER", "AC_CONFIG_HEADERS")
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static \
+                         --disable-example-progs \
+                         --enable-cpp-progs \
+                         --disable-rpath \
+                         --with-cd-paranoia-name=libcdio-paranoia")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % (get.installDIR()))
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README", "THANKS")
