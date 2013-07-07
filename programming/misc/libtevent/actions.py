3e759d250d25682f1d3419c6df6530b615dc4a6d
marcin- <marcin.bojara@gmail.com>
2013-07-07 21:04:59 +0200

    libtevent: version bump

--- programming/misc/libtevent/actions.py
+++ programming/misc/libtevent/actions.py
@@ -4,23 +4,23 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
-WorkDir = "tevent-%s" % get.srcVERSION()
+jobs = get.makeJOBS().replace("-j", "")
 
 def setup():
     autotools.configure()
 
 def build():
-    autotools.make()
+    autotools.make("JOBS=%s" % jobs)
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s JOBS=%s" % (get.installDIR(), jobs))
 
-    pisitools.remove("/usr/lib/*.a")
+#    pisitools.remove("/usr/lib/*.a")
 
     # Create symlinks for so file
-    pisitools.dosym("libtevent.so.%s" % get.srcVERSION(), "/usr/lib/libtevent.so.%s" % get.srcVERSION().split(".")[0])
-    pisitools.dosym("libtevent.so.%s" % get.srcVERSION(), "/usr/lib/libtevent.so")
+#    pisitools.dosym("libtevent.so.%s" % get.srcVERSION(), "/usr/lib/libtevent.so.%s" % get.srcVERSION().split(".")[0])
+#    pisitools.dosym("libtevent.so.%s" % get.srcVERSION(), "/usr/lib/libtevent.so")
