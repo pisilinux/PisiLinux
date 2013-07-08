0b0dde4c69299e6c3e50a0cbdc201d8a0909b5cc
marcin- <marcin.bojara@gmail.com>
2013-07-08 16:03:29 +0200

    deadbeef: rebuild for libcdio-0.90

--- multimedia/sound/deadbeef/actions.py
+++ multimedia/sound/deadbeef/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static --disable-rpath")
 
 def build():
     autotools.make()
