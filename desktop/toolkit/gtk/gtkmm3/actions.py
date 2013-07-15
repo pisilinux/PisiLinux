900a94d524f3711de1509ea6585ad3b100c34153
marcin- <marcin.bojara@gmail.com>
2013-07-15 18:30:24 +0200

    gtkmm3: version bump

--- desktop/toolkit/gtk/gtkmm3/actions.py
+++ desktop/toolkit/gtk/gtkmm3/actions.py
@@ -8,8 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-demos \
-                         --disable-docs")
+    autotools.configure()
     pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
 def build():
