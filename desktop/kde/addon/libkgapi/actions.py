83f2e0be80213dee04643ac26fbc79788e33c019
aydemir <yusuf.aydemir@istanbul.com>
2013-07-21 22:12:54 +0300

    libkgapi

--- desktop/kde/addon/libkgapi/actions.py
+++ desktop/kde/addon/libkgapi/actions.py
@@ -17,4 +17,4 @@ def build():
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("CHANGELOG", "LICENSE", "README")
+    pisitools.dodoc("LICENSE", "README")
