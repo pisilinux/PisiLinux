7a379c2498f7691d4f755d4f5211b79795c3aa3b
marcin- <marcin.bojara@gmail.com>
2013-07-28 10:03:35 +0200

    spidermonkey: version bump

--- system/base/spidermonkey/actions.py
+++ system/base/spidermonkey/actions.py
@@ -3,12 +3,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
-WorkDir = "js-1.8.5/js/src"
+WorkDir = "mozjs%s/js/src" % get.srcVERSION()
 
 def setup():
    autotools.configure("--enable-jemalloc \
