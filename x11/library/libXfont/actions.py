b19273ef2bceb602add606afb7b66d79fabf4fba
marcin- <marcin.bojara@gmail.com>
2013-07-30 17:21:11 +0200

    libXfont: version bump

--- x11/library/libXfont/actions.py
+++ x11/library/libXfont/actions.py
@@ -11,6 +11,9 @@ def setup():
     autotools.configure("--disable-static \
                          --disable-devel-docs \
                          --without-xmlto")
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
