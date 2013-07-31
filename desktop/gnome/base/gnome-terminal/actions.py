7b4a39ba18e3e79999d761d9a63214c8e2a420a5
marcin- <marcin.bojara@gmail.com>
2013-07-31 22:24:45 +0200

    gnome-terminal: version bump

--- desktop/gnome/base/gnome-terminal/actions.py
+++ desktop/gnome/base/gnome-terminal/actions.py
@@ -4,15 +4,18 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 def setup():
     autotools.configure("--disable-static\
                          --disable-scrollkeeper\
                          --disable-schemas-install")
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
@@ -20,4 +23,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS")
