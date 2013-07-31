1de8e30d8ca3b0b7daaaee9c5197629d27aab0d1
marcin- <marcin.bojara@gmail.com>
2013-07-31 21:46:14 +0200

    gnome-menus: rebuild

--- desktop/gnome/base/gnome-menus/actions.py
+++ desktop/gnome/base/gnome-menus/actions.py
@@ -13,14 +13,17 @@ shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.configure("--disable-static")
-    
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
+
     pisitools.dodir("/etc/xdg/gnome")
     pisitools.domove("/etc/xdg/menus", "/etc/xdg/gnome")
-    
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
