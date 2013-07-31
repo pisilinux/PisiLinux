12258e72248bb7b167f19d269bb2de3b73f7537e
marcin- <marcin.bojara@gmail.com>
2013-07-31 20:23:18 +0200

    gcr: version bump

--- desktop/gnome/base/gcr/actions.py
+++ desktop/gnome/base/gcr/actions.py
@@ -12,17 +12,20 @@ from pisi.actionsapi import get
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure("--prefix=/usr \
-			 --sysconfdir=/etc \
-			 --libexecdir=/usr/lib/gnome-keyring")
+    autotools.configure("\
+                         --disable-static \
+                         --disable-update-mime \
+                         --disable-schemas-compile \
+                         --enable-introspection \
+                        ")
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.remove("/usr/share/glib-2.0/schemas/org.gnome.crypto.pgp.gschema.xml")
-    pisitools.remove("/usr/share/GConf/gsettings/org.gnome.crypto.pgp.convert")
 
     pisitools.dodoc("AUTHORS", "ChangeLog","COPYING", "README", "NEWS")
