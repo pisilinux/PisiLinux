831d54d16df52a38b0b22adb1da317fe6ab03923
marcin- <marcin.bojara@gmail.com>
2013-07-31 22:36:39 +0200

    gvfs: rebuild

--- desktop/gnome/base/gvfs/actions.py
+++ desktop/gnome/base/gvfs/actions.py
@@ -22,6 +22,9 @@ def setup():
                          --enable-gtk \
                          --enable-udisks2 \
                          --with-dbus-service-dir=/usr/share/dbus-1/services")
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
