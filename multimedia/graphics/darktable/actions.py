17433f90c58a1e0add04d31070abd27a0d8b310b
marcin- <marcin.bojara@gmail.com>
2013-08-01 08:32:38 +0200

    darktable: fix build

--- multimedia/graphics/darktable/actions.py
+++ multimedia/graphics/darktable/actions.py
@@ -6,14 +6,19 @@
 
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    pisitools.ldflags("+lgs")
     shelltools.makedirs("build")
     shelltools.cd("build")
     cmaketools.configure("-DDONT_INSTALL_GCONF_SCHEMAS=1 \
                           -DCMAKE_BUILD_TYPE=Release \
                           -DBINARY_PACKAGE_BUILD=1 \
+                          -DCUSTOM_CFLAGS=ON \
+                          -DINSTALL_IOP_EXPERIMENTAL=ON \
+                          -DINSTALL_IOP_LEGACY=ON \
                           -DBUILD_USERMANUAL=0", sourceDir="..")
 
 def build():
