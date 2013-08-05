cafaf93b53ad14679e395bfe1e72aaf3ba58433b
namso-01 <namso-01@hotmail.it>
2013-08-05 22:40:48 +0300

    qtractor new package

---
+++ multimedia/editor/qtractor/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-lilv \
+                         --enable-suil  \
+                         --enable-librubberband \
+                         --enable-libsamplerate \
+                         --enable-lv2 \
+                         --enable-nsm \
+                         --enable-dssi \
+                         --enable-vestige \
+                         --enable-ladspa \
+                         --enable-liblo \
+                         --localedir=/usr/share/locale")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "AUTHORS", "TODO", "ChangeLog")
