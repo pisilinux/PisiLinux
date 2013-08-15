9c2b50f8a9d7bd304ed82abdb5712d6b8308c54d
marcin- <marcin.bojara@gmail.com>
2013-08-15 21:44:25 +0200

    xfconf: fix build

--- desktop/xfce/base/xfconf/actions.py
+++ desktop/xfce/base/xfconf/actions.py
@@ -6,11 +6,16 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import perlmodules
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system('xdt-autogen')
     autotools.configure('--disable-static')
+
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
 
 def build():
     autotools.make()
