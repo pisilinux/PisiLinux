0a834af87614060ef9207037abe69fed949fd72f
marcin- <marcin.bojara@gmail.com>
2013-08-15 21:47:58 +0200

    xfconf: fix build

--- desktop/xfce/base/xfconf/actions.py
+++ desktop/xfce/base/xfconf/actions.py
@@ -16,6 +16,7 @@ def setup():
 
     pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
     pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
