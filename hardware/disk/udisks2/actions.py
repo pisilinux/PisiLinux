493cbdc9ceaf3fb9b2db804ab2d18b88630551f2
namso-01 <namso-01@hotmail.it>
2013-08-15 21:36:08 +0300

    dep fixed --rebuild: udisks2 v.bump

--- hardware/disk/udisks2/actions.py
+++ hardware/disk/udisks2/actions.py
@@ -9,20 +9,14 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-shelltools.export("HOME", get.workDIR())
-
 def setup():
-    autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
-                         --disable-gtk-doc")
+                         --enable-gtk-doc")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.domove("/rules.d", "/lib/udev")
-
-    #shelltools.chmod("%s/etc/profile.d/*.sh" % get.installDIR(), mode=0644)
 
     pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
