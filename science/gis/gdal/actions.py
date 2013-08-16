a798d6c61834c83b59ad4962faf02e9884668232
marcin- <marcin.bojara@gmail.com>
2013-08-17 00:07:40 +0200

    gdal: revert to 1.9.2, fix build

--- science/gis/gdal/actions.py
+++ science/gis/gdal/actions.py
@@ -4,18 +4,31 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import get
+from pisi.actionsapi import libtools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import perlmodules
 
 def setup():
+    pisitools.dosed("ogr/ogrsf_frmts/sqlite/ogr_sqlite.h", "spatialite/sqlite3.h", "sqlite3.h")
     libtools.libtoolize()
     autotools.autoreconf("-vfi")
     autotools.aclocal()
-    shelltools.export("CFLAGS",get.CFLAGS().replace("-fno-strict-aliasing",""))
+    pisitools.cflags.add("-fno-strict-aliasing")
+    pisitools.dosed("configure", "-L\$with_cfitsio -L\$with_cfitsio/lib -lcfitsio", "-lcfitsio")
+    pisitools.dosed("configure", "-I\$with_cfitsio -I\$with_cfitsio/include", "-I\$with_cfitsio/include/cfitsio")
+    pisitools.dosed("configure", "-lnetcdf -L\$with_netcdf -L\$with_netcdf/lib \$LIBS", "-lnetcdf $LIBS")
+    pisitools.dosed("configure", "-L\$DODS_LIB -ldap\+\+", "-ldap++")
+    pisitools.dosed("configure", "-L\$with_ogdi -L\$with_ogdi/lib -logdi", "-logdi")
+    pisitools.dosed("configure", "-L\$with_jpeg -L\$with_jpeg/lib -ljpeg", "-ljpeg")
+    pisitools.dosed("configure", "-L\$with_libtiff\/lib -ltiff", "-ltiff")
+    pisitools.dosed("configure", "-lgeotiff -L\$with_geotiff \$LIBS", "-lgeotiff $LIBS")
+    pisitools.dosed("configure", "-L\$with_geotiff\/lib -lgeotiff \$LIBS", "-lgeotiff $LIBS")
+    pisitools.dosed("ogr/ogrct.cpp", "libproj.so", "libproj.so.0")
     autotools.configure("--disable-static \
+                         --enable-shared \
                          --datadir=/usr/share/gdal \
                          --with-ogdi \
                          --with-threads \
@@ -27,16 +40,23 @@ def setup():
                          --with-netcdf \
                          --with-png \
                          --with-geos \
-                         --with-mysql \
+                         --without-mysql \
                          --with-curl \
                          --with-perl \
                          --with-jpeg \
                          --with-jpeg12=no \
                          --with-libtiff \
                          --with-sqlite3 \
-                         --with-geotiff \
-                         --with-podofo \
+                         --with-geotiff=external \
+                         --without-podofo \
                          --with-spatialite \
+                         --with-ogr \
+                         --with-grib \
+                         --with-curl \
+                         --with-webp \
+                         --with-poppler \
+                         --with-xerces \
+                         --without-openjpeg \
                          --without-libtool \
                          --without-hdf4 \
                          --without-fme \
@@ -55,9 +75,16 @@ def setup():
                          --without-idb \
                          --without-sde \
                          --without-ruby")
+    shelltools.cd("swig/perl")
+    perlmodules.configure()
+    pisitools.dosed("Makefile_*"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
+    shelltools.cd("swig/perl")
+    perlmodules.make()
+    shelltools.cd("../..")
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    perlmodules.removePodfiles()
