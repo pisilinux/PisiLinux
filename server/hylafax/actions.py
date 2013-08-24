440942e8f648b6f4afe96dd732435c133714079b
obsoleteman <tulliana@gmail.com>
2013-08-24 14:31:36 +0300

    hylafax: -j1 not needed, fix deps

--- server/hylafax/actions.py
+++ server/hylafax/actions.py
@@ -39,7 +39,7 @@ def setup():
                             --with-PAGESIZE=A4" % (get.srcNAME(), get.curDIR(), get.CFLAGS()))
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     pisitools.dodir("/usr/bin")
