07429d4b1a2fa866c220a4d02f95e1765461ec25
marcin- <marcin.bojara@gmail.com>
2013-08-24 23:05:11 +0200

    module-fglrx: fix libGL name

--- kernel/drivers/module-fglrx/comar/package.py
+++ kernel/drivers/module-fglrx/comar/package.py
@@ -6,7 +6,7 @@ libdir = "/usr/lib/fglrx"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     os.system("/usr/sbin/alternatives \
-                --install   /usr/lib/libGL.so.1.2.0           libGL                   %(libdir)s/libGL.so.1.2.0     50 \
+                --install   /usr/lib/libGL.so.1.2.0           libGL                   %(libdir)s/libGL.so.1.2     50 \
                 --slave     /usr/lib/xorg/modules/volatile  xorg-modules-volatile   %(libdir)s/modules"
               % {"libdir": libdir})
 
