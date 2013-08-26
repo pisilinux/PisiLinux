e9dbcad4cf897d2255e3098834be0447fa1bd970
Aydın Demirel <aydin@demirel.web.tr>
2013-08-26 21:45:04 +0300

    gnome-session: added ldflag for gconf2

--- desktop/gnome/base/gnome-session/actions.py
+++ desktop/gnome/base/gnome-session/actions.py
@@ -6,9 +6,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.export("LDFLAGS","%s -lgconf-2" % get.LDFLAGS())
     autotools.configure("--disable-static")
 
 def build():
