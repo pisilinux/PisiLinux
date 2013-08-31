96bd30be62b9fd93e0f16fe92c6c17a8d38439b9
circass <erdincgultekin@gmail.com>
2013-08-31 21:24:21 +0300

    supertuxkart , have fun

--- game/sports/supertuxkart/actions.py
+++ game/sports/supertuxkart/actions.py
@@ -5,39 +5,23 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-import os
-
-enetsrc = "src/enet"
-
-def removegames(dest):
-    for root, dirs, files in os.walk(dest):
-        for name in files:
-            if name.startswith("Makefile"):
-                pisitools.dosed(os.path.join(root, name), "games/")
-
 def setup():
-    # FIXME: do it by hand until pisi actions api change is packaged
-    # for i in `find -name "Makefile*"|xargs grep games|sed 's/:.*//g' |cut -c3-|sort -u`;do sed -i -e 's/\/games//g' $i;done
-    # removegames("./")
-
-    if shelltools.isDirectory(enetsrc):
-        shelltools.unlinkDir(enetsrc)
-
-    shelltools.sym(".", "m4")
     autotools.autoreconf("-vfi")
-    autotools.configure()
+    shelltools.cd("lib/irrlicht/source/Irrlicht")
+    autotools.make("NDEBUG=1")
+    shelltools.cd("../../../../")
+    cmaketools.configure()
 
 def build():
-    autotools.make("V=1")
+    cmaketools.make("V=1 -j2")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    # pisitools.dobin("src/supertuxkart")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "README", "COPYING", "TODO")
 
