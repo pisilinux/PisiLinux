e0bfd8c020ddca49270d92ea914f1dfbe7a21e70
namso-01 <namso-01@hotmail.it>
2013-09-03 00:08:26 +0300

    libxvmc cosmetic fixed

--- x11/library/libXvMC/actions.py
+++ x11/library/libXvMC/actions.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
