970de0d394b03cfff07c82db09da0c711f5243b6
namso-01 <namso-01@hotmail.it>
2013-09-03 00:05:11 +0300

    libxvvmc added 32bit

--- x11/library/libXvMC/actions.py
+++ x11/library/libXvMC/actions.py
@@ -1,18 +1,38 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# Copyright 2010 TUBITAK/BILGEM
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "_emul32":
+        options += " --libdir=/usr/lib32"
+
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CC())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+        shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+        
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
-
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    if get.buildTYPE() == "emul32":
+        return
+    
     pisitools.dodoc("ChangeLog", "COPYING", "README")
