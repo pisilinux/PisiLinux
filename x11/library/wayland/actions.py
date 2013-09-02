dc4283274a5121969188f6d2d011c3145a462e95
namso-01 <namso-01@hotmail.it>
2013-09-02 10:59:11 +0300

    wayland new package

---
+++ x11/library/wayland/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/BILGEM
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-documentation --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    if get.buildTYPE() == "emul32":
+        return
+    
+    pisitools.dodoc("COPYING", "TODO", "README")
