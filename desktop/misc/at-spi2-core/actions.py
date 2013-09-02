c1e31032dca40bdcc25301d9bdc3852711b0fd2d
marcin- <marcin.bojara@gmail.com>
2013-09-02 21:09:18 +0200

    at-spi2-core: fix build

--- desktop/misc/at-spi2-core/actions.py
+++ desktop/misc/at-spi2-core/actions.py
@@ -4,19 +4,28 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
-libexec = "/usr/libexec/at-spi2" if get.buildTYPE() == "emul32" else "/tmp"
+if get.buildTYPE() == "emul32":
+    libexec = "/tmp"
+    sysconf = "/tmp"
+else:
+    libexec = "/usr/libexec/at-spi2"
+    sysconf = "/etc"
 
 def setup():
     autotools.configure("--disable-static \
                          --disable-xevie \
                          --libexecdir=%s\
                          --with-dbus-daemondir=/usr/bin \
-                        " % libexec)
+                         --sysconfdir=%s \
+                        " % (libexec, sysconf))
+
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool","( -shared )", " -Wl,--as-needed\\1")
 
 def build():
     autotools.make()
