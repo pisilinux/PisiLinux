427014bda14a22fc31ed4c2285c1f6c47de1a80d
obsoleteman <tulliana@gmail.com>
2013-09-03 12:51:54 +0300

    fix libXvMC emul32 splitting, pisi knows how to set environtemnt for emul32 builds

--- x11/library/libXvMC/actions.py
+++ x11/library/libXvMC/actions.py
@@ -4,34 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
-
 def setup():
-    options = "--disable-static"
-
-    if get.buildTYPE() == "_emul32":
-        options += " --libdir=/usr/lib32"
-
-        shelltools.export("CC", "%s -m32" % get.CC())
-        shelltools.export("CXX", "%s -m32" % get.CC())
-        shelltools.export("CXXFLAGS", "%s -m32" % get.CFLAGS())
-        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
-        shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
-        
-    autotools.configure(options)
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
-    if get.buildTYPE() == "emul32":
-        return
-    
+
     pisitools.dodoc("ChangeLog", "COPYING", "README")
