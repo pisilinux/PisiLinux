b302e38f927314f4637d67054a9a9e5e14aebb56
namso-01 <namso-01@hotmail.it>
2013-09-04 11:39:32 +0300

    evolotuion-dataserver rebuild

--- programming/misc/evolution-data-server/actions.py
+++ programming/misc/evolution-data-server/actions.py
@@ -10,28 +10,11 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
-shelltools.export("CFLAGS", "%s -fPIC -fno-strict-aliasing" % get.CFLAGS())
-
 def setup():
-    autotools.aclocal("-I m4")
-    autotools.autoheader()
-    libtools.libtoolize()
-    shelltools.system("intltoolize --force --copy --automake")
-    autotools.autoreconf("-fi")
-    autotools.configure("--with-openldap=yes \
-                         --enable-smime=yes \
-                         --program-prefix= --disable-dependency-tracking \
-                         --disable-goa \
-                         --enable-nss=yes \
-                         --enable-dot-locking=no \
-                         --enable-file-locking=fcntl \
-                         --disable-maintainer-mode \
-                         --enable-nntp=yes \
-                         --enable-vala-bindings=yes \
-                         --enable-introspection=yes \
-                         --disable-weather \
-                         --with-krb5=/usr \
-                         --with-libdb=/usr ")
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/evolution-data-server \
+                         --enable-vala-bindings \
+                         --disable-uoa")
 
 def build():
     autotools.make()
