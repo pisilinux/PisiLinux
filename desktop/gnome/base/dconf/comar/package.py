83ce55a6405ea83fa9f47cffef3f2993d1059522
namso-01 <namso-01@hotmail.it>
2013-09-04 22:04:55 +0300

    dconf recommit

--- desktop/gnome/base/dconf/comar/package.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-	# For every package that ships GSettings schemas, we must recompile
-	# the system's schemas
-	os.system ("glib-compile-schemas /usr/share/glib-2.0/schemas")
-	
