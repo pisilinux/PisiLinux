7e0ccd9e12f83b65dd0ddd9668b3685c4cb24401
marcin- <marcin.bojara@gmail.com>
2013-09-08 15:06:04 +0200

    ConsoleKit: /var/run => /run

--- system/base/ConsoleKit/actions.py
+++ system/base/ConsoleKit/actions.py
@@ -4,11 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
+    # /var/run => /run
+    pisitools.dosed("configure.ac", "^(\s+CONSOLE_KIT_PID_FILE=)\$\{localstatedir\}(\/run\/ConsoleKit\/pid)", r"\1\2")
+    pisitools.dosed("src/Makefile.am", "\$\(localstatedir\)(\/run\/ConsoleKit)", r"\1")
+
     autotools.autoreconf("-fi")
 
     autotools.configure("--disable-static \
@@ -23,9 +27,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s/" % get.installDIR())
 
-    pisitools.dodir("/var/run/ConsoleKit")
-
     # pam_console-compat
-    pisitools.dodir("/var/run/console")
+    pisitools.dodir("/run/console")
 
     pisitools.dodoc("AUTHORS","ChangeLog","README", "COPYING", "HACKING", "NEWS", "TODO")
