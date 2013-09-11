325a33178d5b18c201af7f7af037244ee99b92d8
marcin- <marcin.bojara@gmail.com>
2013-09-11 21:45:31 +0200

    mudur: remove /var/rundirs hack

--- system/base/mudur/comar/pakhandler.py
+++ system/base/mudur/comar/pakhandler.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import piksemel
-import stat
 import imp
-import os
 
 def doenv(filetag):
     for item in filetag:
@@ -13,33 +11,9 @@ def doenv(filetag):
             updenv.update_environment("/")
             return
 
-def docopy(filetag):
-    if not os.path.exists("/var/rundirs"): os.makedirs("/var/rundirs")
-    for item in filetag:
-        path = item.getTagData("Path")
-        if path.startswith("var/run/"): path = path[4:]
-        if path.startswith("run/"):
-            if not os.path.exists("/var/rundirs/%s" % path[4:]): os.makedirs("/var/rundirs/%s" % path[4:])
-            subpath = ""
-            for d in path.split("/")[1:]:
-                subpath = os.path.join(subpath, d)
-                s = os.stat("/run/%s" % subpath)
-                os.chmod("/var/rundirs/%s" % subpath, stat.S_IMODE(s.st_mode))
-                os.chown("/var/rundirs/%s" % subpath, s.st_uid, s.st_gid)
-
-def doclean(filetag):
-    for item in filetag:
-        path = item.getTagData("Path")
-        if path.startswith("var/run/"): path = path[4:]
-        if path.startswith("run/"):
-            if os.path.exists("/var/rundirs/%s" % path[4:]):
-                os.removedirs("/var/rundirs/%s" % path[4:])
-
-
 def setupPackage(metapath, filepath):
     doc = piksemel.parse(filepath)
     doenv(doc.tags("File"))
-    docopy(doc.tags("File"))
 
 def cleanupPackage(metapath, filepath):
     pass
@@ -47,4 +21,3 @@ def cleanupPackage(metapath, filepath):
 def postCleanupPackage(metapath, filepath):
     doc = piksemel.parse(filepath)
     doenv(doc.tags("File"))
-    doclean(doc.tags("File"))
