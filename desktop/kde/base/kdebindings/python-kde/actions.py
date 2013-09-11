4ca99313ec9880aa582b0e82faa2dae0b9f3882f
marcin- <marcin.bojara@gmail.com>
2013-09-11 12:23:09 +0200

    remove unnecessary line

--- desktop/kde/base/kdebindings/python-kde/actions.py
+++ desktop/kde/base/kdebindings/python-kde/actions.py
@@ -4,7 +4,6 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde4
 from pisi.actionsapi import get
