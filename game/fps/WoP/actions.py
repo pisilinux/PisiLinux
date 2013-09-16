c1d623073df8968b99810cae601a8e4945a6cd8f
circass <erdincgultekin@gmail.com>
2013-09-17 01:39:08 +0300

    update worldofpadman  juppii, use system headers

--- game/fps/WoP/actions.py
+++ game/fps/WoP/actions.py
@@ -9,44 +9,52 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-ARCH =  get.ARCH().replace("686", "386")
+ARCH = get.ARCH().replace("686", "386")
 
 builddir = "build/release-linux-%s" % ARCH
 datadir = "/usr/share/WoP"
 NoStrip = ["%s/wop" % datadir]
 
 def setup():
+    shelltools.system("rm -rf code/libcurl")
+    shelltools.system("rm -rf code/SDL12")
+    shelltools.system("rm -rf code/zlib")
+    shelltools.system("rm -rf code/libspeex")
+    shelltools.system("rm -rf code/libtheora")
+    shelltools.system("rm -rf code/libvorbis")
+    shelltools.system("rm -rf code/libogg")
+    shelltools.system("rm -rf code/jpeg-8c")
+
     pisitools.dosed("Makefile", "-O3", "%s -DUSE_ACTOR_DEFAULTS" % get.CFLAGS())
     pisitools.dosed("Makefile", "CC=gcc", "CC=%s" % get.CC())
 
 def build():
     autotools.make("DEFAULT_BASEDIR=%s \
-                    BUILD_SERVER=1 \
                     BUILD_CLIENT=1 \
+                    BUILD_SERVER=1 \
                     BUILD_CLIENT_SMP=1 \
                     BUILD_GAME_SO=1 \
                     BUILD_GAME_QVM=1 \
-                    USE_SDL=1 \
-                    USE_OPENAL=1 \
                     USE_CODEC_VORBIS=1 \
-                    USE_LOCAL_HEADERS=1" % (datadir))
+                    USE_INTERNAL_SPEEX=0 \
+                    USE_INTERNAL_ZLIB=0 \
+                    USE_INTERNAL_JPEG=0 \
+                    USE_INTERNAL_DLOPEN=0 \
+                    USE_LOCAL_HEADERS=0 \
+                    DEBUG_BUILD=NO" % (datadir))
 
 def install():
-    for i in ["wop-engine", "wop-smp", "wopded"]:
+    for i in ["wop", "wopded"]:
         pisitools.dobin("build/release-linux-%s/%s.%s" % (ARCH, i, ARCH))
 
-    pisitools.rename("/usr/bin/wop-engine.%s" % ARCH, "WoP")
-    pisitools.rename("/usr/bin/wop-smp.%s" % ARCH, "WoP-smp")
+    pisitools.rename("/usr/bin/wop.%s" % ARCH, "WoP")
     pisitools.rename("/usr/bin/wopded.%s" % ARCH, "WoP-server")
 
     pisitools.doexe("%s/baseq3/*.so" % builddir, "%s/baseq3" % datadir)
     pisitools.insinto("%s/baseq3/vm/" % datadir, "%s/baseq3/vm/*" % builddir)
 
-    pisitools.doexe("%s/missionpack/*.so" % builddir, "%s/missionpack" % datadir)
-    pisitools.insinto("%s/missionpack/vm" % datadir, "%s/missionpack/vm/*" % builddir)
+    pisitools.doexe("%s/*.so" % builddir, "%s" % datadir)
 
-    pisitools.insinto("%s/wop" % datadir, "wop_patch_1_2/server-allgametypes.cfg")
-    pisitools.insinto("%s/wop" % datadir, "wop_patch_1_2/server-wop_padpack.cfg")
-    pisitools.insinto("%s/wop" % datadir, "wop_patch_1_2/*.pk3")
+    pisitools.insinto("%s/missionpack" % datadir, "%s/missionpack" % builddir)
 
-    pisitools.dodoc("BUGS", "ChangeLog", "*.txt", "wop_patch_1_2/*.txt", "NOTTODO", "TODO", "README")
+    pisitools.dodoc("*.txt", "*-README")
