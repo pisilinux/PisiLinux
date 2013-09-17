277b702bc6951bdb017da55f52e92e5281e051de
namso-01 <namso-01@hotmail.it>
2013-09-17 14:22:34 +0300

    Revert "xfce4-session"
    This reverts commit 7458589f9f540130a1b56c8307c4b5484fe67f3e.

--- desktop/xfce/base/xfce4-session/actions.py
+++ desktop/xfce/base/xfce4-session/actions.py
@@ -10,13 +10,13 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-pluggable-dialogs \
-                         --enable-sound-settings \
-                         --enable-xrandr \
-                         --enable-libnotify \
-                         --enable-gio-unix \
-                         --enable-libxklavier \
-                         --enable-xcursor")
+    shelltools.system('xdt-autogen')
+    autotools.configure("--disable-static \
+                         --disable-legacy-sm")
+
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
