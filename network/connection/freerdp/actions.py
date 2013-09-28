561e102e6f73cc2f7c67cb572e4f356c9599f022
obsoleteman <tulliana@gmail.com>
2013-09-28 22:09:57 +0300

    fix installation

--- network/connection/freerdp/actions.py
+++ network/connection/freerdp/actions.py
@@ -4,23 +4,21 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
-from pisi.actionsapi import get
-
 
 def setup():
-    cmaketools.configure()
+    cmaketools.configure("-DCMAKE_SKIP_RPATH=ON \
+                          -DCMAKE_INSTALL_LIBDIR=lib")
 
 def build():
-    cmaketools.make("-j1")
+    cmaketools.make()
 
-#def check():
-#    autotools.make("-j1 test")
+def check():
+    pass
 
 def install():
-    cmaketools.install("libdir=%s/usr/lib" % get.installDIR())
-
+    #cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.install()
+    
     pisitools.dodoc("LICENSE", "README", )
