5ceb9f6c15e05e840d5dc0df228be6e5eda530fb
Ertuğrul <ertugrulerata@gmail.com>
2013-09-29 22:21:17 +0300

    blender 2.68a version

--- multimedia/editor/blender/actions.py
+++ multimedia/editor/blender/actions.py
@@ -17,18 +17,18 @@ def setup():
     shelltools.cd("..")
     shelltools.makedirs("cmake-make")
     shelltools.cd("cmake-make") 
-    shelltools.system("cmake ../blender-2.68 \
-		      -DCMAKE_INSTALL_PREFIX=/usr \
-		      -DCMAKE_BUILD_TYPE=Release \
-		      -DCMAKE_SKIP_RPATH=ON \
-		      -DWITH_JACK=ON \
+    shelltools.system("cmake ../blender-2.68a \
+                      -DCMAKE_INSTALL_PREFIX=/usr \
+                      -DCMAKE_BUILD_TYPE=Release \
+                      -DCMAKE_SKIP_RPATH=ON \
+                      -DWITH_JACK=ON \
                       -DWITH_IMAGE_OPENEXR=ON \
                       -DWITH_FFTW3=ON\
-		      -DWITH_PLAYER=ON \
-		      -DWITH_CODEC_FFMPEG=ON \
-		      -DWITH_INSTALL_PORTABLE=OFF \
-		      -DWITH_GAMEENGINE=ON \
-		      -DWITH_PYTHON_INSTALL=OFF \
+                      -DWITH_PLAYER=ON \
+                      -DWITH_CODEC_FFMPEG=ON \
+                      -DWITH_INSTALL_PORTABLE=OFF \
+                      -DWITH_GAMEENGINE=ON \
+                      -DWITH_PYTHON_INSTALL=OFF \
                       -DWITH_CODEC_SNDFILE=ON ")
 
 def build():
@@ -43,14 +43,14 @@ def install():
     pisitools.insinto("/usr/share/doc/", "bin/*.txt")
     pisitools.insinto("/usr/share/doc/", "bin/*.html")
     pisitools.insinto("/usr/share/pixmaps/", "bin/blender.svg")
-    pisitools.insinto("/usr/share/", "bin/2.68/datafiles/locale/")
+    pisitools.insinto("/usr/share/blender", "bin/2.68/datafiles/locale/")
     pisitools.insinto("/usr/share/blender/", "bin/2.68/scripts")
-    
+
     shelltools.cd("../") 
-    shelltools.cd("blender-2.68/release/")
+    shelltools.cd("blender-2.68a/release/")
     ## Install miscellaneous files
     pisitools.insinto("/usr/share/blender/", "scripts/*")
-    
+
     pisitools.insinto("/usr/share/blender/", "datafiles/colormanagement/")
 
     ##Install icon files
