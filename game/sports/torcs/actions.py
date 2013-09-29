43ba479de14be346038a47ca0d65118c6808d645
BlackSEA- <blacksea8393@gmail.com>
2013-09-29 14:09:02 +0300

    torcs:add patch

--- game/sports/torcs/actions.py
+++ game/sports/torcs/actions.py
@@ -9,7 +9,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-vif")
     autotools.configure()
+                        
 
 def build():
     autotools.make("-j1")
