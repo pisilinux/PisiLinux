cd935cb938bfdbf1eb7efbd299d9275c64788418
namso-01 <namso-01@hotmail.it>
2013-10-06 16:32:54 +0300

    ufoai: fixed

--- game/strategy/ufoai/actions.py
+++ game/strategy/ufoai/actions.py
@@ -14,9 +14,9 @@ datadir = "/usr/share/ufoai"
 exefiles = ["ufo", "ufo2map", "ufoded"]
 
 def setup():
-    shelltools.export("CFLAGS", "%s -mmmx -msse" % get.CFLAGS())
-    shelltools.export("CXXFLAGS", "%s -mmmx -msse" % get.CXXFLAGS())
-    shelltools.export("LDFLAGS", "%s -mmmx -msse" % get.LDFLAGS())
+    shelltools.export("CFLAGS", "%s -lpthread -mmmx -msse" % get.CFLAGS())
+    shelltools.export("CXXFLAGS", "%s -lpthread -mmmx -msse" % get.CXXFLAGS())
+    shelltools.export("LDFLAGS", "%s -lpthread -mmmx -msse" % get.LDFLAGS())
 
     shelltools.makedirs("base")
     shelltools.system("./configure --prefix=/usr \
