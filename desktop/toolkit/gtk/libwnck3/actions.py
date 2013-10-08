b4f5ac199b7c464a20a929c6943e5f8252c49f7a
obsoleteman <tulliana@gmail.com>
2013-10-08 15:30:24 +0300

    libwnck3 version bump

--- desktop/toolkit/gtk/libwnck3/actions.py
+++ desktop/toolkit/gtk/libwnck3/actions.py
@@ -4,15 +4,14 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-shelltools.export("HOME", get.workDIR())
-
 def setup():
-    #autotools.autoreconf("-fis")
+    shelltools.system("rm -f libtool")
+    autotools.autoreconf("-fi")
     autotools.configure("--disable-static")
 
     pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
