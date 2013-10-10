1674d2ce2161cd788c125929ae3dce8640e9b580
obsoleteman <tulliana@gmail.com>
2013-10-10 03:00:20 +0300

    librsvg: whole vapi, it prevents autoreconf

--- desktop/gnome/base/librsvg/actions.py
+++ desktop/gnome/base/librsvg/actions.py
@@ -8,13 +8,25 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+
 def setup():
+    pisitools.dosed("configure.in", "gdk-pixbuf-query-loaders-[2346]+\s", "")
+    if get.buildTYPE() == "emul32":
+        pisitools.dosed("configure.in", "(gdk-pixbuf-query-loaders)([\s\]])", r"\1-32\2")
+
     autotools.autoreconf("-vfi")
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-gtk-doc \
+                         --enable-pixbuf-loader=yes \
+                         --disable-static")
+
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING", "COPYING.LIB", "NEWS", "README")
+
+    pisitools.dodoc("COPYING", "AUTHORS", "ChangeLog", "README")
