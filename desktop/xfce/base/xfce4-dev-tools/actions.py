39d18f71ee277ff66cd03282122346f71f2486b2
obsoleteman <tulliana@gmail.com>
2013-10-12 02:11:10 +0300

    clean xfce4-dev-tools

--- desktop/xfce/base/xfce4-dev-tools/actions.py
+++ desktop/xfce/base/xfce4-dev-tools/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--prefix=/usr \
+                         --libexecdir=/usr/lib")
 
 def build():
     autotools.make()
