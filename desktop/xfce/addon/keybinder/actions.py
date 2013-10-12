610f87d2513ceabc6a12f61a203be7ed0b816c1d
circass <erdincgultekin@gmail.com>
2013-10-12 21:18:09 +0300

    rebuild keybinder

--- desktop/xfce/addon/keybinder/actions.py
+++ desktop/xfce/addon/keybinder/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
-shelltools.export("HOME", get.workDIR())
-shelltools.export("PYTHONDONTWRITEBYTECODE", "1")
 
 def setup():
     autotools.configure("--disable-static --enable-python")
