232b837fb973e2563287c7b287f085fb8ad1c0ce
Ertuğrul <ertugrulerata@gmail.com>
2013-10-21 22:44:26 +0300

    xfce4-battery-plugin unused direct dependency removed.

--- desktop/xfce/addon/xfce4-battery-plugin/actions.py
+++ desktop/xfce/addon/xfce4-battery-plugin/actions.py
@@ -10,6 +10,9 @@ from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure()
+    
+    # for fix unused dependency
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
