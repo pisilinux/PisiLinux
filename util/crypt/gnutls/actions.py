17e8419b3e643e07264ef1a7512c4f0b90e2bbbe
obsoleteman <tulliana@gmail.com>
2013-10-27 01:37:43 +0300

    gnutls: version bump, fix packaging errors

--- util/crypt/gnutls/actions.py
+++ util/crypt/gnutls/actions.py
@@ -5,21 +5,16 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    shelltools.export("AUTOPOINT", "true")
-
     options = "--disable-static \
                --disable-rpath \
                --disable-dependency-tracking \
                --enable-guile \
-               --with-lzo \
                --with-zlib \
-               --with-libgcrypt \
-               --with-included-libcfg"
+               --disable-valgrind-tests"
 
     if get.buildTYPE() == "emul32":
         options += " --disable-hardware-acceleration"
@@ -28,6 +23,13 @@ def setup():
 
 def build():
     autotools.make()
+
+def check():
+    #some tests fail in emul32
+    if get.buildTYPE() == "emul32":
+        pass
+    else:
+        autotools.make("-k check")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
