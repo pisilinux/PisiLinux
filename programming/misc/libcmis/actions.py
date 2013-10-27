ec57fe5cf7f02a51c7e5ecbf27bf67d6fb014d4e
aydemir <yusuf.aydemir@istanbul.com>
2013-10-27 09:57:40 +0200

    libcmis version bump to 0.4.1

--- programming/misc/libcmis/actions.py
+++ programming/misc/libcmis/actions.py
@@ -9,11 +9,14 @@ from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--disable-werror \
-			 --disable-static \
-			 --with-man=no \
-			 --program-suffix=.pl \
-			 --docdir=/usr/share/doc \
-			 --disable-tests ")
+                         --disable-static \
+                         --with-man=no \
+                         --program-suffix=.pl \
+                         --docdir=/usr/share/doc \
+                         --disable-tests ")
+    
+    # for fix unused dependency
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")     
 
 def build():
     autotools.make()
