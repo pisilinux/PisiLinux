aaf788d20382c296b2a35dafa712fdca4b15e17a
namso-01 <namso-01@hotmail.it>
2013-10-27 09:44:49 +0200

    libgnomekbd: version bump

--- desktop/gnome/base/libgnomekbd/actions.py
+++ desktop/gnome/base/libgnomekbd/actions.py
@@ -6,10 +6,14 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+shelltools.export("HOME", get.workDIR())
+
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --enable-introspection=yes")
 
     pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
