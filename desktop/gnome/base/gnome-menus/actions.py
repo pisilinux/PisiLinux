2f25ce2854d85c6e0c47c4d195dcd28047aa1c12
namso-01 <namso-01@hotmail.it>
2013-10-27 09:48:00 +0200

    gnome-menus: version bump

--- desktop/gnome/base/gnome-menus/actions.py
+++ desktop/gnome/base/gnome-menus/actions.py
@@ -4,26 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-
-shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.configure("--disable-static")
-    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
-    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
-    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodir("/etc/xdg/gnome")
-    pisitools.domove("/etc/xdg/menus", "/etc/xdg/gnome")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
