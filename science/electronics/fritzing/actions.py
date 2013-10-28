809711142b56d7a29742ef78f8e73483066f7fa6
BlackSEA- <blacksea8393@gmail.com>
2013-10-28 14:37:32 +0200

    fritzing:version bump

--- science/electronics/fritzing/actions.py
+++ science/electronics/fritzing/actions.py
@@ -4,16 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import qt4
-from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 
 def setup():
-    qt4.configure(installPrefix='%s/usr' % get.installDIR())
+    qt4.configure()
 
 def build():
     qt4.make()
-    autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    qt4.install()
+    
+    pisitools.dodoc("README.txt", "LICENSE.*")
