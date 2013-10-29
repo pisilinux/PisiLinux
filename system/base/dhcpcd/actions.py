ab6c50023347e4fa8650536f1dd7bd7b338e9b87
BlackSEA- <blacksea8393@gmail.com>
2013-10-29 23:50:52 +0200

    dhcpcd:rebuild

--- system/base/dhcpcd/actions.py
+++ system/base/dhcpcd/actions.py
@@ -7,21 +7,24 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     autotools.configure("--libexecdir=/lib/dhcpcd \
                          --dbdir=/var/lib/dhcpcd \
                          --sbindir=/sbin \
-                         --localstatedir=/var")
+                         --localstatedir=/var \
+                         --rundir=/run")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DBDIR=/var/lib/dhcpcd LIBEXECDIR=/lib/dhcpcd DESTDIR=%s" % get.installDIR())
-
+    # Set Options in /etc/dhcpcd.conf Disable ip4vall
+    shelltools.echo("%s/etc/dhcpcd.conf" % get.installDIR(), "noipv4ll")
     # Remove hooks install the compat one
-    pisitools.remove("/lib/dhcpcd/dhcpcd-hooks/*")
-    pisitools.insinto("/lib/dhcpcd/dhcpcd-hooks", "dhcpcd-hooks/50-dhcpcd-compat")
+   # pisitools.remove("/lib/dhcpcd/dhcpcd-hooks/*")
+    #pisitools.insinto("/lib/dhcpcd/dhcpcd-hooks", "dhcpcd-hooks/50-dhcpcd-compat")
 
     pisitools.dodoc("README")
