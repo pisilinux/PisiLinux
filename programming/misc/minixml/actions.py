44619802244ce8e997e5a03b603f6ae5016a88cd
aydemir <yusuf.aydemir@istanbul.com>
2013-10-29 20:40:43 +0200

    minixml version bump to 2.7

--- programming/misc/minixml/actions.py
+++ programming/misc/minixml/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="mxml-%s" %get.srcVERSION()
-
 def setup():
     autotools.configure("--enable-shared")
 
