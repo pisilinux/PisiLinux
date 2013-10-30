8ba01b66b3488b7a3c20417c92cb0694f6fb8bb9
BlackSEA- <blacksea8393@gmail.com>
2013-10-30 19:11:36 +0200

    CGAL:version bump to 4.3

--- science/mathematics/CGAL/actions.py
+++ science/mathematics/CGAL/actions.py
@@ -13,43 +13,29 @@ from pisi.actionsapi import Error
 import os
 from stat import *
 
-shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release",installPrefix="/usr")
 
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
-                          -DWITH_CGAL_Qt3=OFF \
-                          -DWITH_LEDA=OFF ", installPrefix="/usr",sourceDir="..")
 
 def build():
-    shelltools.cd("build")
     cmaketools.make()
 
-    # these wont compile with our boost
-    obsolotes = ["demo/Mesh_3", "demo/Polyhedron", "examples/Mesh_3", "examples/Polyhedron"]
-
-    try:
-        for i in obsolotes:
-            shelltools.unlinkDir("%s" % i)
-            shelltools.unlinkDir("%s/%s-%s/%s" % (get.workDIR(), get.srcNAME(), get.srcVERSION(), i))
-    except Error, e:
-        print e
-
+    #these wont compile with our boost
+    #obsolotes = ["demo/Mesh_3", "demo/Polyhedron", "examples/Mesh_3", "examples/Polyhedron"]
     cmaketools.make("demos")
     cmaketools.make("examples")
 
+
 def install():
-    shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.removeDir("/usr/bin")
     pisitools.removeDir("/usr/lib/CGAL")
 
-    pisitools.dohtml("../doc_html/")
+    pisitools.dohtml("doc_html/")
 
-    pisitools.insinto("/usr/share/CGAL/examples", "../examples/*")
+    pisitools.insinto("/usr/share/CGAL/examples", "examples/*")
 
     # copy executables only
     for i in ["demo", "examples"]:
