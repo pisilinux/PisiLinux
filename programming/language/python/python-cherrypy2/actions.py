39009cf02865279df0257544a1782c7a43a9ff10
BlackSEA- <blacksea8393@gmail.com>
2013-11-05 22:06:19 +0200

    python-cherrypy2:version bump to 3.2.2

--- programming/language/python/python-cherrypy2/actions.py
+++ programming/language/python/python-cherrypy2/actions.py
@@ -8,9 +8,9 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
 def install():
-    pisitools.dosed("cherrypy/test/test.py", "raw_input", "")
-    pisitools.dosed("cherrypy/test/test.py", "'test_cache_filter',", "")
+  #  pisitools.dosed("cherrypy/test/test.py", "raw_input", "")
+  #  pisitools.dosed("cherrypy/test/test.py", "'test_cache_filter',", "")
 
     pythonmodules.install()
 
-    pisitools.dodoc("README.txt", "CHANGELOG.txt", "CHERRYPYTEAM.txt")
+    pisitools.dodoc("README.txt")
