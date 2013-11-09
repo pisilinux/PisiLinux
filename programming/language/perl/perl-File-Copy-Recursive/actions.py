fa2a610a3da44b1b0e9d28d4cf9cf2fcc01d8b33
rrdebruin <rr.debruin@gmail.com>
2013-11-09 16:58:20 +0100

    Rebuild perl-File-Copy-Recursive

--- programming/language/perl/perl-File-Copy-Recursive/actions.py
+++ programming/language/perl/perl-File-Copy-Recursive/actions.py
@@ -23,4 +23,4 @@ def install():
 
     pisitools.dodoc("Changes", "README")
     pisitools.remove("/usr/share/man/man3/File::Copy::Recursive.3pm")
-    pisitools.remove("/usr/lib/perl5/vendor_perl/5.16.2/File/Copy/Recursive.pm")
+#    pisitools.remove("/usr/lib/perl5/vendor_perl/5.16.2/File/Copy/Recursive.pm")
