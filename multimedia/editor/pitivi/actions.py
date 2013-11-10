102a84d88c6be45d0f56d2681fb14bea142947ba
namso-01 <namso-01@hotmail.it>
2013-11-10 09:16:40 +0200

    pitivi: version bump

--- multimedia/editor/pitivi/actions.py
+++ multimedia/editor/pitivi/actions.py
@@ -17,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
