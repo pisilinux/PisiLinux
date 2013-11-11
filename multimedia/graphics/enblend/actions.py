d9ee05bcef0d7ae4883ab821878a9c1ce93fe5bf
marcin- <marcin.bojara@gmail.com>
2013-11-11 09:37:18 +0100

    enblend: version bump

--- multimedia/graphics/enblend/actions.py
+++ multimedia/graphics/enblend/actions.py
@@ -4,20 +4,18 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
 
 
 def setup():
-   # autotools.autoreconf("-vif")
-    autotools.configure("--prefix=/usr")
+    cmaketools.configure()
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
