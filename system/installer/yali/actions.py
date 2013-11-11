002adb82cdc9f6d8605cab799b11f49b00f46489
marcin- <marcin.bojara@gmail.com>
2013-11-11 12:44:54 +0100

    yali: new version

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -4,18 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import pythonmodules
 
 def setup():
-    # change slider direction
-    pisitools.dosed("yali/gui/Ui/", "RightToLeft", "LeftToRight", filePattern="^(logicalvolume|partition)\..*")
-
-    # let progressbar starts from 0
-    pisitools.dosed("yali/gui/Ui/installprogress.ui", ">65<", ">0<")
-
     repo_uri = "http://packages.pisilinux.org/repositories/1.0/stable/x86_64/pisi-index.xml.xz" # FIXME
     pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
     pisitools.dosed("yali/constants.py", "@REPO_NAME@", "pisi") # FIXME
@@ -27,4 +19,3 @@ def build():
 
 def install():
     pythonmodules.install()
-    shelltools.system("find %s -name '*.py' -exec sed -i 's/_rcfrom/from/g' {} ';'" % get.installDIR())
