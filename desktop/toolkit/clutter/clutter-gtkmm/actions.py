5c3bdba52b8c9738e3ac9c188eef16d924d49113
marcin- <marcin.bojara@gmail.com>
2013-11-11 19:17:50 +0100

    clutter-gtkmm: Remove *-devel packages from runtime dependencies.

--- desktop/toolkit/clutter/clutter-gtkmm/actions.py
+++ desktop/toolkit/clutter/clutter-gtkmm/actions.py
@@ -15,9 +15,9 @@ def setup():
     autotools.configure("--disable-dependency-tracking \
                          --enable-shared \
                          --disable-static")
-    
+
     # for fix unused dependency
-    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")    
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
