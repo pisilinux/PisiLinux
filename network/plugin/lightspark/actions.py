5e05d18273c9c836985491cc9d1dafb74c00b1f3
BlackSEA- <blacksea8393@gmail.com>
2013-11-21 00:09:33 +0200

    lightspark:add patch,fix build

--- network/plugin/lightspark/actions.py
+++ network/plugin/lightspark/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
