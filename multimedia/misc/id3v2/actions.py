d4202d30ab330087af83cbd667eae7ef27cc9488
namso-01 <namso-01@hotmail.it>
2013-12-03 18:19:07 +0200

    id3v2: patched

--- multimedia/misc/id3v2/actions.py
+++ multimedia/misc/id3v2/actions.py
@@ -9,11 +9,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
-
 def build():
+    shelltools.export("CXXFLAGS", "%s -fno-strict-aliasing" % get.CXXFLAGS())
     autotools.make()
-
 
 def install():
     pisitools.dobin("id3v2")
-    pisitools.dobin("id3v2.1")  
+    pisitools.doman("id3v2.1") 
+    
+    pisitools.dodoc("COPYING", "README")
