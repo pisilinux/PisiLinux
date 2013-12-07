c9ebe4b85a897c9c645b952748105b95cbf92a80
namso-01 <namso-01@hotmail.it>
2013-12-07 21:51:36 +0200

    rebuild for perl: perl-Locale-Codes

--- programming/language/perl/perl-Locale-Codes/actions.py
+++ programming/language/perl/perl-Locale-Codes/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import perlmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
-
 def setup():
     perlmodules.configure()
 
@@ -22,29 +20,9 @@ def check():
 def install():
     perlmodules.install()
 
-    #This files are included in Perl 5.14.1
-    for locale in ["Currency", "Language", "Codes::Language", "Codes::Changes" , "Script", "Codes::Currency", "Codes::Country", "Codes", "Country", "Codes::Script"]:
+    #This files are included in Perl 5.18.1
+    for locale in ["Codes::LangExt", "Codes::LangVar", "Codes::API", "Codes::LangFam", "Codes::Constants", "Codes::LangFam_Retired", "Currency", "Language", "Codes::Language", "Codes::Changes" , "Script", "Codes::Currency", "Codes::Country", "Codes", "Country", "Codes::Script"]:
         pisitools.remove("/usr/share/man/man3/Locale::%s.3pm" % locale)
-        #pisitools.remove("/usr/lib/perl5/vendor_perl/%s/Locale/%s.pm" % (get.curPERL(), locale))
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Language_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangFam_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangExt.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangExt_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Script_Retired.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangVar_Retired.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangVar_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangVar.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Country_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Language_Retired.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Currency_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangFam_Retired.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Script_Codes.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Country_Retired.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangFam.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::API.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Constants.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::Currency_Retired.3pm")
-    pisitools.remove("/usr/share/man/man3/Locale::Codes::LangExt_Retired.3pm")
 
-    pisitools.dodoc("README", "ChangeLog")
+    pisitools.dodoc("README", "Changes")
 
