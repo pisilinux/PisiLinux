bdec6dd4026f706b373df298bbfe8d34d83fce3e
namso-01 <namso-01@hotmail.it>
2013-12-12 18:04:57 +0200

    python-pygobject3: split package

--- programming/language/python/python-pygobject3/actions.py
+++ programming/language/python/python-pygobject3/actions.py
@@ -11,12 +11,33 @@ from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("configure", "-Werror=format", "#-Werror=format")
-    autotools.configure()
+    shelltools.makedirs("build-python2")
+    shelltools.makedirs("build-python3")
+
+    shelltools.cd("build-python3")
+    shelltools.export("PYTHON", "/usr/bin/python3.3")
+    shelltools.system("../configure --prefix=/usr \
+                       --localstatedir=/var \
+                       --disable-static")
+
+    shelltools.cd("../build-python2")
+    shelltools.export("PYTHON", "/usr/bin/python2.7")
+    shelltools.system("../configure --prefix=/usr \
+                       --localstatedir=/var \
+                       --disable-static")
 
 def build():
+    shelltools.cd("build-python3")
+    autotools.make()
+
+    shelltools.cd("../build-python2")
     autotools.make()
 
 def install():
-    autotools.install()
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
 
-    pisitools.dodoc("AUTHORS", "NEWS", "ChangeLog", "README")
+    shelltools.cd("build-python3")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("../build-python2")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
