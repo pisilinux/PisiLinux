4b236b2c64d2c2b7048789e1c19beaa1537bfe0c
obsoleteman <tulliana@gmail.com>
2013-12-20 04:05:07 +0200

    re comment icon-theme.cache lines

--- desktop/mate/base/mate-icon-theme/actions.py
+++ desktop/mate/base/mate-icon-theme/actions.py
@@ -18,6 +18,6 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.remove("/usr/share/icons/matefaenza/icon-theme.cache")
+    #pisitools.remove("/usr/share/icons/matefaenza/icon-theme.cache")
 
     pisitools.dodoc("COPYING", "TODO", "README", "NEWS", "AUTHORS")
