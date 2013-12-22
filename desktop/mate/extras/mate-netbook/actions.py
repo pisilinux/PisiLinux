7d7efd865773440ef10d1d6e48e4a6093c99c040
namso-01 <namso-01@hotmail.it>
2013-12-22 10:58:29 +0200

    mate-netbook: new package

---
+++ desktop/mate/extras/mate-netbook/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh")
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --sbindir=/usr/bin \
+                         --libexecdir=/usr/lib/mate-netbook \
+                         --disable-static \
+                         --disable-scrollkeeper")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README")
