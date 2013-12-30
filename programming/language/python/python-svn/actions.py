32be71e2319be56b19f55bba7a4502a45974861c
namso-01 <namso-01@hotmail.it>
2013-12-30 21:39:37 +0200

    python-svn: rebuild

--- programming/language/python/python-svn/actions.py
+++ programming/language/python/python-svn/actions.py
@@ -15,14 +15,6 @@ def setup():
     shelltools.cd("Source")
     shelltools.system("python setup.py configure --enable-debug --verbose --fixed-module-name")
 
-    # Fix compiler flags
-    pisitools.dosed("Makefile", "^CCC=.*$", "CCC=%s" % get.CXX())
-    pisitools.dosed("Makefile", "^CC=.*$", "CC=%s" % get.CC())
-    pisitools.dosed("Makefile", "^LDSHARED=.*$", "LDSHARED=%s -shared %s" % (get.CXX(), get.LDFLAGS()))
-
-    # Fix linking order
-    pisitools.dosed("Makefile", "\$\(LDSHARED\) (-o \$@) (.*)", "$(LDSHARED) \\2 \\1")
-
 def build():
     autotools.make("-C Source")
 
