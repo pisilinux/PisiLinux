7db872af9cdf732e6a6627ced33af890eedb7d1c
namso-01 <namso-01@hotmail.it>
2013-12-30 21:28:49 +0200

    doxygen: v.bump + dowxywizard added

--- office/misc/doxygen/actions.py
+++ office/misc/doxygen/actions.py
@@ -8,19 +8,13 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    # We have patch that includes the PARDUS-foo variables. After that
-    # the env variables below are exported.
-    pisitools.dosed("tmake/lib/linux-g++/tmake.conf", "PARDUS_CC", get.CC())
-    pisitools.dosed("tmake/lib/linux-g++/tmake.conf", "PARDUS_CXX", get.CXX())
-    pisitools.dosed("tmake/lib/linux-g++/tmake.conf", "PARDUS_CFLAGS", get.CFLAGS())
-    pisitools.dosed("tmake/lib/linux-g++/tmake.conf", "PARDUS_LDFLAGS", get.LDFLAGS())
-
     # --shared and --release are default parameters, however we just write them
     # to down to avoid confusing wheter it's a static/shared or release/debug
     # package at the first look :)
     autotools.rawConfigure("--shared \
                             --release \
                             --dot dot \
+                            --with-doxywizard \
                             --prefix /usr")
 
 def build():
@@ -28,9 +22,11 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.doman("doc/doxywizard.1")
 
     # The makefile included is there to generate the html files
     # The user itself should execute it
     pisitools.insinto(get.docDIR() + "/doxygen" , "examples")
 
-    pisitools.dodoc("LANGUAGE.HOWTO", "LICENSE", "README", "VERSION")
+    pisitools.dodoc("LANGUAGE.HOWTO", "LICENSE", "README*", "VERSION")
