7a63a483ec9021739a65f6b5ff73062fef683a58
marcin- <marcin.bojara@gmail.com>
2013-12-31 19:23:09 +0100

    system-config-printer: fix paths

--- hardware/printer/system-config-printer/actions.py
+++ hardware/printer/system-config-printer/actions.py
@@ -4,12 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
+    # we are using different paths
+    pisitools.dosed("cupshelpers/cupshelpers.py", "\/lib64\/", "\/lib\/")
+    pisitools.dosed("troubleshoot/CheckUSBPermissions.py", "\/usr\/bin\/getfacl", "\/bin\/getfacl")
+
     pisitools.dosed("Makefile.in", "xmlto man", "xmlto --skip-validation man")
     autotools.configure("--with-udev-rules \
                          --disable-rpath")
