e29f856368e7fcc1fce572efac76d938ef00a688
obsoleteman <tulliana@gmail.com>
2014-01-06 03:53:29 +0200

    fribidi: we have fribidi package in multimedia/misc

--- editor/fbreader/actions.py
+++ editor/fbreader/actions.py
@@ -4,10 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def build():
