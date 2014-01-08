1ff6bd1071f48d8b3500055dc9cb1cf42830ee2e
BlackSEA- <blacksea8393@gmail.com>
2014-01-08 12:07:12 +0200

    smtube:correction

--- multimedia/video/smtube/actions.py
+++ multimedia/video/smtube/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
@@ -11,15 +11,5 @@ def build():
     autotools.make("PREFIX=/usr")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("PREFIX=/usr DESTDIR=%s" % get.installDIR())
 
-
-# You can use these as variables, they will replace GUI values before build.
-# Package Name : smtube
-# Version : 1.8
-# Summary : SMPlayer is a free media player for Windows and Linux with built-in codecs
-
-# For more information, you can look at the Actions API
-# from the Help menu and toolbar.
-
-# By PiSiDo 2.0.0
