42a92c233570b00975980de407ab6244773225a5
marcin- <marcin.bojara@gmail.com>
2014-01-09 19:20:02 +0100

    libtdb: version bump for samba

---
+++ server/database/libtdb/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+shelltools.export("JOBS", get.makeJOBS().replace("-j", ""))
+
+def setup():
+    autotools.configure("--disable-python")
+
+def build():
+    shelltools.system("make")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("docs/README")
