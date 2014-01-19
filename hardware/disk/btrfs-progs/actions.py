fabe2d7b3fed4f320515b3a4446a6fc638426194
marcin- <marcin.bojara@gmail.com>
2014-01-19 20:19:52 +0100

    btrfs-progs: version bump

--- hardware/disk/btrfs-progs/actions.py
+++ hardware/disk/btrfs-progs/actions.py
@@ -4,21 +4,27 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def build():
-    autotools.make("CC=%s CFLAGS=\"%s\"" % (get.CC(), get.CFLAGS()))
-    #autotools.make("CC=%s CFLAGS=\"%s\" convert" % (get.CC(), get.CFLAGS()))
+    # Fix install prefix
+    pisitools.dosed("Makefile", "^(prefix \?= \/usr)\/local", r"\1")
+
+    autotools.make("V=1 CC=%s CFLAGS=\"%s\"" % (get.CC(), get.CFLAGS()))
+    autotools.make("V=1 CC=%s CFLAGS=\"%s\" btrfs-select-super" % (get.CC(), get.CFLAGS()))
 
 def install():
     pisitools.dosed("man/Makefile", "^prefix \?= .*$", "prefix = /usr/share")
-    autotools.rawInstall("bindir=/sbin DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.remove("/usr/lib/*.a")
 
     pisitools.insinto("/usr/bin", "bcp", "btrfs-bcp")
     pisitools.insinto("/usr/bin", "show-blocks", "btrfs-show-blocks")
+    pisitools.insinto("/usr/bin", "btrfs-select-super")
 
-    pisitools.dosym("btrfsck", "/sbin/fsck.btrfs")
+    pisitools.dosym("btrfsck", "/usr/bin/fsck.btrfs")
 
     pisitools.dodoc("COPYING")
