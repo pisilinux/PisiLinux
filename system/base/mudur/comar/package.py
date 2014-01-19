64dd487b9b2b586ee13037071ea2f62323446566
marcin- <marcin.bojara@gmail.com>
2014-01-19 13:40:11 +0100

    mudur: add comar script

--- system/base/mudur/comar/package.py
+++ system/base/mudur/comar/package.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+import re
+import shutil
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    if fromRelease and int(fromRelease) < 7:
+        shutil.copyfile("/etc/fstab", "/etc/fstab.bak")
+        new = []
+        for line in [line.strip() for line in open("/etc/fstab.bak")]:
+            if re.search("\s+\/run\s+", line): continue
+            new.append(line)
+        new.append("tmpfs                   /run                    tmpfs   nodev,nosuid,size=10%,mode=755    0 0\n")
+        with open("/etc/fstab", "w") as f: f.write("\n".join(new))
     # add disks into fstab
-    os.system("/sbin/update-fstab")
+    # os.system("/sbin/update-fstab")
