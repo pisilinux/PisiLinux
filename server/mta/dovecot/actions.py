b8ed3b517e91c92a93601c01103f9eced179870d
aydemir <yusuf.aydemir@istanbul.com>
2014-01-23 01:53:37 +0200

    dovecot fix unused

--- server/mta/dovecot/actions.py
+++ server/mta/dovecot/actions.py
@@ -25,6 +25,8 @@ def setup():
                          --with-gssapi \
                          --with-ldap \
                          --disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")    
 
 def build():
     autotools.make()
