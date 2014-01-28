99d1859a8da3f27c009b1722d495c3ea31cf9cd7
Alihan Öztürk <alihan@pisilinux.org>
2014-01-28 19:45:54 +0200

    evince:delete patch

--- office/misc/evince/actions.py
+++ office/misc/evince/actions.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
+
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 shelltools.export('HOME', get.workDIR())
 
 def setup():
-    autotools.configure("--disable-static\
-                         --disable-scrollkeeper\
-                         --disable-schemas-compile\
+    shelltools.system("sed -i -e 's/gnome-icon-theme//' configure.ac configure")
+    autotools.autoreconf("-vif")
+    autotools.configure("--prefix=/usr \
+                         --libexecdir=/usr/lib/$_pkgalias \
+                         --sysconfdir=/etc \
+                         --localstatedir=/var \
+                         --disable-maintainer-mode \
+                         --disable-schemas-compile \
+                         --disable-debug \
+                         --disable-tests \
                          --disable-nautilus \
-                         --enable-pixbuf\
+                         --enable-previewer \
+                         --disable-introspection \
                          --enable-t1lib \
                          --enable-comics \
-                         --enable-impress \
-                         --enable-pdf \
-                         --enable-tiff \
-                         --enable-djvu \
-                         --enable-dvi \
-                         --enable-introspection")
+                         --disable-dvi \
+                         --disable-ps \
+                         --disable-djvu \
+                         --disable-gtk-doc \
+                         --without-keyring \
+                         --with-smclient=xsmp")
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("README", "COPYING", "TODO", "AUTHORS", "ChangeLog")
+    
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "COPYING", "TODO")
