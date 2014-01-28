6c2304481353580744a4671b07c38bb2765c2054
waroi <waroi@yandex.com>
2014-01-28 20:24:06 +0200

    daq-rebuild unused-fix

--- network/library/daq/actions.py
+++ network/library/daq/actions.py
@@ -15,6 +15,8 @@ def setup():
     autotools.autoheader()
     libtools.libtoolize("--force --copy --automake")
     autotools.configure("--libdir=/usr/lib")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
