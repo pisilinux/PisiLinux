53d202be0c1f2d2ebdc2d1b30b6f0843fd664a2a
waroi <waroi@yandex.com>
2014-01-29 21:11:14 +0200

    exo-rebuild unused-fix

--- desktop/xfce/base/exo/actions.py
+++ desktop/xfce/base/exo/actions.py
@@ -15,6 +15,8 @@ def setup():
                          --disable-static \
                          --disable-gtk-doc \
                          --disable-debug")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
