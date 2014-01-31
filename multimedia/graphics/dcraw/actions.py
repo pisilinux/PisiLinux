5b6d069418d897455c1d6ac43a485a365fb871c6
Alihan Öztürk <alihan@pisilinux.org>
2014-01-31 15:25:56 +0200

    dcraw:version bump and some fix

--- multimedia/graphics/dcraw/actions.py
+++ multimedia/graphics/dcraw/actions.py
@@ -4,15 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 WorkDir = "dcraw"
 
 def build():
-    autotools.compile('-DLOCALEDIR="\\"/usr/share/locale\\"" -lm -llcms -ljpeg -o dcraw dcraw.c')
+    autotools.compile('-DLOCALEDIR="\\"/usr/share/locale\\"" -lm -ljasper -llcms -ljpeg -o dcraw dcraw.c')
 
 def install():
     pisitools.dobin("dcraw")
