19197853aa03a9901eb4e2677bb3cf9a1ee418ce
tribunal2000 <tribunal@pisilinux.org>
2014-02-01 01:07:04 +0400

    remove .py files

--- desktop/pisilinux/user-manager/ui_edituser.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file './edituser.ui'
-#
-# Created: Sat Feb  1 00:43:48 2014
-#      by: PyQt4 UI code generator 4.9.6
-#
-# WARNING! All changes made in this file will be lost!
-
