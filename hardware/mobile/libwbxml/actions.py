bdc5133124f2e4a48377564714796c252351f3d0
Ertuğrul <ertugrulerata@gmail.com>
2014-02-01 22:32:55 +0200

    libwbxml headers in /usr/include..

--- hardware/mobile/libwbxml/actions.py
+++ hardware/mobile/libwbxml/actions.py
@@ -12,8 +12,9 @@ from pisi.actionsapi import get
 def setup():
     shelltools.makedirs("build")
     shelltools.cd("build")
-    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr", sourceDir="..")
-
+    cmaketools.configure("-DLIBWBXML_INCLUDE_DIR=/usr/include \
+                          -DCMAKE_INSTALL_PREFIX=/usr", sourceDir="..")      
+                          
 def build():
     shelltools.cd("build")
     cmaketools.make()
