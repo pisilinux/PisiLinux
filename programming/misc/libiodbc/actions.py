fc33940273dfcb00e0a038573cc13fc95f562b86
obsoleteman <tulliana@gmail.com>
2014-02-04 02:23:17 +0200

    libiodbc: remove rpath bomb and unneded lib*.la files

--- programming/misc/libiodbc/actions.py
+++ programming/misc/libiodbc/actions.py
@@ -6,21 +6,31 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-if")
+    
     autotools.configure("--disable-gui \
                          --includedir=/usr/include/iodbc \
                          --with-iodbc-inidir=/etc/iodbc \
                          --with-layout=gentoo \
+                         --disable-libodbc \
                          --disable-static")
 
 def build():
     autotools.make()
+    
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    
+    shelltools.system("chrpath --delete %s/usr/bin/iodbctest" % get.installDIR())
+    shelltools.system("chrpath --delete %s/usr/bin/iodbctestw" % get.installDIR())
+    
+    pisitools.remove("/usr/lib/*la")
+    
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
 
     pisitools.domove("/usr/share/libiodbc/samples", "/usr/share/doc/libiodbc")
