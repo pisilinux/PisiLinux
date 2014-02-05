5c038b19f4e62865e52628d05bf0aeb5d4be32a9
marcin- <marcin.bojara@gmail.com>
2014-02-05 19:37:00 +0100

    virtuoso: fix build

--- server/virtuoso/actions.py
+++ server/virtuoso/actions.py
@@ -4,15 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-
-WorkDir = "%s-opensource-%s" % (get.srcNAME(), get.srcVERSION())
 
 def setup():
-    autotools.autoreconf("-fi")
+    pisitools.dosed("configure", "(virt_iodbc_dir\/include)", r"\1/iodbc")
+    pisitools.dosed("configure", '(iodbc_CPPFLAGS=)"[^"]+"', r"\1`iodbc-config --cflags`")
+    pisitools.dosed("configure", '(iodbc_LDFLAGS=)"[^"]+"', r"\1`iodbc-config --libs`")
+
+    pisitools.dosed("configure", '\s+C[X]*FLAGS.*\$SED.*', deleteLine=True)
 
     autotools.configure("--localstatedir=/var \
                          --enable-shared \
@@ -20,16 +22,18 @@ def setup():
                          --without-internal-zlib \
                          --with-debug \
                          --with-iodbc \
-                         --disable-demo-vad \
+                         --with-readline \
+                         --disable-all-vads \
                          --disable-hslookup \
                          --enable-openssl \
                          --enable-xml")
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
 
 def build():
-    #tests fail with parallel build
-    autotools.make("-j1")
+    autotools.make()
 
 """
 def check():
