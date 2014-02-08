9ccbdbc2f81466e82ea9e2b2cbe3c54ef0682d7d
namso-01 <namso-01@hotmail.it>
2014-02-08 10:23:16 +0200

    mate-applets: fixed

--- desktop/mate/extras/mate-applets/actions.py
+++ desktop/mate/extras/mate-applets/actions.py
@@ -10,6 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    pisitools.ldflags.add("-lcpufreq")
     autotools.configure("--prefix=/usr \
                          --sysconfdir=/etc \
                          --libexecdir=/usr/lib/mate-applets \
