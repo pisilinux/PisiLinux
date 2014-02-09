1d977cb062e9685d1e1d12f0f886f8769d5b3d3f
waroi <waroi@yandex.com>
2014-02-10 01:16:43 +0200

    telepathy-farsight: rebuild unused

--- network/chat/telepathy-farsight/actions.py
+++ network/chat/telepathy-farsight/actions.py
@@ -11,6 +11,8 @@ from pisi.actionsapi import get
 def setup():
     autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
