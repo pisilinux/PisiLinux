690a85b04d199d56007fa831bf01c7a64695fad9
obsoleteman <tulliana@gmail.com>
2014-02-12 20:46:56 +0200

    mtools: fix

--- hardware/disk/mtools/actions.py
+++ hardware/disk/mtools/actions.py
@@ -16,18 +16,16 @@ def setup():
     shelltools.export("INSTALL_PROGRAM", "install")
     autotools.autoreconf("-fi")
     autotools.configure("--prefix=/usr \
-			 --sysconfdir=/etc/mtools \
-                         --includedir=/usr/src/linux/include \
-                         ")
+			             --sysconfdir=/etc/mtools \
+                         --includedir=/usr/src/linux/include")
 
 def build():
     autotools.make()
-
+    pisitools.dosed("mtools.conf","SAMPLE FILE","#SAMPLE FILE")
 def install():
-    # autotools.install("sysconfdir=%s/etc/mtools" % get.installDIR())
     autotools.rawInstall('-j1 DESTDIR="%s"' % get.installDIR())
     
 
     pisitools.insinto("/etc/mtools","mtools.conf")
-
+    
     pisitools.dodoc("COPYING", "README*", "Release.notes")
