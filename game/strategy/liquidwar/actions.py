474a530a6ac5d272f1974e0e93f7a8fb3fe4b51e
waroi <waroi@yandex.com>
2014-02-15 01:28:02 +0200

    liquidwar: rebuild unused

--- game/strategy/liquidwar/actions.py
+++ game/strategy/liquidwar/actions.py
@@ -15,6 +15,8 @@ def setup():
                          --enable-mod-gl \
                          --disable-nls \
                          --disable-rpath")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
