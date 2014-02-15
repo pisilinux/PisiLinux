c2a55d922202b474cd5ad87f30a0d7b15123d44c
namso-01 <namso-01@hotmail.it>
2014-02-15 12:45:59 +0200

    user-manager: fixed

--- desktop/pisilinux/user-manager/actions.py
+++ desktop/pisilinux/user-manager/actions.py
@@ -6,8 +6,6 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def install():
     pythonmodules.install()
