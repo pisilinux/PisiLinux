2e94e68c226114d195a02d79ec6e5d829c7d38b8
namso-01 <namso-01@hotmail.it>
2014-02-15 22:16:12 +0200

    easytag: fixed

--- multimedia/editor/easytag/actions.py
+++ multimedia/editor/easytag/actions.py
@@ -9,15 +9,15 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-	autotools.autoreconf("-vif")
-	autotools.configure()
+    pisitools.dosed("data/easytag.desktop.in", "Icon=easytag", "Icon=/usr/share/pixmaps/easytag.png")
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
-	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-	
-	pisitools.dodoc("TODO", "COPYING", "README", "ChangeLog", "THANKS")
-	
-	pisitools.dohtml("doc/EasyTAG_Documentation.html")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+        
+    pisitools.dodoc("TODO", "COPYING", "README", "ChangeLog", "THANKS")
+        
+    pisitools.insinto("/usr/share/pixmaps/", "data/icons/48x48/easytag.png")
