9f21366baff69dd53c38dbb5a4c7084a8bac9c28
namso-01 <namso-01@hotmail.it>
2014-02-15 11:35:51 +0200

    user-manager: desktop dosed

--- desktop/pisilinux/user-manager/actions.py
+++ desktop/pisilinux/user-manager/actions.py
@@ -5,6 +5,9 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
 
 def install():
     pythonmodules.install()
+    
+    pisitools.dosed("%s/usr/share/applications/user-manager.desktop" % get.installDIR(), "Categories=Qt;KDE;System;", "Categories=Qt;KDE;X-KDE-settings-system;")
