a1da026e161a3d631f36b443b4a1aa861b99e515
namso-01 <namso-01@hotmail.it>
2014-02-15 12:56:41 +0200

    back

--- desktop/pisilinux/user-manager/actions.py
+++ desktop/pisilinux/user-manager/actions.py
@@ -5,9 +5,5 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
-
 def install():
     pythonmodules.install()
-    
-    pisitools.insinto("/usr/share/kde4/services/", "data/kcm_user-manager.desktop")
