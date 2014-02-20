62ba17c29ccde7b79dabbdeb326beb0eeb63e891
aydemir <yusuf.aydemir@istanbul.com>
2014-02-20 20:55:26 +0200

    epson-acl1100 epson yazıcı sürücüsü

---
+++ hardware/printer/epson-alc1100/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "Epson-ALC1100-filter-%s" % get.srcVERSION()
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("README-alc1100", "NEWS", "ChangeLog", "AUTHORS", "COPYING", "EAPL.en.txt")
+
