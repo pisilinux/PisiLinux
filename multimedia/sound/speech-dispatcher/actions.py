7e60b2cc252397ba54049b4e93c389f1cd5377cc
marcin- <marcin.bojara@gmail.com>
2014-02-20 22:26:50 +0100

    speech-dispatcher: version bump

--- multimedia/sound/speech-dispatcher/actions.py
+++ multimedia/sound/speech-dispatcher/actions.py
@@ -4,10 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     autotools.autoreconf("-vfi")
@@ -29,15 +29,8 @@ def install():
     # Conflicts with openTTS
     pisitools.remove("/usr/share/info/ssip.info")
 
-    # Rename generically named binaries
-    pisitools.rename("/usr/bin/long_message", "spd_long_message")
-    pisitools.rename("/usr/bin/run_test", "spd_run_test")
-
-    # Remove configuration files from /usr/share
-    pisitools.removeDir("/usr/share/speech-dispatcher")
-
     # Set executable bit
-    shelltools.chmod("%s/usr/lib/%s/site-packages/speechd/_test.py" % (get.installDIR(), get.curPYTHON()), 0755)
+    shelltools.chmod("%s/usr/lib/python3.3/site-packages/speechd/_test.py" % get.installDIR(), 0755)
 
     # Create log directory, it should be world unreadable
     pisitools.dodir("/var/log/speech-dispatcher")
