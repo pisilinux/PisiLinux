80cf6765b9be1fadfda72c0c1eef998b5b8acbbd
waroi <waroi@yandex.com>
2014-02-22 04:11:20 +0200

    libjingle: rebuild unused

--- multimedia/sound/libjingle/actions.py
+++ multimedia/sound/libjingle/actions.py
@@ -15,6 +15,8 @@ def setup():
     shelltools.unlink("talk/base/time.h")
     autotools.autoreconf("-i")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
