00d2d26cf299b705a12d8ea90928c6c984c689c7
waroi <waroi@yandex.com>
2014-02-22 04:18:54 +0200

    libindicate: rebuild unused

--- programming/misc/libindicate/actions.py
+++ programming/misc/libindicate/actions.py
@@ -16,6 +16,8 @@ def setup():
                          --enable-gtk-doc \
                          --enable-introspection=yes \
                          --disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
