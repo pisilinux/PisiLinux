fbb4455a75dee92186067b8ea112e93a18de72f1
alihanozturk <alihan@pisilinux.org>
2014-02-23 17:48:26 +0200

    esound:rebuild

--- multimedia/sound/esound/actions.py
+++ multimedia/sound/esound/actions.py
@@ -20,6 +20,8 @@ def setup():
                          --disable-arts \
                          --disable-dependency-tracking \
                          --sysconfdir=/etc/esd")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
