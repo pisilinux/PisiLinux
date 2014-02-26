a65684be615021fb1e9d10ed4b0ce6839750e1ad
obsoleteman <tulliana@gmail.com>
2014-02-26 05:51:15 +0200

    google-talk-plugin: fix

--- network/chat/google-talkplugin/actions.py
+++ network/chat/google-talkplugin/actions.py
@@ -10,8 +10,8 @@ from pisi.actionsapi import get
 
 WorkDir = "%s" % get.ARCH()
 
-def install():
-    # Define bits variable for 32-bit and 64-bit architectures
+def setup():
+        # Define bits variable for 32-bit and 64-bit architectures
     bits = "amd64" if get.ARCH() == "x86_64" else "i386"
 
     # Extract Debian package
@@ -20,15 +20,17 @@ def install():
     # Extract data.tar.gz, we use some exclude parameters here
     # because otherwise we get fchownat related sandbox errors
     shelltools.system("tar -xzvf data.tar.gz --exclude 'usr/lib/*' --exclude 'usr/lib'")
-
+    
+    shelltools.system("rm -rf opt/google/talkplugin/cron")
+    
     # Extract changelog file
     shelltools.system("gzip -dfq usr/share/doc/google-talkplugin/changelog.Debian.gz")
+    
+
+def install():
 
     # Copying all needed stuff to /opt
     pisitools.insinto("/opt/google/talkplugin", "opt/google/talkplugin/*")
-
-    # We don't need cron stuff
-    #pisitools.removeDir("/opt/google/talkplugin/cron")
 
     # All doc files go to proper directory
     pisitools.domove("/opt/google/talkplugin/attributions.txt", "/usr/share/doc/google-talkplugin")
