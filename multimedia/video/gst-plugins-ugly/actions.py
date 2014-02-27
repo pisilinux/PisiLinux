7899a851f75fc1404b04f0fc1361c478faa764cc
waroi <waroi@yandex.com>
2014-02-27 20:39:01 +0200

    gst-plugins-ugly: rebuild unused

--- multimedia/video/gst-plugins-ugly/actions.py
+++ multimedia/video/gst-plugins-ugly/actions.py
@@ -19,6 +19,8 @@ def setup():
                          --disable-sidplay \
                          --with-package-name=\"PisiLinux gstreamer-plugins-ugly package\" \
                          --with-package-origin=\"http://www.pisilinux.org/eng\"")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
