aab2f76123551f0c1bcb37146b62ee97f895d9dd
waroi <waroi@yandex.com>
2014-02-27 03:32:04 +0200

    gtkglarea: rebuild unused

--- programming/misc/gtkglarea/actions.py
+++ programming/misc/gtkglarea/actions.py
@@ -16,6 +16,8 @@ def setup():
     autotools.autoreconf("-vif")
     autotools.configure("--with-lib-GL \
                          --disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
