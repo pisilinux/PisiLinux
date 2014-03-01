81a6b6f111b821ca2c832a41ae90ff3079095b99
waroi <waroi@yandex.com>
2014-03-01 03:07:05 +0200

    e-module-taskbar: rebuild unused

--- desktop/enlightenment/addon/e-module-taskbar/actions.py
+++ desktop/enlightenment/addon/e-module-taskbar/actions.py
@@ -13,6 +13,8 @@ WorkDir="taskbar"
 def setup():
     autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
