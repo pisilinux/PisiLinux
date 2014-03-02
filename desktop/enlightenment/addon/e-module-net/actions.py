bafbf39c15f5ec5541f087ff0d468076a4d37ab2
waroi <waroi@yandex.com>
2014-03-02 03:12:59 +0200

    e-module-net: rebuild unused

--- desktop/enlightenment/addon/e-module-net/actions.py
+++ desktop/enlightenment/addon/e-module-net/actions.py
@@ -15,6 +15,8 @@ def setup():
     autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
                          --disable-rpath")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
