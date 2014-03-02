7df9c8a7cc1a869975f6b282c3181c3fcd3066d7
marcin- <marcin.bojara@gmail.com>
2014-03-02 11:54:38 +0100

    Revert "upower: version bump"
    This reverts commit d415c4a44f1bd0ccee42a4aadd4adf5359a28ffe.

--- hardware/powermanagement/upower/actions.py
+++ hardware/powermanagement/upower/actions.py
@@ -4,16 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+# For g-i-r
+shelltools.export("HOME", get.workDIR())
 
 def setup():
+    pisitools.dosed("configure", "DISABLE_DEPRECATED", deleteLine=True)
     autotools.configure("--disable-static \
                          --disable-gtk-doc \
                          --enable-deprecated \
                          --enable-introspection")
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
