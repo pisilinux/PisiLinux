be4fe7a6c78b58c3dd6bb9e613df0a2eb5d2007e
waroi <waroi@yandex.com>
2014-03-02 03:16:42 +0200

    e-module-mpdule: rebuild unused

--- desktop/enlightenment/addon/e-module-mpdule/actions.py
+++ desktop/enlightenment/addon/e-module-mpdule/actions.py
@@ -15,6 +15,8 @@ def setup():
     shelltools.touch("README")
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
