8e7edc69360c7f3a46c6c932c753bb7d95ff2b05
namso-01 <namso-01@hotmail.it>
2014-03-02 12:09:00 +0200

    upower/actions.py: add dosed

--- hardware/powermanagement/upower/actions.py
+++ hardware/powermanagement/upower/actions.py
@@ -9,6 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
+    pisitools.dosed("configure", "DISABLE_DEPRECATED", deleteLine=True)
     autotools.configure("--disable-static \
                          --disable-gtk-doc \
                          --enable-deprecated \
