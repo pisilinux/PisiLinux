823731d7ba84e3ca11e4930a5a14708580c98371
waroi <waroi@yandex.com>
2014-03-03 03:44:19 +0200

    e-module-mem: rebuild unused

--- desktop/enlightenment/addon/e-module-mem/actions.py
+++ desktop/enlightenment/addon/e-module-mem/actions.py
@@ -14,6 +14,8 @@ def setup():
 
     autotools.autoreconf("-fiv")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
