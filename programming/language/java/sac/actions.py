693b9addc766d7339de5fd673563b443b1d2f0a4
namso-01 <namso-01@hotmail.it>
2014-03-03 22:34:22 +0200

    sac: rebuild for openjdk

--- programming/language/java/sac/actions.py
+++ programming/language/java/sac/actions.py
@@ -10,8 +10,8 @@ from pisi.actionsapi import get
 
 def setup():
     shelltools.makedirs("src")
-    shelltools.export("JAVAC","/opt/sun-jdk/bin/javac")
-    shelltools.export("JAVA_HOME","/opt/sun-jdk")
+    shelltools.export("JAVAC","/usr/lib/jvm/java-7-openjdk/bin/javac")
+    shelltools.export("JAVA_HOME","/usr/lib/jvm/java-7-openjdk")
     shelltools.export("LC_ALL", "C")
     shelltools.system("ant jar")
     shelltools.system("touch LICENSE.txt")
