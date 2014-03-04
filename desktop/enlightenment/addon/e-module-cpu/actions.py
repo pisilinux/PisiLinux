6d378e3605f0e994e75d5a29d0b503856eda1eed
waroi <waroi@yandex.com>
2014-03-05 00:30:55 +0200

    e-module-cpu: rebuild unused

--- desktop/enlightenment/addon/e-module-cpu/actions.py
+++ desktop/enlightenment/addon/e-module-cpu/actions.py
@@ -14,6 +14,8 @@ def setup():
     shelltools.export("AUTOPOINT", "/bin/true")
     shelltools.system("./autogen.sh")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
