cbd9583d9b892352ece71027db7ea08e0645c493
waroi <waroi@yandex.com>
2014-03-06 00:45:30 +0200

    e-module-calendar: rebuild unused

--- desktop/enlightenment/addon/e-module-calendar/actions.py
+++ desktop/enlightenment/addon/e-module-calendar/actions.py
@@ -16,6 +16,8 @@ def setup():
 
     shelltools.system("./autogen.sh")
     autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
