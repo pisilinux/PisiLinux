40f3bc36f7a6378bb75daed4eabdc652b4e0130d
alihanozturk <alihan@pisilinux.org>
2014-03-08 21:21:39 +0200

    wxMaxima:fix

--- science/mathematics/wxMaxima/actions.py
+++ science/mathematics/wxMaxima/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--prefix=/usr")
+    autotools.configure("--enable-printing \
+                         --enable-unicode-glyphs")
 
 def build():
     autotools.make()
