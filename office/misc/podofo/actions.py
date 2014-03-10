373fac99899ade36d3a52403aba976fc4d84f9b3
obsoleteman <tulliana@gmail.com>
2014-03-11 00:35:06 +0200

    podofo: fix build

--- office/misc/podofo/actions.py
+++ office/misc/podofo/actions.py
@@ -6,23 +6,30 @@
 
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    cmaketools.configure("-DPODOFO_BUILD_SHARED=1 \
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DFREETYPE_INCLUDE_DIR=/usr/include/freetype2 \
+                          -DPODOFO_BUILD_SHARED=1 \
                           -DPODOFO_BUILD_STATIC=0 \
                           -DPODOFO_HAVE_JPEG_LIB=1 \
                           -DPODOFO_HAVE_PNG_LIB=1 \
                           -DPODOFO_HAVE_TIFF_LIB=1 \
                           -DWANT_FONTCONFIG=1 \
                           -DWANT_BOOST=1 \
-                          -DUSE_STLPORT=1")
+                          -DUSE_STLPORT=1", sourceDir="..")
 
 def build():
+    shelltools.cd("build")
     cmaketools.make()
 
 def install():
+    shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    
+    shelltools.cd("..")
     pisitools.dodoc("AUTHORS", "COPYING", "COPYING.LIB", "ChangeLog", "NEWS", "TODO")
     pisitools.dohtml("README.html", "FAQ.html")
