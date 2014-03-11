5dba2dfa8e4aa67f36dbafa3eb756b4af1b6038a
obsoleteman <tulliana@gmail.com>
2014-03-11 04:02:19 +0200

    smbldap-tools: fix build

--- network/share/smbldap-tools/actions.py
+++ network/share/smbldap-tools/actions.py
@@ -6,27 +6,18 @@
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 def setup():
-    #pisitools.dosed("smbldap_tools.pm", "/etc/opt/IDEALX/", "/etc/smbldap-tools/")
-    pisitools.dosed("smbldap.conf", "/etc/opt/IDEALX/", "/etc/smbldap-tools/")
-    #pisitools.dosed("smbldap_tools.pm", "/opt/IDEALX/", "/etc/smbldap-tools/")
-    pisitools.dosed("smbldap.conf", "/opt/IDEALX/", "/etc/smbldap-tools/")
+    autotools.configure()
 
-    # Set SSL certs directory
-    pisitools.dosed("smbldap.conf", "/etc/pki/tls/certs/", "/etc/ssl/certs/")
-
-    # Generate manpages
-    for f in shelltools.ls("smbldap-*"):
-        shelltools.system("pod2man --section=8 %s > doc/%s.8" % (f, f))
+def  build():
+    autotools.make()
 
 def install():
-    pisitools.dosbin("smbldap-*")
-    pisitools.remove("/usr/sbin/smbldap-tools.spec")
-
-    #pisitools.insinto("/usr/lib/perl5/vendor_perl/%s/" % get.curPERL(), "smbldap_tools.pm")
-
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
     pisitools.dodir("/etc/smbldap-tools")
     pisitools.insinto("/etc/smbldap-tools", "smbldap.conf")
     pisitools.insinto("/etc/smbldap-tools", "smbldap_bind.conf")
@@ -34,4 +25,4 @@ def install():
     pisitools.dodoc("CONTRIBUTORS", "COPYING", "ChangeLog", "FILES", "INFRA", \
                     "README", "TODO")
 
-    pisitools.doman("doc/*.8")
+    pisitools.doman("*.8")
