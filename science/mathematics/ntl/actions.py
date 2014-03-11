d1271837a35abdbd64f392643fdc6b35b1a2bf1f
namso-01 <namso-01@hotmail.it>
2014-03-11 22:35:17 +0200

    ntl: v.bump fixed gf2x: new package

--- science/mathematics/ntl/actions.py
+++ science/mathematics/ntl/actions.py
@@ -11,30 +11,24 @@ from pisi.actionsapi import get
 
 def setup():
     # Filter out -nostdlib from libtool
-    shelltools.copy("/usr/bin/libtool", "src")
-    pisitools.dosed("src/libtool", "-nostdlib ", "")
+    #shelltools.copy("/usr/bin/libtool", "src")
+    #pisitools.dosed("src/libtool", "-nostdlib ", "")
 
+    shelltools.system("sed -i.mode \
+                       -e 's| --mode=compile| --tag=CC --mode=compile|' \
+                       -e 's| --mode=link| --tag=LD --mode=link|' \
+                       src/def_makefile \
+                       src/makefile \
+                       src/mfile")
+    
     shelltools.cd("src")
-    autotools.rawConfigure("PREFIX=/usr \
-                            INCLUDEDIR=/usr/include \
-                            LIBDIR=/usr/lib \
-                            LIBTOOL=./libtool \
-                            CC=%s \
-                            CXX=%s \
-                            CFLAGS=\"%s -fPIC\" \
-                            CXXFLAGS=\"%s -fPIC\" \
-                            LDFLAGS=\"%s\" \
-                            NTL_GMP_LIP=on \
-                            SHARED=on" % (get.CC(), get.CXX(),
-                                          get.CFLAGS().replace("-O2", "-O1"),
-                                          get.CXXFLAGS().replace("-O2", "-O1"),
-                                          get.LDFLAGS().replace("-Wl,--hash-style=gnu ", "")))
+    autotools.rawConfigure("DEF_PREFIX=/usr SHARED=on NTL_GF2X_LIB=on NTL_GMP_LIP=on")
 
 def build():
-    autotools.make("-C src")
+    autotools.make("-C src -j1")
 
-#def check():
-    #autotools.make("check")
+def check():
+    autotools.make("-C src -k check -j1")
 
 def install():
     shelltools.cd("src")
