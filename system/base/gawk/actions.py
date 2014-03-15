c3d11e8dd5be7476aeea261fc0b26f2a38aaec50
BlackSEA- <blacksea8393@gmail.com>
2014-03-16 01:36:43 +0200

    gawk:back to old config.fix linking libsigsegv issue

--- system/base/gawk/actions.py
+++ system/base/gawk/actions.py
@@ -4,25 +4,29 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("\
-                         --without-libsigsegv \
+    shelltools.export("ac_cv_libsigsegv", "no")
+    shelltools.export("AUTOPOINT", "true")
+    autotools.configure("--with-libsigsegv-prefix=no \
                          --enable-switch \
-                         --enable-nls \
-                        ")
+                         --enable-nls")
 
 def build():
     autotools.make()
+
+def check():
+    autotools.make("-j1 check diffout")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Remove versioned binaries
-    pisitools.remove("/usr/bin/*-*")
+    pisitools.remove("/usr/bin/*-%s" % get.srcVERSION())
 
     pisitools.dosym("gawk.1", "/usr/share/man/man1/awk.1")
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
