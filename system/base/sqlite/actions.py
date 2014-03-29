a3a5e8dedfbe8386cfaf5113f6afd4cb6b84b7da
marcin- <marcin.bojara@gmail.com>
2014-03-29 17:47:55 +0100

    sqlite: version bump

--- system/base/sqlite/actions.py
+++ system/base/sqlite/actions.py
@@ -13,14 +13,15 @@ from pisi.actionsapi import get
     # but cannot be seen with sqlite query. However, it can be seen by opening the file with a text editor.
     # SQLITE_SECURE_DELETE overwrites written data with zeros.
 def setup():
-    shelltools.export("CFLAGS", "%s \
-                       -DSQLITE_SECURE_DELETE=1 \
-                       -DSQLITE_ENABLE_UNLOCK_NOTIFY=1 \
-                       -DSQLITE_ENABLE_COLUMN_METADATA=1 \
-                       -DSQLITE_DISABLE_DIRSYNC=1 \
-                       -DSQLITE_ENABLE_FTS3=3 \
-                       -DSQLITE_ENABLE_RTREE=1 \
-                       -O3 -DNDEBUG=1 -fno-strict-aliasing" % get.CFLAGS())
+    pisitools.cflags.add("-DSQLITE_SECURE_DELETE=1",
+                         "-DSQLITE_ENABLE_UNLOCK_NOTIFY=1",
+                         "-DSQLITE_ENABLE_COLUMN_METADATA=1",
+                         "-DSQLITE_DISABLE_DIRSYNC=1",
+                         "-DSQLITE_ENABLE_FTS3=3",
+                         "-DSQLITE_ENABLE_RTREE=1",
+                         "-DNDEBUG=1",
+                         "-fno-strict-aliasing")
+    pisitools.cflags.sub("-O[s\d]", "-O3")
     autotools.configure("--disable-static \
                          --enable-readline \
                          --enable-threadsafe")
