b95a71f63d9b8c3c987bf3e147eee708a887cee2
marcin- <marcin.bojara@gmail.com>
2014-03-30 16:39:51 +0200

    automake: version bump

--- system/devel/automake/actions.py
+++ system/devel/automake/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-ver = "1.13.1"
+#ver = "1.14"
 
 def setup():
     #autotools.autoreconf("-fi")
@@ -24,8 +24,8 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Use gnuconfig files
-    for config in ["config.guess","config.sub"]:
-        #pisitools.remove("/usr/share/automake-%s/%s" % (ver, config))
-        pisitools.dosym("/usr/share/gnuconfig/%s" % config, "/usr/share/automake-%s/%s" % (ver, config))
+#    for config in ["config.guess","config.sub"]:
+#        pisitools.remove("/usr/share/automake-%s/%s" % (ver, config))
+#        pisitools.dosym("/usr/share/gnuconfig/%s" % config, "/usr/share/automake-%s/%s" % (ver, config))
 
     pisitools.dodoc("NEWS", "README", "THANKS","AUTHORS", "ChangeLog*")
