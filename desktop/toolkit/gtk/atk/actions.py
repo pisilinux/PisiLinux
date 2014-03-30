50d0ed19322e1774b4ea6b55b39e522a37c45899
marcin- <marcin.bojara@gmail.com>
2014-03-30 18:58:43 +0200

    atk:version bump

--- desktop/toolkit/gtk/atk/actions.py
+++ desktop/toolkit/gtk/atk/actions.py
@@ -4,13 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed(".", "^(SUBDIRS =.*)tests(.*)$", "\\1\\2", filePattern="^Makefile.(am|in)", level=0)
     autotools.autoreconf("-fiv")
-    autotools.configure()
+    autotools.configure("\
+                         --disable-static \
+                         --enable-introspection=%s \
+                        " % ("no" if get.buildTYPE() == "emul32" else "yes"))
 
 def build():
     autotools.make()
