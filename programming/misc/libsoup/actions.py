3cdcff135251c21847e81c667625c71956711500
marcin- <marcin.bojara@gmail.com>
2014-03-30 18:16:16 +0200

    libsoup: version bump

--- programming/misc/libsoup/actions.py
+++ programming/misc/libsoup/actions.py
@@ -4,15 +4,15 @@
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-static \
                          --without-apache-httpd \
                          --without-apache-module-dir")
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
