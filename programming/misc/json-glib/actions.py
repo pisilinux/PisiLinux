8ca764c74346c9cf571e61add1c8e86b63b373bf
marcin- <marcin.bojara@gmail.com>
2014-03-30 17:56:12 +0200

    json-glib: version bump

--- programming/misc/json-glib/actions.py
+++ programming/misc/json-glib/actions.py
@@ -4,12 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
-shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.autoreconf("-fi")
