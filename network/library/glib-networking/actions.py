b15d956c40473c4008ddb59e4ce52bea3b505070
marcin- <marcin.bojara@gmail.com>
2014-03-30 18:37:35 +0200

    glib-networking: version bump

--- network/library/glib-networking/actions.py
+++ network/library/glib-networking/actions.py
@@ -3,12 +3,14 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--disable-static \
+                         --disable-installed-tests \
+                         --with-ca-certificates=/etc/ssl/certs/ca-certificates.crt \
                          --with-libproxy \
                          --with-gnutls \
                          --with-pkcs11")
