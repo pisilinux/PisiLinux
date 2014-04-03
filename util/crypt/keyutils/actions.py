fb1f2409c4293bb41c42e8f978ebafd773985d6c
marcin- <marcin.bojara@gmail.com>
2014-04-03 18:43:05 +0200

    keyutils: version bump

--- util/crypt/keyutils/actions.py
+++ util/crypt/keyutils/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def build():
     autotools.make("NO_ARLIB=1 LIBDIR=/lib USRLIBDIR=/usr/lib NO_GLIBC_KEYERR=1 CFLAGS=\"%s\"" % get.CFLAGS())
