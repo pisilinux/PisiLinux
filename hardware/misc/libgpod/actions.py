613ba96a56466edca71f969773ac0848eba9d8b3
marcin- <marcin.bojara@gmail.com>
2014-04-05 12:00:09 +0200

    libgpod: version bump

--- hardware/misc/libgpod/actions.py
+++ hardware/misc/libgpod/actions.py
@@ -4,13 +4,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
                          --disable-gtk-doc \
                          --without-hal \
@@ -18,7 +17,7 @@ def setup():
                          --with-temp-mount-dir=/run/libgpod \
                          --enable-udev \
                          --with-html-dir=/usr/share/doc/%s/html" % get.srcNAME())
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
