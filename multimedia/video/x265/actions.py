e0b8e3774fded89c188f04c38e4d5ed3ee26d25b
obsoleteman <tulliana@gmail.com>
2014-04-05 17:10:08 +0300

    x265: new package

---
+++ multimedia/video/x265/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.ldflags.add("-z,noexecstack")
+    shelltools.cd("build/linux")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release", sourceDir="../../source")
+
+def build():
+    shelltools.cd("build/linux")
+    cmaketools.make()
+
+    
+def install():
+    shelltools.cd("build/linux")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.remove("/usr/lib/libx265.a")
+
+
