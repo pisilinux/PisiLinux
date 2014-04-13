b56b397dcb47b493605e3968fe7c7f0d52938c89
suvari <suvarice@gmail.com>
2014-04-13 13:10:55 +0300

    bridge-xcursor: new package

---
+++ desktop/lookandfeel/bridge-xcursor/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+WorkDir = "./"
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def install():
+    pisitools.insinto("/usr/share/icons", "bridge")
