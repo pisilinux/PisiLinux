7b55fafb7acc16e9208ceb4a2944cf3a9dcd2d1d
obsoleteman <tulliana@gmail.com>
2014-04-13 14:23:28 +0300

    icon-theme-nitrux:fix

--- desktop/lookandfeel/icon-theme-nitrux/actions.py
+++ desktop/lookandfeel/icon-theme-nitrux/actions.py
@@ -4,14 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-WorkDir = "./"
+WorkDir = "."
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-
+def setup():
+    shelltools.unlink("%s/NITRUX-KDE/apps/scalable/linuxdcpp.sv*" % get.workDIR())
 
 def install():
-	pisitools.insinto("/usr/share/icons", "NITRUX-KDE")
+    pisitools.insinto("/usr/share/icons", "NITRUX-KDE")
 
