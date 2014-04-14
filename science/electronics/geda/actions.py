a770f44fb669da25458707e1758e24f65e1bea05
BlackSEA- <blacksea8393@gmail.com>
2014-04-14 19:27:17 +0300

    geda:remove copyleft

--- science/electronics/geda/actions.py
+++ science/electronics/geda/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
