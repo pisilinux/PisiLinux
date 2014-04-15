6bec08a22a2aa2c54da1ae908d8ba63ebbf2cb8b
marcin- <marcin.bojara@gmail.com>
2014-04-15 13:34:29 +0200

    usbmuxd: version bump

--- hardware/mobile/usbmuxd/actions.py
+++ hardware/mobile/usbmuxd/actions.py
@@ -4,17 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
 
 def setup():
-    cmaketools.configure("-DCMAKE_SKIP_RPATH=ON")
+    autotools.configure("\
+                         --disable-static \
+                         --disable-silent-rules \
+                        ")
 
 def build():
-    cmaketools.make()
+    autotools.make()
 
 def install():
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING*", "README", "README.devel")
+    pisitools.dodoc("AUTHORS", "README")
