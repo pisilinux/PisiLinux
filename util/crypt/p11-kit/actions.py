cd3938849b2bad4b930e9ce9fa03cca8cb13b2ac
marcin- <marcin.bojara@gmail.com>
2014-04-20 22:04:55 +0200

    p11-kit: version bump

--- util/crypt/p11-kit/actions.py
+++ util/crypt/p11-kit/actions.py
@@ -4,17 +4,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    if get.buildTYPE() == "emul32":
-        options = " --with-module-path=/usr/lib32/pkcs11"
-    else:
-        options = " --with-module-path=/usr/lib/pkcs11"
-
-    autotools.configure(options)
+    autotools.configure("--with-module-path=/usr/lib%s/pkcs11" % ("32" if get.buildTYPE() == "emul32" else ""))
 
 def build():
     autotools.make()
@@ -23,5 +18,5 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     if get.buildTYPE() == "emul32": return
-    
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
