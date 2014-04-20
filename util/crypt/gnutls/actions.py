bf2a46658aca183ffee6e0d973451b5080071434
marcin- <marcin.bojara@gmail.com>
2014-04-20 22:05:13 +0200

    gnutls: version bump

--- util/crypt/gnutls/actions.py
+++ util/crypt/gnutls/actions.py
@@ -4,20 +4,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
     options = "--disable-static \
                --disable-rpath \
-               --disable-dependency-tracking \
+               --disable-silent-rules \
                --disable-guile \
                --with-zlib \
                --disable-valgrind-tests"
 
     if get.buildTYPE() == "emul32":
-        options += " --disable-hardware-acceleration"
+        options += " --disable-hardware-acceleration \
+                     --disable-valgrind-tests \
+                     --enable-local-libopts"
 
     autotools.configure(options)
 
