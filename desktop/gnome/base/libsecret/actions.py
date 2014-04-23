0a5b8593002981b6df56d6e5d179c17927b058d9
marcin- <marcin.bojara@gmail.com>
2014-04-23 22:01:08 +0200

    libsecret: version bump

--- desktop/gnome/base/libsecret/actions.py
+++ desktop/gnome/base/libsecret/actions.py
@@ -4,13 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-static")
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
