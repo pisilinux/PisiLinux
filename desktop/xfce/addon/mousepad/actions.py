752074f57f0e84db38d1ee26eb233d97f8cf872d
BlackSEA- <blacksea8393@gmail.com>
2014-04-28 19:06:55 +0300

    mousepad:new package

---
+++ desktop/xfce/addon/mousepad/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.system("./autogen.sh --prefix=/usr \
+                        --libexecdir=/usr/lib/xfce4 \
+                        --disable-static \
+                        --disable-debug")
+
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.domo("po/tr.po", "tr", "mousepad.mo")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
