9bcc978b91e4a4ba35858ca06e8ce38675d7c5b9
BlackSEA- <blacksea8393@gmail.com>
2014-05-01 00:34:53 +0300

    elementary:version bump

--- desktop/enlightenment/base/elementary/actions.py
+++ desktop/enlightenment/base/elementary/actions.py
@@ -9,36 +9,16 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-#    autotools.autoreconf("-vfi")
+    #autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
-                         --enable-emap \
-                         --enable-eweather \
-                         --enable-quick-launch \
-                         --enable-emotion \
-                         --enable-ecore-x \
-                         --enable-ecore-fb \
-                         --disable-ecore-sdl \
-                         --disable-ecore-wayland \
-                         --disable-ecore-cocoa \
-                         --disable-ecore-psl1ght \
-                         --disable-ecore-win32 \
-                         --disable-ecore-wince \
-                         --enable-edbus \
-                         --enable-efreet \
-                         --enable-ethumb \
-                         --disable-debug \
-                         --disable-install-examples \
                          --disable-doc")
 
-    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
-    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
     pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
-    pisitools.dosed("src/lib/.deps/libelementary_la-els_box.Plo", "inalude", "include")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "COPYING", "README")
