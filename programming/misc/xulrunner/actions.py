8bd64fb3021136415bb60c8edb652c7a4721f7b1
namso-01 <namso-01@hotmail.it>
2014-05-01 17:41:41 +0300

    xulrunner: v.bump

--- programming/misc/xulrunner/actions.py
+++ programming/misc/xulrunner/actions.py
@@ -13,19 +13,19 @@ import os
 
 WorkDir = "mozilla-release"
 NoStrip = ["/usr/include", "/usr/share/idl"]
-XulVersion = "28.0"
+XulVersion = "29.0"
 XulDir = "/usr/lib/%s-%s" % (get.srcNAME(), XulVersion)
 ObjDir = "obj-%s-unknown-linux-gnu" % get.ARCH() if get.ARCH() == "x86_64" else "obj-%s-pc-linux-gnu" % get.ARCH()
 
 def setup():
     pisitools.ldflags.add("-Wl,-rpath,/usr/lib/xulrunner-%s" % XulVersion)
     # Fix build with new freetype
-    pisitools.dosed(".", "freetype\/(.*\.h)", r"\1", filePattern="system-headers")
-    pisitools.dosed("gfx/", "freetype\/(.*\.h)", r"\1", filePattern=".*\.cpp$")
-    pisitools.dosed("gfx/", "freetype\/(.*\.h)", r"\1", filePattern=".*\.h$")
+    #pisitools.dosed(".", "freetype\/(.*\.h)", r"\1", filePattern="system-headers")
+    #pisitools.dosed("gfx/", "freetype\/(.*\.h)", r"\1", filePattern=".*\.cpp$")
+    #pisitools.dosed("gfx/", "freetype\/(.*\.h)", r"\1", filePattern=".*\.h$")
     # Write xulrunner version correctly including the minor part
-    for f in ("xulrunner/installer/Makefile.in", ".mozconfig", "20-xulrunner.conf"):
-        pisitools.dosed(f, "PSPEC_VERSION", XulVersion)
+    #for f in ("xulrunner/installer/Makefile.in", ".mozconfig", "20-xulrunner.conf"):
+        #pisitools.dosed(f, "PSPEC_VERSION", XulVersion)
 
     # Mozilla sticks on with autoconf-213, so use autoconf-213 which we provide via a hacky patch to produce configure
     shelltools.chmod("autoconf-213/autoconf-2.13", 0755)
@@ -37,7 +37,9 @@ def setup():
     shelltools.cd("js/src")
     shelltools.system("/bin/bash ../../autoconf-213/autoconf-2.13 --macro-dir=../../autoconf-213/m4")
     shelltools.cd("../..")
-
+    # configure script misdetects the preprocessor without an optimization level
+    # https://bugs.archlinux.org/task/34644
+    shelltools.system("sed -i '/ac_cpp=/s/$CPPFLAGS/& -O2/' configure")
     shelltools.makedirs(ObjDir)
 
 def build():
@@ -57,10 +59,12 @@ def install():
     shelltools.touch("%s%s/dictionaries/tr-TR.aff" % (get.installDIR(), XulDir))
     shelltools.touch("%s%s/dictionaries/tr-TR.dic" % (get.installDIR(), XulDir))
 
+    shelltools.chmod("%s/usr/lib/xulrunner-devel-29.0/sdk/bin/xpt.py" % get.installDIR(), 0755)
+    shelltools.chmod("%s/usr/lib/xulrunner-devel-29.0/sdk/bin/xpcshell" % get.installDIR(), 0755)
     # Remove unnecessary executable bits
     for d in ("%s/usr/share" % get.installDIR(), "%s/usr/include" % get.installDIR()):
         for root, dirs, files in os.walk(d):
             for file in files:
                 shelltools.chmod(os.path.join(root, file), 0644)
 
-    pisitools.insinto("/etc/ld.so.conf.d", "20-xulrunner.conf")
+    pisitools.insinto("/etc/ld.so.conf.d", "20-xulrunner.conf", "xulrunner.conf")
