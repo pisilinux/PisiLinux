3e46ed53465e534a1ec426370578fd5d0af876f7
marcin- <marcin.bojara@gmail.com>
2014-05-05 17:46:40 +0200

    ca-certificates: fix conf file creation

--- system/base/ca-certificates/actions.py
+++ system/base/ca-certificates/actions.py
@@ -3,26 +3,26 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 
 def build():
     autotools.make("SUBDIRS=mozilla")
-    
-    shelltools.cd("mozilla")
-    shelltools.system("find . -name '*.crt' | sort | cut -b3- > ../ca-certificates.conf")
+
 
 def install():
     pisitools.dodir("usr/share/ca-certificates/mozilla")
     pisitools.dodir("usr/sbin")
-    
+
     autotools.install("SUBDIRS=mozilla DESTDIR=%s" % get.installDIR())
     pisitools.doman("sbin/update-ca-certificates.8")
-      
+
+    shelltools.cd("%s/usr/share/ca-certificates" % get.installDIR())
+    shelltools.system("find . -name '*.crt' | sort | cut -b3- > ca-certificates.conf")
     pisitools.insinto("/etc/", "ca-certificates.conf")
-    
+
     pisitools.dodir("/etc/ca-certificates/update.d")
     pisitools.dodir("etc/ssl/certs")
