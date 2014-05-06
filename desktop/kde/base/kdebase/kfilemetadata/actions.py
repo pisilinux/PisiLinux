6491b07e8af25c04887ee805bbea0e2378a361c5
aydemir <yusuf.aydemir@istanbul.com>
2014-05-07 00:35:04 +0300

    kfilemetadata

--- desktop/kde/base/kdebase/kfilemetadata/actions.py
+++ desktop/kde/base/kdebase/kfilemetadata/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde4
 
 def setup():
-    kde4.configure()
+    kde4.configure("-DCMAKE_BUILD_TYPE=Release \
+                    -Wno-dev")
 
 def build():
     kde4.make()
