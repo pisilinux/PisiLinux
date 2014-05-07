048ca8be10fd0a5e27e8b5fc9274c942e993b435
obsoleteman <tulliana@gmail.com>
2014-05-08 01:53:54 +0300

    rekonq: fix version , build without Neponuk

--- network/web/rekonq/actions.py
+++ network/web/rekonq/actions.py
@@ -5,20 +5,17 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 from pisi.actionsapi import kde4
 
-shelltools.export("HOME", get.workDIR())
-
 def setup():
-    kde4.configure()
+    kde4.configure("-DWITH_NEPOMUK=OFF")
 
 def build():
     kde4.make()
 
 def install():
     kde4.install()
+    
     pisitools.dosym("/usr/share/icons/hicolor/128x128/apps/reqonk.png", "/usr/share/pixmaps/reqonk.png")
 
     pisitools.dodoc("COPYING", "AUTHORS", "TODO", "ChangeLog")
