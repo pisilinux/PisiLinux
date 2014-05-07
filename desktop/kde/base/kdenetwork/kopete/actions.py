46fb1c39209925c4c7159bbe3a522dd4faa04eb7
aydemir <yusuf.aydemir@istanbul.com>
2014-05-08 01:21:40 +0300

    kopete

--- desktop/kde/base/kdenetwork/kopete/actions.py
+++ desktop/kde/base/kdenetwork/kopete/actions.py
@@ -4,20 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde4
-from pisi.actionsapi import get
-
-shelltools.export("HOME", get.workDIR())
-NoStrip=["/usr/share"]
 
 def setup():
     kde4.configure("-DWITH_JINGLE=TRUE -DMOZPLUGIN_INSTALL_DIR=/usr/lib/browser-plugins \
                     -DWITH_GOOGLETALK=ON \
                     -DWITH_Xmms=OFF \
                     -DWITH_LibMeanwhile=OFF \
-                    -DWITH_qq=OFF")
+                    -DWITH_qq=OFF \
+                    -DCMAKE_BUILD_TYPE=Release \
+                    -DKDE4_BUILD_TESTS=OFF \
+                    -DCMAKE_SKIP_RPATH=ON \
+                    -Wno-dev")
 
 def build():
     kde4.make()
