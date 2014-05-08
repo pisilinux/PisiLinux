6d994083f0538f93118a4577f2473f659b1a9fdb
aydemir <yusuf.aydemir@istanbul.com>
2014-05-08 11:59:41 +0300

    amarok disable soprano nepomuk

--- multimedia/sound/amarok/actions.py
+++ multimedia/sound/amarok/actions.py
@@ -12,9 +12,12 @@ from pisi.actionsapi import shelltools
 shelltools.export("XDG_DATA_DIRS", get.workDIR())
 
 def setup():
-    kde4.configure("-DWITH_IPOD=ON \
+    kde4.configure("-DWITH_NepomukCore=OFF \
+                    -DWITH_Soprano=OFF \
+                    -DWITH_IPOD=ON \
                     -DCMAKE_BUILD_TYPE=Release \
                     -DKDE4_BUILD_TESTS=OFF \
+                    -Wno-dev \
                     -DKDE_DISTRIBUTION_TEXT='PisiLinux'")
 
 def build():
