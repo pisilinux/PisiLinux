6991a528724a21d90afcec5e1d3113e37c39e12f
Aydın Demirel <aydin@demirel.web.tr>
2014-05-09 21:36:17 +0300

    libvirt-glib: new package

---
+++ hardware/virtualization/libvirt-glib/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --with-python=/usr/bin/python2.7")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "NEWS", "README", "ChangeLog", "INSTALL", "COPYING")
