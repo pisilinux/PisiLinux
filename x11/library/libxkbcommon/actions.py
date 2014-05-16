d4e8bd01aefe6dd4efc603a6f6dd3bd9f50e8e20
alihanozturk <alihan@pisilinux.org>
2014-05-16 20:24:34 +0300

    libxkbcommon:version bump

--- x11/library/libxkbcommon/actions.py
+++ x11/library/libxkbcommon/actions.py
@@ -19,4 +19,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("COPYING", "TODO", "README")
+    pisitools.dodoc("COPYING", "NEWS", "README.md")
