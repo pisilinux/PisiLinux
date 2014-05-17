4e2334b5b4be6f190c406adc45f82de3aa31c20b
obsoleteman <tulliana@gmail.com>
2014-05-17 08:30:15 +0300

    phpmyadmin: version bump

--- util/admin/phpmyadmin/actions.py
+++ util/admin/phpmyadmin/actions.py
@@ -13,4 +13,4 @@ def install():
     pisitools.insinto("/usr/share/phpmyadmin", "*")
 
     pisitools.dohtml("*")
-    pisitools.dodoc("ChangeLog","Documentation.txt","LICENSE","README")
+    pisitools.dodoc("ChangeLog","LICENSE","README")
