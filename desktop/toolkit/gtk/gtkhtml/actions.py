15aa69b63542ff27737b727ef46869b04b9bbf3e
aydemir <yusuf.aydemir@istanbul.com>
2014-05-19 00:39:03 +0300

    gtkhtml

---
+++ desktop/toolkit/gtk/gtkhtml/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING.LIB", "NEWS", "TODO")
