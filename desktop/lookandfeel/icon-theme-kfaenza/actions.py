98e93948e30d31e20a1a637f2a985ecb5a132550
aydemir <yusuf.aydemir@istanbul.com>
2014-05-19 01:38:16 +0300

    kfaenza

--- desktop/lookandfeel/icon-theme-kfaenza/actions.py
+++ desktop/lookandfeel/icon-theme-kfaenza/actions.py
@@ -13,3 +13,8 @@ def install():
     pisitools.insinto("/usr/share/icons", "kfaenza")
     shelltools.system("wget -q http://kde-look.org/CONTENT/content-files/153813-kfaenza-icon-patch-0.3.tar.gz -P /tmp")
     shelltools.system("tar -zxvf /tmp/153813-kfaenza-icon-patch-0.3.tar.gz -C %s/usr/share/icons/kfaenza" % get.installDIR())
+    
+    pisitools.dosym("/usr/share/icons/kfaenza/actions/16/application-exit.png", "/usr/share/icons/kfaenza/actions/16/cancel.png")
+    pisitools.dosym("/usr/share/icons/kfaenza/actions/22/application-exit.png", "/usr/share/icons/actions/kfaenza/22/cancel.png")
+    pisitools.dosym("/usr/share/icons/kfaenza/actions/32/application-exit.png", "/usr/share/icons/actions/kfaenza/32/cancel.png")
+    pisitools.dosym("/usr/share/icons/kfaenza/actions/48/application-exit.png", "/usr/share/icons/actions/kfaenza/48/cancel.png")
