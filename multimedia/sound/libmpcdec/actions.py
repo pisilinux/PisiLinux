df91e1af628dd57b932fc2b51174f9e85ecc60bc
obsoleteman <tulliana@gmail.com>
2014-05-21 15:02:16 +0300

    libmpcdec: rebuild, cleanup

--- multimedia/sound/libmpcdec/actions.py
+++ multimedia/sound/libmpcdec/actions.py
@@ -8,9 +8,6 @@ from pisi.actionsapi import autotools
 import os
 
 def setup():
-    # This is a very important workaround / fix
-    os.system('touch -d "15 March 1980" configure.ac')
-
     autotools.configure("--disable-static")
 
 def build():
