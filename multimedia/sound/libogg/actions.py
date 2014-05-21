3e4ac81e8ee8139787ffe89cd7a87f3cb380d649
obsoleteman <tulliana@gmail.com>
2014-05-21 12:11:55 +0300

    libogg: cleanup

--- multimedia/sound/libogg/actions.py
+++ multimedia/sound/libogg/actions.py
@@ -7,22 +7,10 @@
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
 
 
 def setup():
-    cflags = get.CFLAGS()
-    options = "--disable-static"
-    pisitools.dosed("doc/Makefile.in", "^docdir = .*$", "docdir = $(datadir)/doc/$(PACKAGE)")
-    pisitools.dosed("doc/libogg/Makefile.in", "^docdir = .*$", "docdir = $(datadir)/doc/$(PACKAGE)/ogg")
-
-    if get.buildTYPE() == "emul32":
-        cflags += " -m32"
-        options += " --libdir=/usr/lib32"
-        shelltools.export("CFLAGS", cflags)
-
-    pisitools.dosed("configure", "-O20", cflags)
-    autotools.configure(options)
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
