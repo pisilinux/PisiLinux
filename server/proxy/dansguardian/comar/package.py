6718b348ac889c3cf4111686bce369e47e8dda61
Aydın Demirel <aydin@demirel.web.tr>
2014-05-24 23:24:45 +0300

    dansguardian: fixed package.py

--- server/proxy/dansguardian/comar/package.py
+++ server/proxy/dansguardian/comar/package.py
@@ -3,5 +3,7 @@
 import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    os.system("mkdir -p /var/dansguardian")
+    os.system("/bin/chown dansguardian /var/dansguardian")
     os.system("/bin/chown dansguardian:dansguardian /var/log/dansguardian -R")
     os.system("/bin/chmod o-rwx /var/log/dansguardian")
