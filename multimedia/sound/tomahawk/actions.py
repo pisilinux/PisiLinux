d06f127030d913c5eb07cf1905c128bec5f14b23
vdemir <v.demir@mail.com>
2014-05-24 17:15:11 +0300

    tomahawk:qt-sql-sqlite dep.

--- multimedia/sound/tomahawk/actions.py
+++ multimedia/sound/tomahawk/actions.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import kde4
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde4
 
-shelltools.export("HOME", get.workDIR())
+# if pisi can't find source directory, see /var/pisi/tomahawk/work/ and:
+# WorkDir="tomahawk-"+ get.srcVERSION() +"/sub_project_dir/"
 
 def setup():
-    kde4.configure("-DBUILD_RELEASE:BOOL=ON")
-    pisitools.dosed("build/CMakeCache.txt", "CMAKE_INSTALL_LIBDIR:PATH=lib64", "CMAKE_INSTALL_LIBDIR:PATH=lib")
+    kde4.configure("-DBUILD_BUILD_RELEASE=on \
+                    -DBUILD_WITH_QT4=on \
+                    -DBUILD_WITH_KDE4=on \
+                    -DBUILD_BUILD_TESTS=off \
+                    -DCMAKE_INSTALL_PREFIX=/usr \
+                    -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+                    -DCMAKE_INSTALL_LIBEXECDIR=lib/")
+    #pisitools.dosed("build/CMakeCache.txt", "CMAKE_INSTALL_LIBDIR:PATH=lib64", "CMAKE_INSTALL_LIBDIR:PATH=lib")
 
 def build():
     kde4.make()
 
 def install():
-    kde4.install()
+    kde4.install("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "README*", "LICENSE*")
+# Take a look at the source folder for these file as documentation.
+#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
+
+# If there is no install rule for a runnable binary, you can 
+# install it to binary directory.
+#    pisitools.dobin("tomahawk")
+
+# You can use these as variables, they will replace GUI values before build.
+# Package Name : tomahawk
+# Version : 0.8.0
+# Summary : Tomahawk, the social music player app (Qt / C++)
+
+# For more information, you can look at the Actions API
+# from the Help menu and toolbar.
+
+# By PiSiDo 2.0.0
