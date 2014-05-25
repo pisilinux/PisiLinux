f653099a7028fe3d70f4efd4e243b4ecbc6e1f90
vedat <vedat@pisi_linux1.0>
2014-05-25 18:06:37 +0300

    asymptote: v. bump

--- multimedia/graphics/asymptote/actions.py
+++ multimedia/graphics/asymptote/actions.py
@@ -9,13 +9,17 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-shelltools.export("HOME", get.workDIR())
+#shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.autoreconf("-fi")
-    autotools.configure("--enable-gsl \
+    #autotools.autoreconf("-fi")
+    autotools.configure("--prefix=/usr \
+                         --includedir=/usr/include \
+                         --enable-gsl \
                          --enable-gc=system \
-                         --with-latex=/usr/share/texmf-dist/tex/latex")
+                         --with-latex=/usr/share/texmf/tex/latex \
+                         --with-context=/usr/share/texmf/tex/context \
+                         --enable-texlive-build")
 
 def build():
     autotools.make()
@@ -27,5 +31,5 @@ def install():
     pisitools.domove("/usr/share/asymptote/*.el", "/usr/share/emacs/site-lisp/")
 
     # move file to correct path.
-    pisitools.domove("/usr/local/share/texmf/tex/context/third/asymptote/colo-asy.tex", "/usr/share/texmf/tex/context/third/asymptote/")
-    pisitools.removeDir("/usr/local")
+    #pisitools.domove("/usr/local/share/texmf/tex/context/third/asymptote/colo-asy.tex", "/usr/share/texmf/tex/context/third/asymptote/")
+    #pisitools.removeDir("/usr/local")
