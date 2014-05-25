191e98a99dca92ba6fa80964d51be9b5dfcd90d8
marcin- <marcin.bojara@gmail.com>
2014-05-25 21:04:16 +0200

    media-player-info: fix build

--- hardware/info/media-player-info/actions.py
+++ hardware/info/media-player-info/actions.py
@@ -4,18 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("PYTHON","python2.7")
+    # force to use python2.x
+    pisitools.dosed("configure.ac", "(python)3", "\\1")
+
+    autotools.autoreconf("-fi")
     autotools.configure("--prefix=/usr \
                          --with-udevdir=/lib/udev")
 
 def build():
-    shelltools.export("LANG","en_US.UTF-8")
     autotools.make()
 
 def install():
