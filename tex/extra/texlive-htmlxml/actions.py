9abc1996634d8477128c66ffab22360bde05c66f
vedat <vedat@pisi_linux1.0>
2014-05-29 03:34:58 +0300

    texlive-htmlxml:version bump.

--- tex/extra/texlive-htmlxml/actions.py
+++ tex/extra/texlive-htmlxml/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import get
 import os
 from distutils.dir_util import copy_tree
 
-WorkDir = "%s" % get.srcNAME()
+WorkDir = "."
 
 def setup():
     # Unpack and prepare files
