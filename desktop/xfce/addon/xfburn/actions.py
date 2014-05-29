ffc10deab7a0ba1b8cc8198914487412d5c90512
obsoleteman <tulliana@gmail.com>
2014-05-29 10:17:01 +0300

    xfburn: fix actions.py

--- desktop/xfce/addon/xfburn/actions.py
+++ desktop/xfce/addon/xfburn/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--prefix=/usr 
+    autotools.configure("--prefix=/usr \
                          --enable-gstreamer")
 
 def build():
