c2e056cbd6347fbe4c507339bb28998d22565f05
vedat <vedat@pisi_linux1.0>
2014-05-30 20:35:10 +0300

    btrfs-progs: version bump.

--- hardware/disk/btrfs-progs/actions.py
+++ hardware/disk/btrfs-progs/actions.py
@@ -7,24 +7,22 @@
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def build():
-    # Fix install prefix
-    pisitools.dosed("Makefile", "^(prefix \?= \/usr)\/local", r"\1")
-
-    autotools.make("V=1 CC=%s CFLAGS=\"%s\"" % (get.CC(), get.CFLAGS()))
-    autotools.make("V=1 CC=%s CFLAGS=\"%s\" btrfs-select-super" % (get.CC(), get.CFLAGS()))
+  
+    shelltools.cd("%s/" % get.workDIR())
+    pisitools.dosym("./Makefile", "Makefile")
+    autotools.make("CC=%s CFLAGS=\"%s\"" % (get.CC(), get.CFLAGS()))
 
 def install():
-    pisitools.dosed("man/Makefile", "^prefix \?= .*$", "prefix = /usr/share")
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+  
+    shelltools.cd("%s/" % get.workDIR())
+    autotools.rawInstall("prefix=/usr DESTDIR=%s" % get.installDIR())
     pisitools.remove("/usr/lib/*.a")
 
     pisitools.insinto("/usr/bin", "bcp", "btrfs-bcp")
     pisitools.insinto("/usr/bin", "show-blocks", "btrfs-show-blocks")
-    pisitools.insinto("/usr/bin", "btrfs-select-super")
-
-    pisitools.dosym("btrfsck", "/usr/bin/fsck.btrfs")
-
+    
     pisitools.dodoc("COPYING")
+   
