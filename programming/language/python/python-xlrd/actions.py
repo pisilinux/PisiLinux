4eb13f49dd92a721598aad7e93aedd0ef588e969
obsoleteman <tulliana@gmail.com>
2014-05-31 13:01:03 +0300

    python-xlrd: fix

--- programming/language/python/python-xlrd/actions.py
+++ programming/language/python/python-xlrd/actions.py
@@ -5,11 +5,8 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-
-WorkDir = "xlrd-%s" % get.srcVERSION()
 
 def setup():
     pythonmodules.compile()
@@ -18,6 +15,3 @@ def install():
     pythonmodules.install()
     shelltools.chmod("%s/usr/bin/runxlrd.py" % get.installDIR(), 0755)
 
-    pisitools.domove("/usr/lib/%s/site-packages/xlrd/doc/*.html" % get.curPYTHON(), "%s/python-xlrd" % get.docDIR())
-    pisitools.removeDir("/usr/lib/%s/site-packages/xlrd/doc/" % get.curPYTHON())
-    pisitools.domove("/usr/lib/%s/site-packages/xlrd/examples" % get.curPYTHON(), "%s/python-xlrd" % get.docDIR())
