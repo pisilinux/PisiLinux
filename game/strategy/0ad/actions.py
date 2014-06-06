96f135dad548eb86a23ebe8acaf932ff88a4fa5f
alihanozturk <alihan@pisilinux.org>
2014-06-06 23:07:23 +0300

    0ad:version bump

--- game/strategy/0ad/actions.py
+++ game/strategy/0ad/actions.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-def setup():
-    #thanks to s.dalgic
-    shelltools.export("pardusCC", get.CC())
-    shelltools.export("pardusCXX", get.CXX())
-    shelltools.export("pardusCFLAGS", get.CXX())
-    shelltools.export("pardusCPPFLAGS", get.CXXFLAGS())
-    
+def setup():    
     shelltools.cd("build/workspaces/")
     shelltools.system("./update-workspaces.sh \
-                        --enable-atlas \
-                        --with-system-enet \
-                        JOBS=%s" % get.makeJOBS())
+                       --enable-atlas \
+                       --with-system-enet \
+                       --with-system-miniupnpc \
+                       --bindir=/usr/bin \
+                       --libdir=/usr/lib/0ad \
+                       --datadir=/usr/share/0ad/data \
+                       JOBS=%s" % get.makeJOBS())
 
 def build():
     shelltools.cd("build/workspaces/gcc")
-    autotools.make("V=1 CONFIG=Release")
+    autotools.make("CONFIG=Release")
 
 def install():
     pisitools.dodoc("LICENSE.txt","license_dbghelp.txt","license_gpl-2.0.txt","license_lgpl-2.1.txt","README.txt",)
-    pisitools.insinto("/opt/0ad","binaries/*")
+    pisitools.insinto("/usr/share/0ad", "binaries/*")
+    pisitools.insinto("/usr/bin/", "binaries/system/pyrogenesis")
+    pisitools.insinto("/usr/lib/0ad/", "binaries/system/*.so*")
     pisitools.insinto("/usr/share/pixmaps/", "build/resources/0ad.png", "0ad.png")
     pisitools.insinto("/usr/share/applications/", "build/resources/0ad.desktop", "0ad.desktop")
-    pisitools.insinto("/usr/share/applications/", "build/resources/0ad-editor.desktop", "0ad-editor.desktop")
-
