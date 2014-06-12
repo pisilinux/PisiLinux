87e100c3da607ef0afcbf25a31d4a3e50e30ce7c
obsoleteman <tulliana@gmail.com>
2014-06-12 15:36:52 +0300

    cegui: version bump

--- game/misc/cegui/actions.py
+++ game/misc/cegui/actions.py
@@ -4,41 +4,21 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "CEGUI-%s" % get.srcVERSION()
-
 def setup():
-    pisitools.touch("NEWS")
-    pisitools.touch("README")
-    pisitools.touch("AUTHORS")
-    pisitools.touch("ChangeLog")
-    autotools.autoreconf("-vfi")
-    autotools.configure("--enable-xerces-c \
-                         --enable-libxml \
-                         --enable-expat \
-                         --enable-tinyxml \
-                         --enable-opengl-renderer \
-                         --enable-toluacegui \
-                         --enable-freeimage \
-                         --enable-silly \
-                         --enable-lua-module \
-                         --with-x \
-                         --with-gtk2 \
-                         --with-default-xml-parser=TinyXMLParser \
-                         --with-default-image-codec=STBImageCodec \
-                         --disable-devil \
-                         --disable-samples")
-    
-    # for fix unused dependency
-    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+    pisitools.dosed("CMakeLists.txt", "lib64", "lib")
+
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCEGUI_LIB_INSTALL_DIR=lib \
+                          -DCEGUI_BUILD_PYTHON_MODULES=OFF")
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("doc/COPYING", "doc/GLEW-LICENSE", "doc/PCRE-LICENSE", "doc/TinyXML-License", "doc/stringencoders-license", "doc/README")
+    pisitools.dodoc("doc/COPYING", "doc/PCRE-LICENSE", "doc/README", "doc/stringencoders-license")
