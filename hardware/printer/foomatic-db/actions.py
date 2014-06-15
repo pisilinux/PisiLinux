f1c8660a1db42c35b75c4bd6e9a747430b379245
marcin- <marcin.bojara@gmail.com>
2014-06-15 10:17:40 +0200

    foomatic-db: version bump

--- hardware/printer/foomatic-db/actions.py
+++ hardware/printer/foomatic-db/actions.py
@@ -4,32 +4,27 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 
-import os
-
-WorkDir = "foomatic-db-%s" % get.srcVERSION().split("_", 1)[1]
 NoStrip = "/"
 
 def setup():
     # For gutenprint printers, use gutenprint-ijs-simplified.5.2
-    pisitools.dosed("db/source/printer/*.xml", ">gutenprint<", ">gutenprint-ijs-simplified.5.2<")
+    #pisitools.dosed("db/source/printer/*.xml", ">gutenprint<", ">gutenprint-ijs-simplified.5.2<")
 
     autotools.configure()
 
     # Cleanup conflicts
-    shelltools.cd("db/source")
-    shelltools.system("../../cleanup-conflicts")
+    #shelltools.cd("db/source")
+    #shelltools.system("../../cleanup-conflicts")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Fix permissions
-    for root, dirs, files in os.walk("%s/usr/share/foomatic/db" % get.installDIR()):
-        for name in dirs:
-            shelltools.chmod(os.path.join(root, name), 0755)
-        for name in files:
-            shelltools.chmod(os.path.join(root, name), 0644)
+    #for root, dirs, files in os.walk("%s/usr/share/foomatic/db" % get.installDIR()):
+    #    for name in dirs:
+    #        shelltools.chmod(os.path.join(root, name), 0755)
+    #    for name in files:
+    #        shelltools.chmod(os.path.join(root, name), 0644)
