c09b36c3a590dff77865ea7210871d7adb2ff2ab
marcin- <marcin.bojara@gmail.com>
2014-06-15 10:30:39 +0200

    foomatic-db-engine: version bump

--- hardware/printer/foomatic-db-engine/actions.py
+++ hardware/printer/foomatic-db-engine/actions.py
@@ -4,11 +4,11 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import perlmodules
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import perlmodules
 
 def setup():
     # The LANG vars aren't reset early enough so when sed tries to use [a-zA-Z], it borks
