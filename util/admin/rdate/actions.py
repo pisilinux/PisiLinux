0eda8f0a46c2454afb6fca6efc24f1368366a8b7
alihanozturk <alihan@pisilinux.org>
2014-06-18 23:32:12 +0300

    rdate:version bump

--- util/admin/rdate/actions.py
+++ util/admin/rdate/actions.py
@@ -7,8 +7,12 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --mandir=/usr/share/man")
+
 def build():
     autotools.make("RCFLAGS=\"%s %s -DINET6\"" % (get.CFLAGS(),get.LDFLAGS()))
 
 def install():
-    autotools.rawInstall("prefix=%s/usr" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
