cf24d83f2cefe07bd947300c95a6f49f1eb00961
alihanozturk <alihan@pisilinux.org>
2014-06-18 22:40:36 +0300

    pgadmin3:version bump

--- util/admin/pgadmin3/actions.py
+++ util/admin/pgadmin3/actions.py
@@ -5,13 +5,17 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sed -i -e 's/wx-config/wx-config-2.8/' configure")
+    shelltools.export("WX_CONFIG", "/usr/bin/wx-config-2.8 ")
     autotools.configure("--disable-static \
                          --disable-debug \
                          --disable-dependency-tracking \
+                          --with-wx-config=/usr/bin/wx-config-2.8 \
                          --with-wx-version=2.8")
 
 def build():
