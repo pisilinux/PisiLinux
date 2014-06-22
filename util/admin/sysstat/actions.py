6837ba1bc7f3d82825356298da4d519c61060799
BlackSEA- <blacksea8393@gmail.com>
2014-06-22 12:51:37 +0300

    sysstat:fix build

--- util/admin/sysstat/actions.py
+++ util/admin/sysstat/actions.py
@@ -5,22 +5,14 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.makedirs("%s/crontabs" % get.installDIR())
-    libtools.libtoolize("--force --install")
     autotools.autoreconf("-vfi")
-    autotools.configure("--enable-yesterday \
-                    	--enable-install-isag \
-                    	--enable-install-cron \
-                    	--enable-copy-only \
-                    	--disable-man-group")
+    autotools.configure()
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
