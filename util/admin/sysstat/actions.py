ee1c94bffb41913acd638b4e3aa718385ea7d329
namso-01 <namso-01@hotmail.it>
2014-07-05 18:17:19 +0300

    sysstat: fixed

--- util/admin/sysstat/actions.py
+++ util/admin/sysstat/actions.py
@@ -5,16 +5,33 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
     autotools.autoreconf("-vfi")
-    autotools.configure()
+    autotools.configure("conf_dir=/etc/conf.d --enable-yesterday \
+                        --mandir=/usr/share/man \
+                        --enable-install-isag \
+                        --enable-install-cron \
+                        --enable-copy-only \
+                        --disable-man-group")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
-def install():
+def install():    
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    shelltools.makedirs("/usr/lib/systemd/system/")
+    shelltools.makedirs("%s/etc/cron.daily/" % get.installDIR())
+    shelltools.makedirs("%s/etc/cron.hourly/" % get.installDIR())
+    
+    pisitools.insinto("/usr/lib/systemd/system/", "sysstat.service")
+    pisitools.insinto("/usr/lib/systemd/system/", "cron/sysstat-summary.timer")
+    pisitools.insinto("/usr/lib/systemd/system/", "cron/sysstat-collect.service")
+    pisitools.insinto("/usr/lib/systemd/system/", "cron/sysstat-collect.timer")
+    pisitools.insinto("/usr/lib/systemd/system/", "cron/sysstat-summary.service")
+    
     pisitools.insinto("/etc/sysstat", "cron/sysstat.crond")
+    
+    pisitools.remove("sysstat*")
