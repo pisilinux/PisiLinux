307f8c91f48141c6d5ae39feae1cbcbe63e1b2cc
vedat <vedat@pisi_linux1.0>
2014-07-05 07:26:08 +0300

    Revert "alsa-utils: some fixes"
    This reverts commit edff412faed6af1c634a1faccd579894bf0ccf5f.

--- hardware/sound/alsa-utils/actions.py
+++ hardware/sound/alsa-utils/actions.py
@@ -4,26 +4,24 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("\
+    autotools.configure("--prefix=/usr \
                          --sbindir=/sbin \
-                         --with-udev-rules-dir=/lib/udev/rules.d \
-                         --disable-alsaconf \
-                         --disable-maintainer-mode \
-                         --disable-rpath \
-                        ")
+                         --disable-xmlto \
+                         --with-udev-rules-dir=/usr/lib/udev/rules.d \
+                         --disable-alsaconf")
 
 def build():
     autotools.make()
     shelltools.system("cd alsactl")
     autotools.make()
-
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    
+    
     pisitools.dodoc("ChangeLog", "README", "TODO", "seq/aconnect/README.aconnect", "seq/aseqnet/README.aseqnet")
