5f88ae169270f4f2827823f2c9e46712b863a5a4
vedat <vedat@pisi_linux1.0>
2014-07-10 23:25:31 +0300

    marble: delete desktop files

--- desktop/kde/base/kdeedu/marble/actions.py
+++ desktop/kde/base/kdeedu/marble/actions.py
@@ -14,12 +14,18 @@ NoStrip=["/usr/share/icons"]
 # TODO: missing deps? liblocation, QtLocation, QtMobility
 
 def setup():
-    kde4.configure()
+    kde4.configure("-DQTONLY=ON \
+                    -DCMAKE_INSTALL_PREFIX=/usr \
+                    -DCMAKE_BUILD_TYPE=release")
 
 def build():
     kde4.make()
 
 def install():
     kde4.install()
-
+    pisitools.remove("/usr/bin/marble-mobile")
+    pisitools.remove("/usr/bin/marble-touch")
+    pisitools.remove("/usr/share/applications/marble-mobile.desktop")
+    pisitools.remove("/usr/share/applications/marble-touch.desktop")
     pisitools.dodoc("BUGS", "ChangeLog", "CODING", "COPYING*", "CREDITS", "LICENSE*", "MANIFESTO.txt", "TODO", "USECASES")
+  
