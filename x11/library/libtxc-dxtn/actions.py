c5309df3a397a0c86ee0219fe6e53f76419eb5cc
aydemir <yusuf.aydemir@istanbul.com>
2014-07-12 01:49:06 +0300

    libtxc-dxtn

---
+++ x11/library/libtxc-dxtn/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.system("sed -n '5,22{s|^ \* \?||;p}' txc_dxtn.h > LICENSE")
+    pisitools.dodoc("LICENSE")
