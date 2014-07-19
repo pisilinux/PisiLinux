2638e2c805290845d1fd28a6362c529bdb9abdc4
suvari <suvarice@gmail.com>
2014-07-19 04:25:44 +0300

    taglib-sharp:fix build

--- programming/language/dotnet/taglib-sharp/actions.py
+++ programming/language/dotnet/taglib-sharp/actions.py
@@ -4,13 +4,14 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+
 
 def setup():
-    autotools.configure("--disable-docs")
+    autotools.configure("--prefix=/usr \
+                         --disable-docs")
 
 def build():
     autotools.make()
