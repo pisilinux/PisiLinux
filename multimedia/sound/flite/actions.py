902e3e751582df6c4ac5332d3f3120603d0017b9
vedat <vedat@pisi_linux1.0>
2014-07-23 00:46:32 +0300

    flite: version bump.

--- multimedia/sound/flite/actions.py
+++ multimedia/sound/flite/actions.py
@@ -6,13 +6,15 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "flite-%s-release" % get.srcVERSION()
-
-def setup():
-    autotools.configure("--enable-shared \
-                         --with-audio=alsa")
+WorkDir = "flite-%s-current" % get.srcVERSION()
+   
+def setup(): 
+    autotools.configure("--prefix=/usr \
+                         --with-audio=alsa \
+	                 --with-vox=cmu_us_kal16")
 
 def build():
     autotools.make("-j1")
