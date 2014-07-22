9b8699931e2c3813157195c9bdcac59c847ba2c1
obsoleteman <tulliana@gmail.com>
2014-07-22 12:48:40 +0300

    Revert, old qscintilla2 "pisido: fix compiling error"
    This reverts commit d85271174aaf5ce38f48a651f9fb7d741e330d6f.

--- util/admin/pisido/actions.py
+++ util/admin/pisido/actions.py
@@ -9,14 +9,12 @@ from pisi.actionsapi import qt4
 def setup():
     pisitools.dosed("h/pchs.h", "qtermwidget/qtermwidget.h", "qtermwidget4/qtermwidget.h")
     pisitools.dosed("cpp/mainwindow.cpp", "qtermwidget/qtermwidget.h", "qtermwidget4/qtermwidget.h")
-
     qt4.configure()
 
 def build():
-    pisitools.dosed("Makefile", "-lqtermwidget", "-lqtermwidget4")
     qt4.make()
 
 def install():
     qt4.install()
-    
+
     pisitools.dodoc("LICENSE", "LISANS", "OKUBUNU", "README")
