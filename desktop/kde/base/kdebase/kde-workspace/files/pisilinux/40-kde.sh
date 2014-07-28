762191144d563ae0f6cba0acd2ac5d22d785a6bd
aydemir <yusuf.aydemir@istanbul.com>
2014-07-28 10:21:44 +0300

    kde-workspace

--- desktop/kde/base/kdebase/kde-workspace/files/pisilinux/40-kde.sh
+++ desktop/kde/base/kdebase/kde-workspace/files/pisilinux/40-kde.sh
@@ -57,10 +57,3 @@ EOF
         export GTK_RC_FILES
     fi
 fi
-if test -z "${XDG_RUNTIME_DIR}"; then
-    export XDG_RUNTIME_DIR=/tmp/${UID}-runtime-dir
-    if ! test -d "${XDG_RUNTIME_DIR}"; then
-        mkdir "${XDG_RUNTIME_DIR}"
-        chmod 0700 "${XDG_RUNTIME_DIR}"
-    fi
-fi
