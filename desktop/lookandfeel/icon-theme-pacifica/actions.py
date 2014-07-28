6549b458307bf34789e6dd5b51095cb6cab18894
vedat <vedat@pisi_linux1.0>
2014-07-28 19:37:43 +0300

    icon-theme-pacifica: fix permissions

--- desktop/lookandfeel/icon-theme-pacifica/actions.py
+++ desktop/lookandfeel/icon-theme-pacifica/actions.py
@@ -5,10 +5,14 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 WorkDir = "./"
 
 def install():
+  
+    shelltools.chmod("%s/pacifica-icon-theme-master/Pacifica/*" % get.workDIR())
     pisitools.insinto("/usr/share/icons", "pacifica-icon-theme-master/Pacifica")
     
 #    pisitools.dodoc("AUTHORS", "LICENSE")
