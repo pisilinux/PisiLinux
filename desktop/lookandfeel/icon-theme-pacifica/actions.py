fd2e2272ec21058fa9e38f0d7c97570b97cba3c0
marcin- <marcin.bojara@gmail.com>
2014-07-29 18:49:14 +0200

    icon-theme-pacifica: fix and clean

--- desktop/lookandfeel/icon-theme-pacifica/actions.py
+++ desktop/lookandfeel/icon-theme-pacifica/actions.py
@@ -5,13 +5,8 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-
-WorkDir = "./"
 
 def install():
-    
-    pisitools.insinto("/usr/share/icons", "pacifica-icon-theme-master/Pacifica")
-    
-    pisitools.dodoc("pacifica-icon-theme-master/CREDITS", "pacifica-icon-theme-master/README.md")
+    pisitools.insinto("/usr/share/icons", "Pacifica")
+
+    pisitools.dodoc("CREDITS", "README.md")
