ff47bd107df6fe4680e91aa29e26ef38480fd66b
aydemir <yusuf.aydemir@istanbul.com>
2014-07-30 20:56:22 +0300

    faenza icon

--- desktop/lookandfeel/icon-theme-faenza/actions.py
+++ desktop/lookandfeel/icon-theme-faenza/actions.py
@@ -8,9 +8,16 @@ from pisi.actionsapi import pisitools
 
 
 def install():
-    pisitools.insinto("/usr/share/icons", "Faenza")
-    pisitools.insinto("/usr/share/icons", "Faenza-Ambiance")
-    pisitools.insinto("/usr/share/icons", "Faenza-Dark")
-    pisitools.insinto("/usr/share/icons", "Faenza-Darker")
-    pisitools.insinto("/usr/share/icons", "Faenza-Darkest")
-    pisitools.insinto("/usr/share/icons", "Faenza-Radiance")
+    prefix = "/usr/share/icons/"
+    klasor = ("Faenza", "Faenza-Ambiance", "Faenza-Dark", "Faenza-Darker", "Faenza-Darkest", "Faenza-Radiance")
+    boyut = ("16", "22", "24", "32", "48", "64", "96", "128", "256")
+    isim = ("distributor-logo-pisilinux.png", "pisilinux-logo.png", "start-here-kde.png", "start-here-pisilinux.png")
+
+    for dosya in klasor:
+        pisitools.insinto("/usr/share/icons", "%s" % dosya)
+
+    for size in boyut:
+        pisitools.dosym("%s%s/actions/%s/application-exit.png", "%s%s/actions/%s/cancel.png"% (prefix, dosya, size))
+
+    for ad in isim:
+        pisitools.dosym("/usr/share/icons/Faenza/places/% boyut/start-here.png", "%s%s/places/%s/%s"% (prefix, dosya, size, ad))
