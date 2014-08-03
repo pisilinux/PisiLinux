d2c81df8fe7f0e2647e6a2bbe6e9792b5292c5ab
marcin- <marcin.bojara@gmail.com>
2014-08-03 18:16:08 +0200

    pisi: pisi search-file works without sudo now

--- system/base/pisi/actions.py
+++ system/base/pisi/actions.py
@@ -3,9 +3,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
 
 def build():
     pisitools.dosed("pisi/__init__.py", "2.4", "2.6")
@@ -16,6 +17,14 @@ def install():
     pythonmodules.install("--install-lib=/usr/lib/pisilinux")
 
     pisitools.dosym("pisi-cli", "/usr/bin/pisi")
+
+    shelltools.touch("LOCK")
+    shelltools.chmod("LOCK", 0666)
+    pisitools.dodir("/run/lock/files.ldb")
+    pisitools.insinto("/run/lock/files.ldb", "LOCK")
+    pisitools.dodir("/var/lib/pisi/info/files.ldb")
+    pisitools.dosym("/run/lock/files.ldb/LOCK", "/var/lib/pisi/info/files.ldb/LOCK")
+
 
     pisitools.insinto("/etc/pisi", "pisi.conf-%s" % get.ARCH(), "pisi.conf")
 
