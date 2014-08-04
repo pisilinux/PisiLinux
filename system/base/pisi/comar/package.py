c25452055b2c9f0894155ac2181d7342eba04779
marcin- <marcin.bojara@gmail.com>
2014-08-04 19:57:51 +0200

    pisi: fix permissions

--- system/base/pisi/comar/package.py
+++ system/base/pisi/comar/package.py
@@ -3,6 +3,10 @@
 import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    if not os.path.exists("/var/lib/pisi/info/files.ldb"):
+        os.mkdir("/var/lib/pisi/info/files.ldb")
+    os.chmod("/var/lib/pisi/info/files.ldb", 509)
+    os.chown("/var/lib/pisi/info/files.ldb", 0, 10)
     if not os.path.exists("/var/lib/pisi/package"):
         os.system("mkdir /var/lib/pisi/package")
         os.system("mv /var/lib/pisi/* /var/lib/pisi/package/")
