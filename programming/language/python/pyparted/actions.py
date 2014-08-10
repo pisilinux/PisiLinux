c4006113906375f70877c1ac31acd8018e7d8c51
obsoleteman <tulliana@gmail.com>
2014-08-10 03:10:15 +0300

    pyparted: use fedora way

--- programming/language/python/pyparted/actions.py
+++ programming/language/python/pyparted/actions.py
@@ -5,7 +5,11 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+
+def build():
+    autotools.make()
 
 def install():
-    pythonmodules.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
