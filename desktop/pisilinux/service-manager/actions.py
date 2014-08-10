b73bc1afaecdae14bd6b03c780d71a7394ef41d0
obsoleteman <tulliana@gmail.com>
2014-08-10 17:55:30 +0300

    service-manager: use dosed for desktop file

--- desktop/pisilinux/service-manager/actions.py
+++ desktop/pisilinux/service-manager/actions.py
@@ -5,7 +5,12 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+
+def build():
+    pisitools.dosed("data/service-manager.desktop", "NotShowIn=KDE;", "")
+
 
 def install():
     pythonmodules.install()
-    
+
