70f115534cec34f7c52a574bdeee2f86d2a7ff09
Aydın Demirel <aydin@demirel.web.tr>
2014-08-16 12:24:54 +0300

    log4j: removed shelltools lines

--- programming/language/java/log4j/actions.py
+++ programming/language/java/log4j/actions.py
@@ -5,9 +5,11 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def install():
     shelltools.makedirs("%s/usr/share/java" % get.installDIR())
-    shelltools.move("log4j-1.2.17.jar","%s/usr/share/java" % get.installDIR())
-    shelltools.move("log4j-1.2.17-javadoc.jar","%s/usr/share/java" % get.installDIR())
+    pisitools.insinto("/usr/share/java", "log4j-1.2.17.jar")
+    pisitools.insinto("/usr/share/java", "log4j-1.2.17-javadoc.jar")
+
