f4b175fabd18657c271a1e8672578d830e4a5cc6
BlackSEA- <blacksea8393@gmail.com>
2014-08-18 18:59:44 +0300

    FFMultiConverter:fix runtime error,update source

--- multimedia/converter/FFMultiConverter/actions.py
+++ multimedia/converter/FFMultiConverter/actions.py
@@ -8,9 +8,9 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
 def build():
-    pythonmodules.compile()
+    pythonmodules.compile(pyVer = "3")
 
 def install():
-    pythonmodules.install()
+    pythonmodules.install(pyVer = "3")
 
     pisitools.dodoc("ChangeLog", "COPYING", "README*")
