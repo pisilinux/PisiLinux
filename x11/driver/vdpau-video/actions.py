77ae4e59ed16ac6103055f5576e266f45e34d92e
marcin- <marcin.bojara@gmail.com>
2014-09-02 22:16:58 +0200

    x11.driver: rebuild for xorg-server-1.16.0

--- x11/driver/vdpau-video/actions.py
+++ x11/driver/vdpau-video/actions.py
@@ -3,14 +3,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed("configure.ac", "AM_CONFIG_HEADER", "AC_CONFIG_HEADERS")
     autotools.autoreconf("-fiv")
+
     autotools.configure()
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
