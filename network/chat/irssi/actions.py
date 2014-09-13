5f2d9eed72ca0299d3f458c756c1b49f5cf22aa8
obsoleteman <tulliana@gmail.com>
2014-09-13 16:28:30 +0300

    irssi: remove perl pod file

--- network/chat/irssi/actions.py
+++ network/chat/irssi/actions.py
@@ -6,6 +6,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import perlmodules
 from pisi.actionsapi import get
 
 def setup():
@@ -23,4 +24,4 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.remove("/usr/lib/irssi/modules/libirc_proxy.a")
-    #pisitools.remove("/usr/lib/perl5/5.18.1/x86_64-linux-thread-multi/perllocal.pod")
+    perlmodules.removePodfiles()
