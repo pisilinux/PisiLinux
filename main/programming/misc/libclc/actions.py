a47e2594eeb5c33a817d2866cf6ffa5fc4e19822
obsoleteman <tulliana@gmail.com>
2014-09-24 20:33:40 +0300

    libclc: new mesa dependency

---
+++ main/programming/misc/libclc/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("svn checkout http://llvm.org/svn/llvm-project/libclc/trunk libclc")
+
+    shelltools.cd("libclc")
+
+    shelltools.system("./configure.py --prefix=/usr --pkgconfigdir=/usr/lib/pkgconfig")
+
+def build():
+    shelltools.cd("libclc")
+
+    autotools.make()
+
+def install():
+    shelltools.cd("libclc")
+
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE.TXT","CREDITS.TXT", "README.TXT")
