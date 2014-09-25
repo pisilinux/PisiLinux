ebc2450977821c4dfcdcee91037f95c2066102aa
obsoleteman <tulliana@gmail.com>
2014-09-25 15:43:45 +0300

    wayland: version bump

--- main/x11/library/wayland/actions.py
+++ main/x11/library/wayland/actions.py
@@ -5,7 +5,6 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -15,7 +14,7 @@ Libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib"
 def setup():
     autotools.autoreconf("-vif")
     autotools.configure("--disable-documentation --disable-static")
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
@@ -23,8 +22,8 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
+
     if get.buildTYPE() == "emul32":
         return
-    
+
     pisitools.dodoc("COPYING", "TODO", "README")
