05152ad2b6fbc532a0eb7c2241eb04ce79be1a10
namso-01 <namso-01@hotmail.it>
2014-09-29 13:37:30 +0300

    libreoffice: version bump, deps version bump and new packages

---
+++ main/office/misc/libgltf/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--docdir=/usr/share/doc \
+                         --disable-werror")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
