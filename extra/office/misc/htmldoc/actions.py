7e0051b20cd16d098c34eb6b463c40f6d088d7c3
namso-01 <namso-01@hotmail.it>
2014-10-16 20:53:45 +0300

    htmldoc: fixed thansk onlined #118 close

--- extra/office/misc/htmldoc/actions.py
+++ extra/office/misc/htmldoc/actions.py
@@ -9,21 +9,16 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-rpath \
-                         --disable-localpng \
-                         --disable-localjpeg \
-                         --disable-localzlib \
-                         --with-openssl-libs \
-                         --with-openssl-includes")
-
+    pisitools.dosed("htmldoc/http-addrlist.c","RESOLV_H \*/","RESOLV_H \*/\n#include <errno.h>")
+    autotools.rawConfigure("--prefix={0}/usr".format(get.installDIR()))
+    
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dohtml("doc/*.html")
-    pisitools.dohtml("doc/*.png")
-
-    pisitools.insinto("/usr/share/applications", "desktop/htmldoc.desktop")
-    pisitools.insinto("/usr/share/icons/", "desktop/htmldoc-48.png")
+    autotools.install()
+    pisitools.dodoc("README.txt","CHANGES.txt")
+    pisitools.insinto("/usr/share/applications/","desktop/htmldoc.desktop")
+    for i in [16,24,32,48,64,96,128]:
+		pisitools.insinto("/usr/share/icons/hicolor/{0}x{0}/apps/htmldoc.png".format(i),"desktop/htmldoc-{0}.png".format(i))
+    pisitools.insinto("/usr/share/mime/packages/","desktop/htmldoc.xml")
