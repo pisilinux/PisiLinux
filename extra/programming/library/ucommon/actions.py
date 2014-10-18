fe71119c49181f071ad7fcf09455b4c476f6d8a3
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2014-10-18 15:43:33 +0300

    ucommon:version bump. and removed unneeded lines

--- extra/programming/library/ucommon/actions.py
+++ extra/programming/library/ucommon/actions.py
@@ -18,5 +18,6 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.remove("/usr/lib/*.a")
 
     pisitools.dodoc("COPYING", "AUTHORS", "README")
