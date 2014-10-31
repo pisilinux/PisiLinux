06f7b5975881d197d0a89f393913f4723abf2eaa
Ertuğrul Erata <ertugrulerata@gmail.com>
2014-10-31 23:17:14 +0200

    nodejs in repo...

---
+++ extra/network/library/nodejs/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+# WorkDir = ""
+# NoStrip = "/"
+
+def setup():
+    autotools.rawConfigure("--prefix=/usr \
+                            --shared-openssl")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README*")
