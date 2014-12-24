86777701736ca9f212033efd41d0af9faf145c74
Ertuğrul Erata <ertugrulerata@gmail.com>
2014-12-24 21:30:20 +0200

    bridge-utils fix

--- extra/network/connection/bridge-utils/actions.py
+++ extra/network/connection/bridge-utils/actions.py
@@ -6,15 +6,22 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
 
 def setup():
     autotools.autoconf()
-    autotools.configure()
+    autotools.configure("--prefix=/usr \
+                         --datarootdir=/usr/share \
+                         --with-linux-headers=/usr/include \
+                         --sbindir=/usr/bin \
+                         --sysconfdir=/etc")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install('SUBDIRS="brctl doc"')
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    # autotools.install('SUBDIRS="brctl doc"')
 
     pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "doc/FAQ", "doc/HOWTO")
