9fbd34d0dcae316e68cd683efcd41a6565cced40
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-01-11 14:16:16 +0200

    libndp:new package for NetworkManager

---
+++ main/network/misc/libndp/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --localstatedir=/var \
+                         --disable-static \
+                         --libexecdir=/usr/lib")
+
+def build():
+    autotools.make()
+
+#def check():
+#    autotools.make("check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("COPYING", "README")
