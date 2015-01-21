a1d1555a25d8fc07e356e3420848f7d47e8d3969
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-01-21 12:26:55 +0200

    python-pyside:removed python3 lines

--- extra/programming/language/python/python-pyside/actions.py
+++ extra/programming/language/python/python-pyside/actions.py
@@ -14,34 +14,32 @@ WorkDir = "."
 def setup():
     shelltools.cd("./pyside-qt4.8+1.2.2")
     shelltools.makedirs("build2")
-    shelltools.makedirs("build3")
+    #shelltools.makedirs("build3")
     shelltools.cd("build2")
     cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
                           -DCMAKE_BUILD_TYPE=Release \
                           -DBUILD_TESTS=OFF \
                           -DPYTHON_SUFFIX=-python2.7", sourceDir="..")
-
-                          #-DQT_PHONON_INCLUDE_DIR=/usr/include/qt4/phonon \
     #python3
-    shelltools.cd("../build3")
-    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
-                          -DCMAKE_BUILD_TYPE=Release \
-                          -DBUILD_TESTS=OFF", sourceDir="..")
+    #shelltools.cd("../build3")
+    #cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          #-DCMAKE_BUILD_TYPE=Release \
+                          #-DBUILD_TESTS=OFF", sourceDir="..")
 def build():
     #python2
     shelltools.cd("pyside-qt4.8+1.2.2/build2")
     autotools.make()
     #python3
-    shelltools.cd("../build3")
-    autotools.make()
+    #shelltools.cd("../build3")
+    #autotools.make()
 
 def install():
     #Python2
     shelltools.cd("pyside-qt4.8+1.2.2/build2")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.rename("/usr/lib/pkgconfig/pyside.pc", "pyside-py2.pc")
+    #pisitools.rename("/usr/lib/pkgconfig/pyside.pc", "pyside-py2.pc")
 
     #Python3
-    shelltools.cd("../build3")
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.rename("/usr/lib/pkgconfig/pyside.pc", "pyside-py3.pc")
+    #shelltools.cd("../build3")
+    #cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #pisitools.rename("/usr/lib/pkgconfig/pyside.pc", "pyside-py3.pc")
