4fd49ca9e76792a4ab260b5c77fdfe22101d4b68
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-01-24 15:14:35 +0200

    blender-2.73a

--- extra/multimedia/editor/blender/actions.py
+++ extra/multimedia/editor/blender/actions.py
@@ -16,7 +16,7 @@ def setup():
     shelltools.cd("..")
     shelltools.makedirs("cmake-make")
     shelltools.cd("cmake-make") 
-    shelltools.system("cmake ../blender-2.73 \
+    shelltools.system("cmake ../blender-2.73a \
                       -DCMAKE_INSTALL_PREFIX=/usr \
                       -DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2 \
                       -DCMAKE_BUILD_TYPE=Release \
