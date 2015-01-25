5876093306b78a80d1d9d092c8f07b40e247acaa
“Hakan <hknyldz93@gmail.com>
2015-01-25 12:04:48 +0200

    some packages upgraded

--- extra/network/chat/hexchat/actions.py
+++ extra/network/chat/hexchat/actions.py
@@ -10,19 +10,19 @@ from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                          --enable-python \
-                          --enable-shm \
-                          --enable-spell='libsexy' \
-                          --enable-textfe")
+    autotools.configure(" --prefix=/usr \
+                          --enable-{dbus,perl,python='python2.7',textfe,fishlim,doat,sysinfo,minimal-flags}")
 
     pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
-    
+
 def build():
     autotools.make()
 
 def install():
     pisitools.domo("po/tr.po", "tr", "hexchat.mo")
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.cd("share/doc")
-    #pisitools.dodoc("COPYING", "readme.*")
+    autotools.rawInstall("DESTDIR='%s' \
+               	          UPDATE_ICON_CACHE=true \
+                          UPDATE_MIME_DATABASE=true \
+                          UPDATE_DESKTOP_DATABASE=true" % get.installDIR())
+    #shelltools.cd("share/doc")
+    pisitools.dodoc("COPYING", "readme.*")
