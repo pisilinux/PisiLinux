a70329987303bedac82608ab5e19aedbdc61824f
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-01-28 00:30:07 +0200

    phase 2

--- main/kernel/kernel/actions.py
+++ main/kernel/kernel/actions.py
@@ -24,14 +24,6 @@ def setup():
 def build():
     kerneltools.build(debugSymbols=False)
 
-    # When bumping major version build man files and put them into files/man
-
-    autotools.make("JOBS=5 WERROR=0 -C tools/perf perf HAVE_CPLUS_DEMANGLE=1")
-
-    # Build cpupowertools
-    autotools.make("-C tools/power/cpupower CPUFREQ_BENCH=false")
-    autotools.make("-C tools/power/cpupower/debug/%s centrino-decode powernow-k8-decode" % cpupower_arch)
-
 def install():
     kerneltools.install()
 
@@ -40,15 +32,7 @@ def install():
 
     kerneltools.installLibcHeaders()
 
-    # Install cpupowertools stuff
-    autotools.install("-C tools/power/cpupower DESTDIR=%s libdir=/usr/lib mandir=/%s CPUFREQ_BENCH=false" % (get.installDIR(), get.manDIR()))
-
-    pisitools.dobin("tools/power/cpupower/debug/%s/centrino-decode" % cpupower_arch)
-    pisitools.dobin("tools/power/cpupower/debug/%s/powernow-k8-decode" % cpupower_arch)
-
     # Generate some module lists to use within mkinitramfs
     shelltools.system("./generate-module-list %s/lib/modules/%s" % (get.installDIR(), kerneltools.__getSuffix()))
-
-    # Build and install the new 'perf' tool
-    pisitools.insinto("/usr/bin", "tools/perf/perf", "perf.%s-%s" % (get.srcNAME(), get.srcVERSION()))
-
+    
+    #generate perf and cpupowertools tar.xz's
