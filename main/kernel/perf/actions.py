445afd9ec58a6bf349bde76c605e2c1745648ab1
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-01-30 23:49:05 +0200

    perf is 1.

---
+++ main/kernel/perf/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("PYTHONDONTWRITEBYTECODE", "1")
+
+def build():
+    shelltools.cd("tools/perf")
+    autotools.make("JOBS=5 WERROR=0 perf HAVE_CPLUS_DEMANGLE=1")
+
+def install():
+    shelltools.cd("tools/perf")
+    # Build and install the new 'perf' tool
+    #pisitools.insinto("/usr/bin", "tools/perf/perf", "perf.%s-%s" % (get.srcNAME(), get.srcVERSION()))
+    autotools.install("JOBS=5 WERROR=0 perf HAVE_CPLUS_DEMANGLE=1")
+    
+    pisitools.domove("/usr/etc/bash_completion.d/perf", "/etc/bash_completion.d")
+    pisitools.removeDir("/usr/etc")
+    
+    pisitools.domove("/usr/lib64/libperf-gtk.so", "/usr/lib64/")
+    pisitools.removeDir("/usr/lib64")
