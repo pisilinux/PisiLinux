b62858fb0a4679599257bdd086d8c03488d4c15a
groni <groni3427@gmail.com>
2015-02-04 18:34:06 +0100

    kadu version bump

--- extra/network/chat/kadu/actions.py
+++ extra/network/chat/kadu/actions.py
@@ -10,9 +10,9 @@ from pisi.actionsapi import shelltools
 
 def setup():
     #fix install path for sdk
-    pisitools.dosed("cmake/FindKadu.cmake", "(\s+set\s\(KADU_SDK_DIR\s\$\{CMAKE_INSTALL_PREFIX\}\/)(sdk\))", r"\1share/kadu/\2")
+    #pisitools.dosed("cmake/FindKadu.cmake", "(\s+set\s\(KADU_SDK_DIR\s\$\{CMAKE_INSTALL_PREFIX\}\/)(sdk\))", r"\1share/kadu/\2")
     #kadu using lib64 as default if 64 bits linux and not debian
-    cmaketools.configure("-DCMAKE_INSTALL_LIBDIR=lib")
+    cmaketools.configure("-DCMAKE_INSTALL_LIBDIR=/usr/lib")
 
 def build():
     cmaketools.make()
