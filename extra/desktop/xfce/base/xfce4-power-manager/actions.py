38ea698e1dfbc6a6c2fbcb0863053a12bf20e3d4
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-03-11 17:31:03 +0200

    xfce4-power-manager:fixed deps

--- extra/desktop/xfce/base/xfce4-power-manager/actions.py
+++ extra/desktop/xfce/base/xfce4-power-manager/actions.py
@@ -15,7 +15,6 @@ def build():
     autotools.make()
 
 def install():
-    #autotools.install()
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
