d75f76b956a284c6a158a0396c66afc066309f81
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-03-11 16:31:09 +0200

    xfce4-notes-plugin:rebuild

--- extra/desktop/xfce/addon/xfce4-notes-plugin/actions.py
+++ extra/desktop/xfce/addon/xfce4-notes-plugin/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
     pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
