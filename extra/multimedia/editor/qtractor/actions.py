eccb146576cf77c7638a04d9721b873b9cd10b22
groni <groni3427@gmail.com>
2015-04-04 18:00:19 +0200

    qtractor version bump

--- extra/multimedia/editor/qtractor/actions.py
+++ extra/multimedia/editor/qtractor/actions.py
@@ -4,6 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import qt4
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
