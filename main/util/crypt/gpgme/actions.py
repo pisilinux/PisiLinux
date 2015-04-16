550c932dba4bace92706c45a7bd15fb075add54b
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-04-16 11:19:43 +0300

    gpgme version bump

--- main/util/crypt/gpgme/actions.py
+++ main/util/crypt/gpgme/actions.py
@@ -9,9 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vfi")
-    
-    autotools.configure("--disable-fd-passing \
+     autotools.configure("--disable-fd-passing \
                          --disable-static \
                          --disable-gpgsm-test")
 
