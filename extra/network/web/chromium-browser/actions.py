c9944e0228dd271fe08eb9f489c8f2fcfaa12f59
vedat <vedat@pisilinux.org>
2015-04-25 01:01:14 +0300

    v. bump

--- extra/network/web/chromium-browser/actions.py
+++ extra/network/web/chromium-browser/actions.py
@@ -115,7 +115,7 @@ def install():
     for size in ["22", "24", "48", "64", "128", "256"]:
         pisitools.insinto("/usr/share/icons/hicolor/%sx%s/apps" %(size, size), "chrome/app/theme/chromium/product_logo_%s.png" % size, "chromium-browser.png")
         
-    shelltools.copytree("PepperFlash", "%s/usr/lib/chromium-browser/" % get.installDIR())
+    #shelltools.copytree("PepperFlash", "%s/usr/lib/chromium-browser/" % get.installDIR())
 			
     pisitools.dosym("/usr/share/icons/hicolor/256x256/apps/chromium-browser.png", "/usr/share/pixmaps/chromium-browser.png")
     
