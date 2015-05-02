8fc39b282a350d7bda4c9d1adace73f509810205
BlackSEA- <blacksea8393@gmail.com>
2015-05-02 18:42:32 +0300

    Revert "fix radeon issue"
    This reverts commit 098b8c7f7615aff72e86e8b251aa6a06972ccd69.

--- main/x11/library/mesa/actions.py
+++ main/x11/library/mesa/actions.py
@@ -16,28 +16,23 @@ def setup():
 # --enable-32-bit option is not present anymore. Although build fails in emul32. With --disable-asm option, not fail. Needs to be tested.
 
     options ="\
-              --with-dri-driverdir=/usr/lib/xorg/modules/dri \
-              --with-gallium-drivers=r300,r600,nouveau,svga,swrast \
+              --with-gallium-drivers=r300,r600,radeonsi,nouveau,svga,swrast \
               --with-dri-drivers=i915,i965,r200,radeon,nouveau,swrast \
               --with-egl-platforms=x11,drm,wayland \
-              --enable-xa \
-              --enable-dri \
+              --enable-llvm-shared-libs \
               --enable-egl \
               --enable-gbm \
+              --enable-shared-glapi \
               --enable-glx \
-              --enable-dri3 \
+              --enable-glx-tls \
+              --enable-dri \
+              --enable-osmesa \
               --enable-gles1 \
               --enable-gles2 \
-              --enable-vdpau \
-              --enable-osmesa \
-              --enable-sysfs \
-              --enable-xvmc \
-              --enable-glx-tls \
-              --enable-gallium-egl \
-              --enable-gallium-gbm \
-              --enable-gallium-llvm \
-              --enable-shared-glapi \
               --enable-texture-float \
+              --enable-xa \
+              --enable-vdpau \
+              --enable-nine \
              "
 
     if get.buildTYPE() == "emul32":
@@ -63,8 +58,9 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.domove("%s/libGL.so.1.2.0" % Libdir, "%s/mesa" % Libdir)
+    pisitools.domove("%s/libGL.so.1.2.0" % Libdir, "%s/" % Libdir)
     pisitools.dosym("libGL.so.1.2.0", "%s/libGL.so.1.2" % Libdir)
+
 
     if get.buildTYPE() == "emul32":
         return
