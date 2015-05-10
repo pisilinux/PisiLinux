1efcc3b377b719aa6dbe503c671ecf7b271dc6d9
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-05-10 09:26:25 +0300

    kbd new version

--- main/system/base/kbd/actions.py
+++ main/system/base/kbd/actions.py
@@ -8,19 +8,22 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-nls")
+    autotools.configure("--enable-nls \
+                         --prefix=/usr \
+                         --datadir=/usr/share/kbd \
+                         --mandir=/usr/share/man")
 
 def build():
-    autotools.make()
+    autotools.make("KEYCODES_PROGS=yes RESIZECONS_PROGS=yes")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("KEYCODES_PROGS=yes RESIZECONS_PROGS=yes DESTDIR=%s" % get.installDIR())
 
     for exe in ("loadkeys", "setfont", "unicode_start", "unicode_stop"):
         pisitools.domove("/usr/bin/%s" % exe, "/bin")
         pisitools.dosym("/bin/%s" % exe, "/usr/bin/%s" % exe)
 
-    pisitools.remove("/usr/share/keymaps/i386/qwerty/trf.map.gz")
+    pisitools.remove("/usr/share/kbd/keymaps/i386/qwerty/trf.map.gz")
 
-    pisitools.dohtml("doc/*")
+    pisitools.dohtml("docs/doc/*.html")
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
