40ca4f944e81458f3ec10eb0eef80afbf0bda7c3
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-05-11 14:31:17 +0300

    lxdm:moved into misc folder

--- extra/desktop/misc/lxdm/actions.py
+++ extra/desktop/misc/lxdm/actions.py
@@ -9,14 +9,18 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--prefix=/usr --sysconfdir=/etc")
+    autotools.configure("--prefix=/usr     \
+                         --sysconfdir=/etc \
+                         --with-pam        \
+                         --with-systemdsystemunitdir=no \
+                         --enable-gtk3")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
+
     #install Pisi Linux default theme
     pisitools.insinto("/usr/share/lxdm/themes", "lxdm-pisilinux-theme")
     pisitools.remove("/usr/share/lxdm/themes/lxdm-pisilinux-theme/login.png")
