833896a8f96e6f0ba677786a8b77ac3b769f1729
vedat <vedat@pisilinux.org>
2015-06-01 18:44:31 +0300

    smplayer : version bump

--- extra/multimedia/video/smplayer/actions.py
+++ extra/multimedia/video/smplayer/actions.py
@@ -14,7 +14,7 @@ def setup():
     pisitools.dosed("Makefile", "^DOC_PATH=.*$", "DOC_PATH=$(PREFIX)/share/doc/smplayer")
 
 def build():
-    autotools.make("PREFIX=/usr")
+    autotools.make("PREFIX=/usr QMAKE=/usr/lib/qt5/bin/qmake LRELEASE=/usr/lib/qt5/bin/lrelease")
 
 def install():
     autotools.rawInstall("PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcNAME()))
