e2975e9b6a03db75a6f11f8a77442fb52e2a73b0
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-06-11 14:58:20 +0300

    ConsoleKit v.bump

--- main/system/base/ConsoleKit/actions.py
+++ main/system/base/ConsoleKit/actions.py
@@ -26,9 +26,10 @@ def setup():
                          --enable-pam-module \
                          --enable-udev-acl \
                          --enable-pam-module \
+                         --with-rundir=/run \
                          --with-dbus-services=/usr/share/dbus-1/services \
                          --with-logrotate-dir=/etc/logrotate.d \
-                         --with-xinitrc-dir=/etc/X11/xinit/xinitrc \
+                         --with-xinitrc-dir=/etc/X11/xinit \
                          --with-pam-module-dir=/lib/security \
                          --without-systemdsystemunitdir \
                           XMLTO_FLAGS='--skip-validation' \
