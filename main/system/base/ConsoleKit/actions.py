e3c59558f4178a6c27938910531c8e4ebd59e7eb
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-06-12 21:08:31 +0300

    sync with manjaro --not work --

--- main/system/base/ConsoleKit/actions.py
+++ main/system/base/ConsoleKit/actions.py
@@ -13,32 +13,35 @@ def setup():
     # /var/run => /run
     #pisitools.dosed("configure.ac", "^(\s+CONSOLE_KIT_PID_FILE=)\$\{localstatedir\}(\/run\/ConsoleKit\/pid)", r"\1\2")
     #pisitools.dosed("src/Makefile.am", "\$\(localstatedir\)(\/run\/ConsoleKit)", r"\1")
-    shelltools.system("sed -i -e '/SystemdService/d' data/org.freedesktop.ConsoleKit.service.in")
+    #shelltools.system("sed -i -e '/SystemdService/d' data/org.freedesktop.ConsoleKit.service.in")
 
-    autotools.autoreconf("-fi")
+    #autotools.autoreconf("-fi")
 
-    autotools.configure("\
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --sbindir=/usr/sbin \
+                         --with-rundir=/run \
                          --libexecdir=/usr/libexec/ConsoleKit \
                          --localstatedir=/var \
-                         --with-pid-file=/run/ConsoleKit/pid \
-                         --with-systemdsystemunitdir=no \
-                         --disable-static \
+                         --enable-polkit \
                          --enable-pam-module \
                          --enable-udev-acl \
-                         --enable-pam-module \
-                         --with-rundir=/run \
+                         --enable-docbook-docs \
+                         --disable-static \
                          --with-dbus-services=/usr/share/dbus-1/services \
-                         --with-xinitrc-dir=/etc/X11/xinit/xinitrc \
+                         --with-logrotate-dir=/etc/logrotate.d \
+                         --with-xinitrc-dir=/etc/X11/xinit/xinitrc.d \
                          --with-pam-module-dir=/lib/security \
                          --with-systemdsystemunitdir=no \
-                          XMLTO_FLAGS='--skip-validation' \
-                        ")
+                         XMLTO_FLAGS='--skip-validation' \
+                         ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s/" % get.installDIR())
-    pisitools.dodir("/run/console")
+    
+    pisitools.removeDir("/run")
 
     pisitools.dodoc("AUTHORS", "README", "COPYING", "HACKING", "NEWS", "TODO")
